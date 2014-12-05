2f6e2bd21ff31a525c958f2e035561cf2ae8f835
Rohan Garg <rohan@kde.org>
2014-12-05 16:26:35 +0100

    Host tooling for Debian CI

---
+++ dci/source.rb
@@ -0,0 +1,16 @@
+require 'logger'
+
+logger = Logger.new(STDOUT)
+
+logger.info("Starting source only build")
+
+Dir.chdir('packaging') do
+    $changelog = Changelog.new
+end
+
+SOURCE_NAME = $changelog.name
+
+WORKSPACE_DIR = '/workspace/' + ENV['WORKSPACE'].split('/')[-1].strip
+
+system("schroot -c unstable-amd64 -d #{WORKSPACE_DIR} -- ./tooling/contained/dci.rb source #{WORKSPACE_DIR}")
+system("dcmd mv /var/lib/sbuild/build/#{SOURCE_NAME}*.changes build/")
