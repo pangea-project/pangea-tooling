f5cb99d7dbfeeab69f613ebd9b1d6391138fa152
Rohan Garg <rohan@kde.org>
2014-12-09 17:11:23 +0100

    Make sure we clean out things before copying over artifacts

--- dci/source.rb
+++ dci/source.rb
@@ -15,4 +15,6 @@ RELEASE = ENV['WORKSPACE'].split('_')[-1]
 
 system("schroot -u root -c unstable-amd64 -d #{ENV['WORKSPACE']} -- ruby ./tooling/ci-tooling/dci.rb source #{ENV['WORKSPACE']} #{RELEASE}")
 Dir.mkdir('build') unless Dir.exist? 'build'
+
+system("dcmd rm -rf build/*.changes")
 raise 'Cant move files!' unless system("dcmd mv /var/lib/sbuild/build/#{SOURCE_NAME}*.changes build/")
