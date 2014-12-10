66b5bad7a1bcab0fbbb8a995dd37201721924e1d
Rohan Garg <rohan@kde.org>
2014-12-10 01:56:05 +0100

    Make sure we mkdir the dir before copying things around

--- dci/build.rb
+++ dci/build.rb
@@ -1,4 +1,5 @@
 require 'logger'
+require 'fileutils'
 
 logger = Logger.new(STDOUT)
 
@@ -10,4 +11,5 @@ logger.info("Starting binary build for #{RELEASE}")
 # TODO: Extend this so that we don't hardcode amd64 here, and instead use something from the job
 system("schroot -u root -c #{RELEASE}-amd64 -d #{ENV['WORKSPACE']} -- ruby ./tooling/ci-tooling/dci.rb build #{ARGV[1]}")
 
+FileUtils.mkdir_p('build/binary') unless Dir.exists? 'build/binary'
 system("dcmd mv /var/lib/sbuild/build/binary/#{PACKAGE}*.changes build/binary/")
