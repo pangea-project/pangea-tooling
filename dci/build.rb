f89dcfc378b034a08bce661889213a68f807b64d
Rohan Garg <rohan@kde.org>
2014-12-11 00:14:12 +0100

    Make sure we search in the right dir

--- dci/build.rb
+++ dci/build.rb
@@ -5,6 +5,7 @@ logger = Logger.new(STDOUT)
 
 RELEASE = `grep Distribution #{ARGV[1]}`.split(':')[-1].strip
 PACKAGE = `grep Source #{ARGV[1]}`.split(':')[-1].strip
+RESULT_DIR = '/var/lib/sbuild/build/'
 
 logger.info("Starting binary build for #{RELEASE}")
 
@@ -12,9 +13,9 @@ logger.info("Starting binary build for #{RELEASE}")
 system("schroot -u root -c #{RELEASE}-amd64 -d #{ENV['WORKSPACE']} -- ruby ./tooling/ci-tooling/dci.rb build #{ARGV[1]}")
 
 FileUtils.mkdir_p('build/binary') unless Dir.exists? 'build/binary'
-changes_files = Dir.glob("#{PACKAGE}*changes").select { |changes| !changes.include? 'source' }
+changes_files = Dir.glob("#{RESULT_DIR}/#{PACKAGE}*changes").select { |changes| !changes.include? 'source' }
 
 changes_files.each do |changes_file|
     logger.info("Copying over #{changes_file} into Jenkins")
-    system("dcmd mv /var/lib/sbuild/build/#{changes_file} build/binary/")
+    system("dcmd mv #{RESULT_DIR}/#{changes_file} build/binary/")
 end
