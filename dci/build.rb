f59f510872c4d2f2cb8ebd26e471e4a520639d2a
Rohan Garg <rohan@kde.org>
2014-12-17 18:49:15 +0100

    Update with tooling to build with custom repos

--- dci/build.rb
+++ dci/build.rb
@@ -1,16 +1,26 @@
 require 'logger'
 require 'fileutils'
+require 'json'
 
 logger = Logger.new(STDOUT)
 
 RELEASE = `grep Distribution #{ARGV[1]}`.split(':')[-1].strip
 PACKAGE = `grep Source #{ARGV[1]}`.split(':')[-1].strip
 RESULT_DIR = '/var/lib/sbuild/build'
+REPOS_FILE = 'debian/meta/add_repos.json'
 
 logger.info("Starting binary build for #{RELEASE}")
+repos = []
+Dir.chdir("#{ENV['WORKSPACE']}/packaging") do
+  if File.exist? REPOS_FILE
+      repos = JSON::Parse(File.read(REPOS_FILE))['repos']
+  end
+end
+
+repos = repos.join(',')
 
 # TODO: Extend this so that we don't hardcode amd64 here, and instead use something from the job
-system("schroot -u root -c #{RELEASE}-amd64 -d #{ENV['WORKSPACE']} -- ruby ./tooling/ci-tooling/dci.rb build #{ARGV[1]}")
+system("schroot -u root -c #{RELEASE}-amd64 -d #{ENV['WORKSPACE']} -- ruby ./tooling/ci-tooling/dci.rb build -r #{repos} -w #{ENV['WORKSPACE']}/tooling/data #{ARGV[1]}")
 
 FileUtils.mkdir_p('build/binary') unless Dir.exists? 'build/binary'
 changes_files = Dir.glob("#{RESULT_DIR}/#{PACKAGE}*changes").select { |changes| !changes.include? 'source' }
