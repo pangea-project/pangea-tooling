c45e7064fa97e0e094063be1510911fb6ba778ca
Rohan Garg <rohan@kde.org>
2014-12-17 19:29:46 +0100

    Add custom repo tooling to source builds

--- dci/source.rb
+++ dci/source.rb
@@ -9,11 +9,20 @@ Dir.chdir('packaging') do
     $changelog = Changelog.new
 end
 
+repos = []
+Dir.chdir("#{ENV['WORKSPACE']}/packaging") do
+  if File.exist? REPOS_FILE
+      repos = JSON::Parse(File.read(REPOS_FILE))['repos']
+  end
+end
+
+repos = repos.join(',')
+
 SOURCE_NAME = $changelog.name
 
 RELEASE = ENV['JOB_NAME'].split('_')[-1]
 
-system("schroot -u root -c #{RELEASE}-amd64 -d #{ENV['WORKSPACE']} -- ruby ./tooling/ci-tooling/dci.rb source #{ENV['WORKSPACE']} #{RELEASE}")
+system("schroot -u root -c #{RELEASE}-amd64 -d #{ENV['WORKSPACE']} -- ruby ./tooling/ci-tooling/dci.rb source -r #{repos} -w #{ENV['WORKSPACE']}/tooling/data #{ENV['WORKSPACE']} #{RELEASE}")
 Dir.mkdir('build') unless Dir.exist? 'build'
 
 raise 'Cant move files!' unless system("dcmd mv /var/lib/sbuild/build/#{SOURCE_NAME}*.changes build/")
