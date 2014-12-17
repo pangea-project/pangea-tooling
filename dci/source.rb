3f9b90627a07e6636cabfad745bd6789e9cb0aff
Rohan Garg <rohan@kde.org>
2014-12-17 23:20:24 +0100

    Make sure we specify releases via the command line

--- dci/source.rb
+++ dci/source.rb
@@ -26,7 +26,12 @@ SOURCE_NAME = $changelog.name
 
 RELEASE = ENV['JOB_NAME'].split('_')[-1]
 
-system("schroot -u root -c #{RELEASE}-amd64 -d #{ENV['WORKSPACE']} -- ruby ./tooling/ci-tooling/dci.rb source -r #{repos} -w #{ENV['WORKSPACE']}/tooling/data #{ENV['WORKSPACE']} #{RELEASE}")
+system("schroot -u root -c #{RELEASE}-amd64 -d #{ENV['WORKSPACE']} -- ruby ./tooling/ci-tooling/dci.rb source \
+        -r #{repos} \
+        -w #{ENV['WORKSPACE']}/tooling/data \
+        -R #{RELEASE} \
+         #{ENV[WORKSPACE]}")
+
 Dir.mkdir('build') unless Dir.exist? 'build'
 
 raise 'Cant move files!' unless system("dcmd mv /var/lib/sbuild/build/#{SOURCE_NAME}*.changes build/")
