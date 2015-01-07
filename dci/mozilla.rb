e9760f66048a20a1800343c3c78827fff1c5ee2f
Rohan Garg <rohan@kde.org>
2015-01-07 19:12:46 +0100

    Need to assign values to RELEASE and PACKAGE

--- dci/mozilla.rb
+++ dci/mozilla.rb
@@ -1,3 +1,9 @@
+fail "Need a mozilla product to build for!" unless ARGV[1]
+fail "Need a release to build for!" unless ARGV[2]
+
+PACKAGE = ARGV[1]
+RELEASE = ARGV[2]
+
 system("schroot -u root -c #{RELEASE}-amd64 -d #{ENV['WORKSPACE']} -- ruby ./tooling/ci-tooling/dci.rb mozilla \
     #{PACKAGE} #{RELEASE}")
 
