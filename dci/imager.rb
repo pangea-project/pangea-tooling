c2d052a1900decd046486519022587a14a06a159
Rohan Garg <rohan@kde.org>
2015-01-12 14:21:59 +0100

    Remove superfluous brackets

--- dci/imager.rb
+++ dci/imager.rb
@@ -10,4 +10,4 @@ $logger = new_logger
 
 logger.info("Starting ISO build for #{ARGV[1]}")
 system("schroot -u root -c #{RELEASE}-amd64 -d #{ENV['WORKSPACE']} -- ruby ./tooling/ci-tooling/dci.rb imager \
-        #{RELEASE} #{{FLAVOR}}")
+        #{RELEASE} #{FLAVOR}")
