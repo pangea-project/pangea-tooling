2461741b6b91cbb44826345f053864eb965ca148
Rohan Garg <rohan@kde.org>
2015-01-12 14:18:22 +0100

    Add imager tooling

---
+++ dci/imager.rb
@@ -0,0 +1,13 @@
+require_relative '../ci-tooling/lib/logger'
+
+fail 'Need a release to build for!' unless ARGV[1]
+fail 'Need a flavor!' unless ARGV[2]
+
+RELEASE = ARGV[1]
+FLAVOR = ARGV[2]
+
+$logger = new_logger
+
+logger.info("Starting ISO build for #{ARGV[1]}")
+system("schroot -u root -c #{RELEASE}-amd64 -d #{ENV['WORKSPACE']} -- ruby ./tooling/ci-tooling/dci.rb imager \
+        #{RELEASE} #{{FLAVOR}}")
