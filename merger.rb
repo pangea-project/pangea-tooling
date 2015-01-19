b50a245fbd47ce1c29bbf111f90d0661be5c61ff
Harald Sitter <sitter@kde.org>
2015-01-19 10:32:25 +0100

    a whole new merger to marvel at

---
+++ merger.rb
@@ -0,0 +1,125 @@
+#!/usr/bin/env ruby
+
+require 'git'
+require 'logger'
+require 'logger/colors'
+
+class Logger
+  def self.new_for_merger
+    l = Logger.new(STDOUT)
+    l.progname = 'merger'
+    l.level = Logger::INFO
+    l
+  end
+
+  def self.new_for_git
+    l = Logger.new(STDOUT)
+    l.progname = 'git'
+    l.level = Logger::WARN
+    l
+  end
+end
+
+# Merger merges delpoyment branches into KCI integration branches and KCI
+# integration branches into one another.
+class Merger
+  # Logger instance used by the Merger.
+  attr_reader :log
+
+  # Creates a new Merger. Creates a logger, sets up dpkg-mergechangelogs and
+  # opens Dir.pwd as a Git::Base.
+  def initialize
+    @log = Logger.new_for_merger
+
+    Git.global_config('merge.dpkg-mergechangelogs.name', 'debian/changelog merge driver')
+    Git.global_config('merge.dpkg-mergechangelogs.driver', 'dpkg-mergechangelogs -m %O %A %B %A')
+    if File.exist?('/var/lib/jenkins/tooling/git')
+      Git.configure { |c| c.binary_path = '/var/lib/jenkins/tooling/git' }
+    end
+    @git = Git.open(Dir.pwd, log: Logger.new_for_git)
+  end
+
+  def merge_stable(source)
+    target = []
+    target = @git.branches.remote.select {|b| b.name.end_with?('kubuntu_stable') } if target.empty?
+    target = @git.branches.remote.select {|b| b.name.end_with?('kubuntu_unstable') } if target.empty?
+    raise "There appears to be no kubuntu_stable nor kubuntu_unstable branch!" if target.empty? || target.size > 1
+    merge(source, target[0])
+
+    merge_variants('kubuntu_stable') # stable in variants
+  end
+
+  def merge_variants(typebase)
+    # FIXME: should make sure typebase exists
+    @git.branches.remote.each do |target|
+      next unless target.name.start_with?("#{typebase}_")
+      merge(typebase, target)
+    end
+  end
+
+  def merge_unstable(source)
+    target = @git.branches.remote.select {|b| b.name == 'kubuntu_unstable' }[0]
+    raise "There is no unstable branch!" unless target
+    merge(source, target)
+
+    merge_variants('kubuntu_unstable') # unstable in variants
+  end
+
+  # Merge order:
+  #  - master | kubuntu_vivid_archive
+  #   -> merge into stable | unstable depending on what is available
+  #  - kubuntu_stable
+  #   -> merge into unstable
+  #   -> merge into series variants
+  #  - kubuntu_unstable
+  #   -> merge into series variants
+  def run(trigger_branch)
+    @log.info "triggered by #{trigger_branch}"
+
+    cleanup('master')
+
+    # merge_stable('master')# trigger_branch in stable
+    merge_stable('kubuntu_vivid_archive')# trigger_branch in stable
+    merge_unstable('kubuntu_stable') # stable in unstable
+  end
+
+private
+  # Hard resets to head, cleans everything, and sets dpkg-mergechangelogs in
+  # .gitattributes afterwards.
+  def cleanup(target=@git.current_branch)
+    @git.fetch('origin')
+    @git.reset("remotes/origin/#{target}", hard: true)
+    @git.clean(force: true, d: true)
+    @git.pull('origin', target)
+    File.write('.gitattributes', 'debian/changelog merge=dpkg-mergechangelogs\n')
+  end
+
+  # Merges source into target and pushes the merge result.
+  # @param source either a Git::Branch or a String specifying the branch from
+  #   which should be merged
+  # @param target either a Git::Branch or a String specifying the branch in
+  #   which  should be merged
+  def merge(source, target)
+    unless source.respond_to?(:full) # We want the full branch name of the remote to work with
+      source_name = source.clone
+      source = @git.branches.remote.select { |b| b.name == source_name }[0]
+      unless source
+        @log.warn "Apparently there is no branch named #{source_name}!"
+        return
+      end
+    end
+    if target.respond_to?(:name)
+      target = target.name
+    end
+    @git.checkout(target)
+    cleanup(target)
+    msg = "Merging #{source.full} into #{target}."
+    @log.info msg
+    @git.merge(source.full, msg)
+    @log.info @git.push('origin', target)
+  end
+end
+
+if __FILE__ == $0
+  Merger.new.run(ENV['GIT_BRANCH'])
+end
---
+++ test/test_merger.rb
@@ -0,0 +1,122 @@
+require 'fileutils'
+require 'git'
+require 'test/unit'
+require 'tmpdir'
+
+require_relative '../merger'
+
+class MergerTest < Test::Unit::TestCase
+  def in_repo(&block)
+    Dir.mktmpdir(__callee__.to_s) do |t|
+      g = Git.clone(repo, t)
+      g.chdir do
+        yield g
+      end
+    end
+  end
+
+  def create_sample_branch(g, name)
+    g.checkout('master', new_branch: name)
+    FileUtils.touch("#{name}file")
+    g.add("#{name}file")
+    g.commit_all("#{name}msg")
+    g.push('origin', name)
+  end
+
+  def repo
+    return @remotedir if @remotedir
+
+    @remotedir = "#{@tmpdir}/remote"
+    puts ":::: creating #{@remotedir}"
+    Dir.mkdir(@remotedir)
+    Dir.chdir(@remotedir) { Git.init('.', bare: true) }
+
+    in_repo do |g|
+      FileUtils.touch('masterfile')
+      g.add('masterfile')
+      g.commit_all('mastermsg')
+      g.push
+
+      # Create all default branches
+      create_sample_branch(g, 'kubuntu_unstable')
+    end
+
+    return @remotedir
+  end
+
+  def setup
+    @tmpdir = Dir.mktmpdir(self.class.to_s)
+    Dir.chdir(@tmpdir)
+  end
+
+  def teardown
+    Dir.chdir('/')
+    FileUtils.rm_rf(@tmpdir)
+  end
+
+  def test_full_merge_chain
+    in_repo do |g|
+      create_sample_branch(g, 'kubuntu_vivid_archive')
+      create_sample_branch(g, 'kubuntu_stable')
+      create_sample_branch(g, 'kubuntu_stable_utopic')
+      # NOTE: unstable already exists
+      create_sample_branch(g, 'kubuntu_unstable_utopic')
+    end
+
+    in_repo do
+      assert_nothing_raised { Merger.new.run('origin/kubuntu_vivid_archive') }
+    end
+
+    in_repo do |g|
+      assert_nothing_raised do
+         g.checkout('kubuntu_stable')
+       end
+       assert(File.exist?('kubuntu_vivid_archivefile'))
+       assert(File.exist?('kubuntu_stablefile'))
+
+       assert_nothing_raised do
+         g.checkout('kubuntu_stable_utopic')
+       end
+       assert(File.exist?('kubuntu_vivid_archivefile'))
+       assert(File.exist?('kubuntu_stablefile'))
+       assert(File.exist?('kubuntu_stable_utopicfile'))
+
+       assert_nothing_raised do
+         g.checkout('kubuntu_unstable')
+       end
+       assert(File.exist?('kubuntu_vivid_archivefile'))
+       assert(File.exist?('kubuntu_stablefile'))
+       assert(File.exist?('kubuntu_unstablefile'))
+
+       assert_nothing_raised do
+         g.checkout('kubuntu_unstable_utopic')
+       end
+       assert(File.exist?('kubuntu_vivid_archivefile'))
+       assert(File.exist?('kubuntu_stablefile'))
+       assert(File.exist?('kubuntu_unstablefile'))
+       assert(File.exist?('kubuntu_unstable_utopicfile'))
+    end
+  end
+
+  def test_stable_trigger
+    in_repo do |g|
+      g.checkout('master', new_branch: 'kubuntu_stable')
+      FileUtils.touch('stablefile')
+      g.add('stablefile')
+      g.commit_all('stablemsg')
+      g.push('origin', 'kubuntu_stable')
+
+      g.checkout('master', new_branch: 'kubuntu_stable_utopic')
+      g.push('origin', 'kubuntu_stable_utopic')
+    end
+
+    in_repo do
+      Merger.new.run('origin/kubuntu_stable')
+    end
+
+    in_repo do |g|
+      assert_nothing_raised { g.checkout('kubuntu_stable_utopic') }
+      assert(File.exist?('stablefile'), 'Apparently stable did not get merged into stable_utopic')
+    end
+  end
+end
