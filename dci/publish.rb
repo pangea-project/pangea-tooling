8ebc9445eaf2beb63f45c0459b8912f37787f173
Rohan Garg <rohan@kde.org>
2015-01-26 15:32:39 +0100

    Make use of the new functions in ci-tooling/lib/dci.rb

--- dci/publish.rb
+++ dci/publish.rb
@@ -1,4 +1,5 @@
 require_relative '../ci-tooling/lib/logger'
+require_relative '../ci-tooling/lib/dci'
 
 fail 'Need target and changes file!' unless ARGV.count >= 2
 fail 'File is not a changes file!' unless ARGV[2].end_with? '.changes'
@@ -30,21 +31,6 @@ allow_unsigned_uploads = 1
 
 $logger = DCILogger.instance
 
-def run_cmd(cmd)
-    retry_count = 0
-    begin
-        if retry_count <= 5
-            raise unless system(cmd)
-        else
-            $logger.fatal("Tried to run #{cmd} but retry count exceeded!")
-            exit 1
-        end
-    rescue RuntimeError
-        retry_count += 1
-        retry
-    end
-end
-
 DPUT_CF = ENV['HOME'] + '/.dput.cf'
 if File.read(DPUT_CF) != DPUT_CONTENTS
     File.open(DPUT_CF, 'w') { |f|
@@ -55,6 +41,6 @@ if File.read(DPUT_CF) != DPUT_CONTENTS
     }
 end
 
-run_cmd("dput #{ARGV[1]} #{ARGV[2]}")
+dci_run_cmd("dput #{ARGV[1]} #{ARGV[2]}")
 
 $logger.close
