b9f9fdaae59dcd1ecf79651e16945926b084a23c
Harald Sitter <sitter@kde.org>
2015-01-28 11:53:46 +0100

    make mgmt-lxc an upstream to the respective meta-build job
    this makes mgmt-lxc a transitive upstream to all builds that depend on the
    specific container. since mgmt-lxc is blocking on downstreams and builds
    are blocking on upstreams this makes mgmt-lxc not execute while builds are
    in progress.
    has the somewhat unfortunate downside of mgmt-lxc being able to hold up
    a batch integration if it gets queued while builds are in progress. still
    better than having builds fail en mass because the original container
    is running.

--- jenkins-jobs/mgmt-lxc.rb
+++ jenkins-jobs/mgmt-lxc.rb
@@ -3,10 +3,12 @@ require_relative 'job'
 class MGMTLXCJob < JenkinsJob
   attr_reader :type
   attr_reader :distribution
+  attr_reader :dependees
 
-  def initialize(type:, distribution:, architecture:)
+  def initialize(type:, distribution:, dependees:)
     super("mgmt_lxc_#{distribution}_#{type}", 'mgmt-lxc.xml.erb')
     @type = type
     @distribution = distribution
+    @dependees = dependees.collect(&:job_name)
   end
 end
--- update-projects.rb
+++ update-projects.rb
@@ -111,11 +111,18 @@ class ProjectUpdater
         # FIXME: this maybe should be moved into MetaIsoJob or something
         # all_isos is actually unused
         ARCHITECTURES.each do |architecture|
-          args = { type: type,
-                   distribution: distribution,
-                   architecture: architecture }
-          enqueue(IsoJob.new(args))
-          enqueue(MGMTLXCJob.new(args))
+          isoargs = { type: type,
+                      distribution: distribution,
+                      architecture: architecture }
+          enqueue(IsoJob.new(isoargs))
+          # TODO: this is a bit of naughty trick. we fish out the latest meta
+          #       builder which would be utopic_unstable or utopic_stable etc.
+          #       It probably should be put in an expicit variable, or at least
+          #       architecture-ized.
+          lxcargs = { type: type,
+                      distribution: distribution,
+                      dependees: [all_meta_builds.last] }
+          enqueue(MGMTLXCJob.new(lxcargs))
         end
         # FIXME: doesn't automatically add new ISOs ...
         enqueue(MetaIsoJob.new(type: type, distribution: distribution))
