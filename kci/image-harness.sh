542052a5085fd0a4e54ec758fc3aff7fce5539be
Harald Sitter <sitter@kde.org>
2015-02-06 15:41:49 +0100

    sync image harness

--- kci/image-harness.sh
+++ kci/image-harness.sh
@@ -15,22 +15,30 @@ finish() {
         unset SCHROOT_SESSION
     fi
 }
-trap finish EXIT
+trap finish EXIT 
 
 # Manually handle the schroot session to prevent it from lingering after we exit.
 export SCHROOT_SESSION="session:$(schroot -b -c $CNAME)"
 # Creepy argument handling, but shell is shit
+
 ssh jenkins@localhost "cd `pwd` && pwd && schroot -r -c $SCHROOT_SESSION $TOOLING_PATH/imager/build.sh `pwd` $DIST $ARCH $TYPE"
+#schroot -r -c $SCHROOT_SESSION $TOOLING_PATH/imager/build.sh `pwd` $DIST $ARCH $TYPE
+
 schroot -e -c $SCHROOT_SESSION
+
 unset SCHROOT_SESSION
 
 ls -lah result
+
+
 DATE=$(cat result/date_stamp)
 PUB=/var/www/kci/images/$ARCH/$DATE
 mkdir -p $PUB
 cp -r --no-preserve=ownership result/*.iso $PUB
 cp -r --no-preserve=ownership result/*.manifest $PUB
 cp -r --no-preserve=ownership result/*.zsync $PUB
-ls -lah $PUB
 chown jenkins:www-data -Rv $PUB
+
 cp -avr $PUB /mnt/s3/kci/images/$ARCH/
+
+unset SCHROOT_SESSION
