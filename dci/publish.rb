9e578da1520928815d012ba70ceaec7de221e595
Rohan Garg <rohan@kde.org>
2015-02-16 17:08:58 +0100

    Use a new dput method

--- dci/publish.rb
+++ dci/publish.rb
@@ -4,43 +4,15 @@ require_relative '../ci-tooling/lib/dci'
 fail 'Need target and changes file!' unless ARGV.count >= 2
 fail 'File is not a changes file!' unless ARGV[2].end_with? '.changes'
 
-DPUT_CONTENTS = "[plasma]
-fqdn = dci.pangea.pub
-login = publisher
-method = sftp
-incoming = /home/publisher/kde/incoming
-run_dinstall = 0
-allow_unsigned_uploads = 1
-
-[maui]
-fqdn = dci.pangea.pub
-login = publisher
-method = sftp
-incoming = /home/publisher/maui/incoming
-run_dinstall = 0
-allow_unsigned_uploads = 1
-
-[moz-plasma]
-fqdn = dci.pangea.pub
-login = publisher
-method = sftp
-incoming = /home/publisher/moz/incoming
-run_dinstall = 0
-allow_unsigned_uploads = 1
+DPUT_CONTENTS = "[dci]
+fqdn                    = dci.pangea.pub
+method                  = sftp
+incoming                = /home/publisher/%(dci)s
+login                   = publisher
 "
 
 $logger = DCILogger.instance
 
-DPUT_CF = ENV['HOME'] + '/.dput.cf'
-if File.read(DPUT_CF) != DPUT_CONTENTS
-    File.open(DPUT_CF, 'w+') { |f|
-        f.flock(File::LOCK_EX)
-        f.puts(DPUT_CONTENTS)
-        f.flush()
-        f.flock(File::LOCK_UN)
-    }
-end
-
-dci_run_cmd("dput #{ARGV[1]} #{ARGV[2]}")
+dci_run_cmd("echo #{DPUT_CONTENTS} | dput -c /dev/stdin dci:#{ARGV[1]} #{ARGV[2]}")
 
 $logger.close
