a25abbcff31ca8dbbc9a131a8ef441c062083546
Harald Sitter <sitter@kde.org>
2015-02-19 12:45:33 +0100

    cover ci-tooling in parser test

--- test/test_parse.rb
+++ test/test_parse.rb
@@ -1,108 +1,117 @@
-require "test/unit"
+require 'test/unit'
 
 class Shebang
-    attr_reader :valid
-    attr_reader :parser
+  attr_reader :valid
+  attr_reader :parser
 
-    def initialize(line)
-        @valid = false
-        @parser = nil
+  def initialize(line)
+    @valid = false
+    @parser = nil
 
-        return unless line
-        return unless line.start_with?("#!")
+    return unless line
+    return unless line.start_with?("#!")
 
-        parts = line.split(" ")
+    parts = line.split(" ")
 
-        return unless parts.size >= 1
-        if parts[0].end_with?("/env")
-            return unless parts.size >= 2
-            @parser = parts[1]
-        elsif !parts[0].include?("/") or parts[0].end_with?("/")
-            return # invalid
-        else
-            @parser = parts[0].split("/").pop
-        end
-
-        @valid = true
+    return unless parts.size >= 1
+    if parts[0].end_with?("/env")
+      return unless parts.size >= 2
+      @parser = parts[1]
+    elsif !parts[0].include?("/") or parts[0].end_with?("/")
+      return # invalid
+    else
+      @parser = parts[0].split("/").pop
     end
+
+    @valid = true
+  end
 end
 
 class ParseTest < Test::Unit::TestCase
-    def test_shebang
-        s = Shebang.new(nil)
-        assert(!s.valid)
+  def test_shebang
+    s = Shebang.new(nil)
+    assert(!s.valid)
 
-        s = Shebang.new("")
-        assert(!s.valid)
+    s = Shebang.new("")
+    assert(!s.valid)
 
-        s = Shebang.new("#!")
-        assert(!s.valid)
+    s = Shebang.new("#!")
+    assert(!s.valid)
 
-        s = Shebang.new("#!/usr/bin/env ruby")
-        assert(s.valid)
-        assert(s.parser == "ruby")
+    s = Shebang.new("#!/usr/bin/env ruby")
+    assert(s.valid)
+    assert(s.parser == "ruby")
 
-        s = Shebang.new("#!/usr/bin/bash")
-        assert(s.valid)
-        assert(s.parser == "bash")
+    s = Shebang.new("#!/usr/bin/bash")
+    assert(s.valid)
+    assert(s.parser == "bash")
 
-        s = Shebang.new("#!/bin/sh -xe")
-        assert(s.valid)
-        assert(s.parser == "sh")
+    s = Shebang.new("#!/bin/sh -xe")
+    assert(s.valid)
+    assert(s.parser == "sh")
+  end
+
+  def test_syntax
+    basedir = File.dirname(File.expand_path(File.dirname(__FILE__)))
+    Dir.chdir(basedir)
+
+    source_dirs = %w(
+      dci
+      kci
+      lib
+      test
+      ci-tooling/kci
+      ci-tooling/dci
+      ci-tooling/lib
+      ci-tooling/test
+    )
+    source_dirs.each do | source_dir |
+      Dir.glob("#{source_dir}/**/*.rb").each do |file|
+        parse_ruby(file)
+      end
+      Dir.glob("#{source_dir}/**/*.sh").each do |file|
+        parse_shell(file)
+      end
     end
 
-    def test_syntax
-        basedir = File.dirname(File.expand_path(File.dirname(__FILE__)))
-        Dir.chdir(basedir)
-
-        source_dirs = %w[dci kci lib tests]
-        source_dirs.each do | source_dir |
-            Dir.glob("#{source_dir}/**/*.rb").each do |file|
-                parse_ruby(file)
-            end
-            Dir.glob("#{source_dir}/**/*.sh").each do |file|
-                parse_shell(file)
-            end
-        end
-
-        # Do not recurse the main dir.
-        Dir.glob("*.rb").each do |file|
-            parse_ruby(file)
-        end
-        Dir.glob("*.sh").each do |file|
-            parse_shell(file)
-        end
+    # Do not recurse the main dir.
+    Dir.glob('*.rb').each do |file|
+      parse_ruby(file)
     end
-
-private
-    def parse_bash(file)
-        assert(system("bash -n #{file}"))
+    Dir.glob('*.sh').each do |file|
+      parse_shell(file)
     end
+  end
 
-    def parse_ruby(file)
-        puts "ruby file: #{file}"
-        assert(system("ruby -c #{file} 1> /dev/null"))
-    end
+  private
+  def parse_bash(file)
+    assert(system("bash -n #{file}"))
+  end
 
-    def parse_sh(file)
-        assert(system("sh -n #{file}"))
-    end
+  def parse_ruby(file)
+    puts "ruby file: #{file}"
+    assert(system("ruby -c #{file} 1> /dev/null"))
+  end
 
-    def parse_shell(file)
-        puts "shell file: #{file}"
-        shebang = Shebang.new(File.open(file).readline)
-        case shebang.parser
-        when "bash"
-            parse_bash(file)
-        when "sh"
-            parse_sh(file)
-        else
-            if shebang.valid
-                warn "  shell type unknown, falling back to bash"
-            else
-                warn "  shebang invalid, falling back to bash"
-            end
-            parse_bash(file)
-        end
+  def parse_sh(file)
+    assert(system("sh -n #{file}"))
+  end
+
+  def parse_shell(file)
+    puts "shell file: #{file}"
+    shebang = Shebang.new(File.open(file).readline)
+    case shebang.parser
+    when "bash"
+      parse_bash(file)
+    when "sh"
+      parse_sh(file)
+    else
+      if shebang.valid
+        warn "  shell type unknown, falling back to bash"
+      else
+        warn "  shebang invalid, falling back to bash"
+      end
+      parse_bash(file)
     end
+  end
 end
