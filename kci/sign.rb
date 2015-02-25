4ff56d1edcbdda488b83a888e187081411f73604
Harald Sitter <sitter@kde.org>
2015-02-25 16:43:04 +0100

    import sign from ci-tooling

---
+++ kci/sign.rb
@@ -0,0 +1,10 @@
+CHANGES = ARGV[0]
+
+fail 'No changes file/path specified.' unless CHANGES
+
+args = []
+args << '--no-re-sign'
+args << '-k' << env['KEYID'] if env.key?('KEYID')
+args << CHANGES
+
+fail 'Signing the source package failed.' unless system('debsign', *args)
