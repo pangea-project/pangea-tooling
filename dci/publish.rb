a67aecde096bef163c0e30609b5fe808f17f755c
Rohan Garg <rohan@garg.io>
2015-03-09 18:28:32 +0100

    Make sure we process each package separately

--- dci/publish.rb
+++ dci/publish.rb
@@ -1,9 +1,8 @@
 require_relative '../ci-tooling/lib/logger'
 require_relative '../ci-tooling/lib/dci'
 
-fail 'Need target and changes file!' unless ARGV.count >= 2
-fail 'File is not a changes file!' unless ARGV[2].end_with? '.changes'
 
+fail 'Need target and changes file!' unless ARGV.count >= 2
 DPUT_CONTENTS = "[dci]
 fqdn                    = dci.pangea.pub
 method                  = sftp
@@ -13,6 +12,10 @@ login                   = publisher
 
 $logger = DCILogger.instance
 
-dci_run_cmd("echo \"#{DPUT_CONTENTS}\" | dput -u -c /dev/stdin dci:#{ARGV[1]} #{ARGV[2..-1].join(' ')}")
+ARGV.each do |a|
+  next unless a.end_with '.changes'
+  dci_run_cmd("echo \"#{DPUT_CONTENTS}\" | dput -uf -c /dev/stdin dci:#{ARGV[1]} #{a}")
+
+end
 
 $logger.close
