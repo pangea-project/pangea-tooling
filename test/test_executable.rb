c355397a06d3315c0cbcc1202fcfd3fd5f133cf1
Harald Sitter <sitter@kde.org>
2015-03-12 13:45:52 +0100

    mark binaries +x and add a test to make sure it stays that way

---
+++ test/test_executable.rb
@@ -0,0 +1,27 @@
+require 'test/unit'
+
+class ExecutableTest < Test::Unit::TestCase
+  BINARY_DIRS = %w(
+    .
+    ci-tooling
+    kci
+    ci-tooling/kci
+  )
+
+  SUFFIXES = %w(.py .rb .sh)
+
+  def test_all_binaries_exectuable
+    basedir = File.dirname(File.expand_path(File.dirname(__FILE__)))
+    not_executable = []
+    BINARY_DIRS.each do |dir|
+      SUFFIXES.each do |suffix|
+        pattern = File.join(basedir, dir, "*#{suffix}")
+        Dir.glob(pattern).each do |file|
+          next unless File.exist?(file)
+          not_executable << file unless File.executable?(file)
+        end
+      end
+    end
+    assert(not_executable.empty?, "Missing +x on #{not_executable.join("\n")}")
+  end
+end
