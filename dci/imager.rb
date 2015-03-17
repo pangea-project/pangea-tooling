b63c3123de874f881f8a9b2cd9165523d38ee5da
Rohan Garg <rohan@garg.io>
2015-03-17 17:37:10 +0100

    Fix argument handling

--- dci/imager.rb
+++ dci/imager.rb
@@ -1,14 +1,14 @@
 require_relative '../ci-tooling/lib/logger'
 
-fail 'Need a release to build for!' unless ARGV[1]
-fail 'Need a flavor!' unless ARGV[2]
+fail 'Need a release to build for!' unless ARGV[0]
+fail 'Need a flavor!' unless ARGV[1]
 
-RELEASE = ARGV[1]
-FLAVOR = ARGV[2]
+RELEASE = ARGV[0]
+FLAVOR = ARGV[1]
 
 $logger = DCILogger.instance
 
-$logger.info("Starting ISO build for #{ARGV[1]}")
+$logger.info("Starting ISO build for #{FLAVOR}")
 system("schroot -u root -c #{RELEASE}-amd64 -d #{ENV['WORKSPACE']} \
         -o jenkins.workspace=#{ENV['WORKSPACE']} \
         -- ruby ./tooling/ci-tooling/dci.rb imager \
