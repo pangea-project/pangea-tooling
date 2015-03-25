1c7c47f106ecaf6d6a866c9f7766c387c2c4561c
Rohan Garg <rohan@garg.io>
2015-03-25 15:45:55 +0100

    Use new naming scheme

--- dci/imager.rb
+++ dci/imager.rb
@@ -1,4 +1,5 @@
 require_relative '../ci-tooling/lib/logger'
+require 'fileutils'
 
 fail 'Need a release to build for!' unless ARGV[1]
 fail 'Need a flavor!' unless ARGV[2]
@@ -6,18 +7,17 @@ fail 'Need a flavor!' unless ARGV[2]
 RELEASE = ARGV[1]
 FLAVOR = ARGV[2]
 
-$logger = DCILogger.instance
+logger = DCILogger.instance
 
-$logger.info("Starting ISO build for #{RELEASE}")
-system("schroot -u root -c #{RELEASE}-amd64 -d #{ENV['WORKSPACE']} \
+logger.info("Starting ISO build for #{RELEASE}")
+
+system("schroot -u root -c #{RELEASE}-#{ENV['arch']} -d #{ENV['WORKSPACE']} \
         -o jenkins.workspace=#{ENV['WORKSPACE']} \
         -- ruby ./tooling/ci-tooling/dci.rb imager \
         #{RELEASE} #{FLAVOR}")
 
 Dir.mkdir('build') unless Dir.exist? 'build'
-system('mv',
-       '-v',
-       '/var/lib/sbuild/build/MD5SUM',
-       '/var/lib/sbuild/build/SHA256SUM',
-       'build/')
-system('mv -v /var/lib/sbuild/build/live-image* build/')
+
+FileUtils.mv("/var/lib/sbuild/build/#{FLAVOR}", 'build/')
+
+logger.close
