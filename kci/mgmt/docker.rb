90ea1c65564026a514c903e8a81ac082c7dd032b
Harald Sitter <sitter@kde.org>
2015-03-31 12:34:27 +0200

    dockerfile and getting the tooling deployed is a bit shitty, so workaround
    see comments

--- kci/mgmt/docker.rb
+++ kci/mgmt/docker.rb
@@ -6,6 +6,9 @@ require 'logger/colors'
 
 NAME = ENV.fetch('NAME')
 Docker.options[:read_timeout] = 3 * 60 * 60 # 3 hours.
+REPO = "jenkins/#{NAME}"
+TAG = 'deploying'
+REPO_TAG = "#{REPO}:#{TAG}"
 
 @log = Logger.new(STDOUT)
 @log.level = Logger::WARN
@@ -14,8 +17,8 @@ Thread.new do
   Docker::Event.stream { |event| @log.debug event }
 end
 
-Docker::Image.build(File.read(File.dirname(__FILE__) + '/Dockerfile'),
-                    t: "jenkins/#{NAME}:latest") do |chunk|
+image = Docker::Image.build(File.read(File.dirname(__FILE__) + '/Dockerfile'),
+                            t: REPO_TAG) do |chunk|
   chunk = JSON.parse(chunk)
   keys = chunk.keys
   if keys.include?('stream')
@@ -27,3 +30,39 @@ Docker::Image.build(File.read(File.dirname(__FILE__) + '/Dockerfile'),
     fail "Unknown response type in #{chunk}"
   end
 end
+
+# Dockerfiles can not mount host paths, so instead manually conduct this step
+# through an intermediate container. This containers is then commited to both
+# deploying and latest. Such that we also can reuse the bundle install when
+# re-deploying.
+
+# FIXME: there should be a deploy target on rake or something to avoid having
+# to write this shite here.=
+cmd = <<EOF
+  set -ex
+  cd /var/lib/jenkins/tooling-pending
+  bundle install --no-cache --local --frozen --system --without development test
+  rm -rf /var/lib/jenkins/ci-tooling /var/lib/jenkins/.gem /var/lib/jenkins/.rvm
+  cp -r /var/lib/jenkins/tooling-pending/ci-tooling /var/lib/jenkins/ci-tooling
+EOF
+helper_script = '/var/lib/jenkins/tooling-pending/_helper'
+File.write(helper_script, cmd)
+
+c = Docker::Container.create(Image: REPO_TAG,
+                             WorkingDir: ENV.fetch('HOME'),
+                             Cmd: ['sh', helper_script])
+
+c.start(Binds: ['/var/lib/jenkins/tooling-pending:/var/lib/jenkins/tooling-pending'])
+c.attach do |_stream, chunk|
+  puts chunk
+end
+# FIXME: we completely ignore errors
+c.stop!
+# FIXME: we are leaking images...
+# dockerfile build will reuse the original image that matches the file, essentially
+# rolling back the bundle step, the only possible way to work around this is
+# with COPY but that only wants relative paths, so we'd have to build from a tar
+# which is slightly meh and a bit trickier.
+c.commit(repo: REPO, tag: 'latest', comment: 'autodeploy', author: 'Kubuntu CI <sitter@kde.org>')
+# p new_i.tag(repo: REPO, tag: 'latest', force: true)
+c.remove
