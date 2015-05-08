891eb003f6b4e02d318b8fbe26b0d78ff08b164d
Harald Sitter <sitter@kde.org>
2015-05-08 17:00:53 +0200

    install zlib for nokogiri

--- deploy_in_container.sh
+++ deploy_in_container.sh
@@ -8,7 +8,7 @@ echo 'Acquire::http { Proxy "http://10.0.3.1:3142"; };' > /etc/apt/apt.conf.d/00
 echo 'Acquire::Languages "none";' > /etc/apt/apt.conf.d/00aptitude
 echo 'APT::Color "1";' > /etc/apt/apt.conf.d/99color
 
-apt-get -y -o APT::Get::force-yes=true -o Debug::pkgProblemResolver=true install rake ruby ruby-dev build-essential
+apt-get -y -o APT::Get::force-yes=true -o Debug::pkgProblemResolver=true install rake ruby ruby-dev build-essential zlib1g-dev
 
 cd $SCRIPTDIR
 exec rake -f deploy_in_container.rake deploy_in_container
