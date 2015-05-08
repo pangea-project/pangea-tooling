e307437323da57a518d0f517498ecffb7e24b0c9
Harald Sitter <sitter@kde.org>
2015-05-08 16:54:45 +0200

    install build-essential as part of bootstrap install
    we need it to build native ruby extensions

--- deploy_in_container.sh
+++ deploy_in_container.sh
@@ -4,7 +4,7 @@ export DEBIAN_FRONTEND=noninteractive
 export LANG=en_US.UTF-8
 SCRIPTDIR=$(readlink -f $(dirname -- "$0"))
 
-apt-get -y -o APT::Get::force-yes=true -o Debug::pkgProblemResolver=true install rake ruby ruby-dev
+apt-get -y -o APT::Get::force-yes=true -o Debug::pkgProblemResolver=true install rake ruby ruby-dev build-essential
 
 cd $SCRIPTDIR
 exec rake -f deploy_in_container.rake deploy_in_container
