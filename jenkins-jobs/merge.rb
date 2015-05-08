0f46cd8fd9515e1de656cb354971d3ff47e4bce8
Harald Sitter <sitter@kde.org>
2015-05-08 13:06:15 +0200

    port merge job branch listing to lib/kci
    each merger watches
    - master
    - kubuntu_series_{archive,backports}
    - kubuntu_type_series (e.g. kubuntu_unstable_vivid)

--- jenkins-jobs/merge.rb
+++ jenkins-jobs/merge.rb
@@ -1,4 +1,5 @@
 require_relative 'job'
+require_relative '../ci-tooling/lib/kci'
 
 # Mergers merge a set of branches
 class MergeJob < JenkinsJob
@@ -11,13 +12,14 @@ class MergeJob < JenkinsJob
     super("merger_#{project.name}", 'merger.xml.erb')
     @name = project.name
     @component = project.component
-    @merge_branches = %w(kubuntu_stable
-                         kubuntu_stable_utopic
-                         kubuntu_unstable
-                         kubuntu_unstable_utopic
-                         master
-                         kubuntu_vivid_archive
-                         kubuntu_vivid_backports)
+    @merge_branches = %w(master)
+    KCI.series.each_key do |series|
+      @merge_branches << "kubuntu_#{series}_archive"
+      @merge_branches << "kubuntu_#{series}_backports"
+      KCI.types.each do |type|
+        @merge_branches << "kubuntu_#{type}_#{series}"
+      end
+    end
     @dependees = dependees
   end
 end
