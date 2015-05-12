38e721e27b1435edd82ab77f8adda9e335353d25
Debian CI <rohan@garg.io>
2015-05-12 16:20:16 +0200

    Copy over tooling into the image and then commit it

--- dci/mgmt/build.rb
+++ dci/mgmt/build.rb
@@ -9,11 +9,12 @@ require_relative '../../ci-tooling/lib/retry'
 
 Docker.options[:read_timeout] = 3 * 60 * 60 # 3 hours.
 
+ARCH = RbConfig::CONFIG['host_cpu']
 RELEASE = ENV.fetch('RELEASE')
 JOB_NAME = ENV.fetch('JOB_NAME')
 WORKSPACE = ENV.fetch('WORKSPACE')
 JENKINS_HOME = ENV.fetch('JENKINS_HOME')
-REPO = "ci/#{RbConfig::CONFIG['host_cpu']}"
+REPO = "ci/#{ARCH}"
 TAG = ENV.fetch('BUILD_NUMBER')
 REPO_TAG = "#{REPO}:#{TAG}"
 
@@ -25,8 +26,8 @@ class Dockerfile
   def initialize
     @series = RELEASE
     @jenkins_home = JENKINS_HOME
-    @user = 'debian' if RbConfig::CONFIG['host_cpu'] == 'x86_64'
-    @user = 'armbuild/debian' if RbConfig::CONFIG['host_cpu'] == 'arm'
+    @user = 'debian' if ARCH == 'x86_64'
+    @user = 'armbuild/debian' if ARCH == 'arm'
   end
 
   def render
@@ -36,6 +37,15 @@ class Dockerfile
 
   def self.render
     new.render
+  end
+end
+
+def excavate_stdout(c)
+  Thread.new do
+    c.attach do |_stream, chunk|
+      puts chunk
+      STDOUT.flush
+    end
   end
 end
 
@@ -62,7 +72,6 @@ end
 
 @log.debug('Dockerfile successfully built')
 
-# FIXME: Hard coded paths
 cmd = "
 set -ex
 cd #{WORKSPACE}/tooling
@@ -78,22 +87,33 @@ File.write(helper_script, cmd)
 binds = ["#{WORKSPACE}:#{WORKSPACE}",
          "#{JENKINS_HOME}/.ssh:#{JENKINS_HOME}/.ssh"]
 
-c = Docker::Container.create(Image: REPO_TAG,
-                             WorkingDir: WORKSPACE,
-                             Cmd: ['/bin/bash', '-l',
-                                   "#{WORKSPACE}/helper.sh"])
-
+c = Containment.new(JOB_NAME,
+                    image: REPO_TAG,
+                    WorkingDir: WORKSPACE)
+excavate_stdout(c)
 c.start(Binds: binds)
-Thread.new do
-  c.attach do |_stream, chunk|
-    puts chunk
-    STDOUT.flush
-  end
-end
+status_code = c.wait.fetch('StatusCode', 1)
+interim_id = c.stop![:id]
+
+exit status_code unless status_code == 0
+
+cmd = "
+set -ex
+cp -aRv #{WORKSPACE}/tooling /opt/
+"
+
+helper_script = "#{WORKSPACE}/copier.sh"
+File.write(helper_script, cmd)
+
+c = Containment.new(JOB_NAME,
+                    image: interim_id,
+                    WorkingDir: WORKSPACE)
+excavate_stdout(c)
+c.start(Binds: binds)
 status_code = c.wait.fetch('StatusCode', 1)
 c.stop!
 
-c.commit(repo: REPO, tag: 'latest', comment: 'autodeploy',
-         author: 'Debian CI <rohan@garg.io>') unless status_code == 0
+exit status_code unless status_code == 0
 
-exit status_code
+c.commit(repo: REPO, tag: 'latest', comment: 'autodeploy',
+         author: 'Debian CI <rohan@garg.io>')
