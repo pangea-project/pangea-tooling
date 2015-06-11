386effa1b8b72246cbc29d276b7a35608784229e
Harald Sitter <sitter@kde.org>
2015-06-11 09:02:40 -0700

    add dynamic node listing to deploy

--- kci/mgmt/docker_armhf_image_deploy.rb
+++ kci/mgmt/docker_armhf_image_deploy.rb
@@ -2,13 +2,16 @@
 
 require 'net/scp'
 
+require_relative '../../ci-tooling/lib/jenkins'
+
 NAME = ENV.fetch('NAME')
 VERSION = ENV.fetch('VERSION')
 REPO = "jenkins/#{NAME}"
 TAG = 'latest'
 REPO_TAG = "#{REPO}:#{TAG}"
 
-%w(alpha beta gamma delta).each do |node|
+Jenkins.client.node.list.each do |node|
+  next if node == 'master'
   Net::SCP.start(node, 'jenkins-slave') do |scp|
     puts scp.upload!('image.tar', '/tmp/image.tar')
     puts ssh.exec!("docker import /tmp/image.tar #{REPO_TAG}")
