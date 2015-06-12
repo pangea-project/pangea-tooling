68534abad1d0b5668f4a7bc4bbc06210ddd17ba3
Harald Sitter <sitter@kde.org>
2015-06-12 10:29:50 -0700

    log the image to node deployment
    gets stuck for reasons
    
    supposedly import implodes or something

--- kci/mgmt/docker_armhf_image_deploy.rb
+++ kci/mgmt/docker_armhf_image_deploy.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 
 require 'net/scp'
+require 'logger'
+require 'logger/colors'
 
 require_relative '../../ci-tooling/lib/jenkins'
 
@@ -10,10 +12,16 @@ REPO = "jenkins/#{NAME}"
 TAG = 'latest'
 REPO_TAG = "#{REPO}:#{TAG}"
 
+@log = Logger.new(STDERR)
+
 Jenkins.client.node.list.each do |node|
   next if node == 'master'
+  @log.info "deploying to node #{node}"
   Net::SCP.start(node, 'jenkins-slave') do |scp|
+    @log.info 'uploading image'
     puts scp.upload!('image.tar', '/tmp/image.tar')
+    @log.info 'importing'
     puts scp.session.exec!("cat /tmp/image.tar > docker import - #{REPO_TAG}")
+    @log.info 'import done'
   end
 end
