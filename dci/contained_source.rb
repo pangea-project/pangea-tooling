5e7c53b9c2a5217f6182bf446a9c1d6d2a33bc66
Rohan Garg <rohan@garg.io>
2015-07-08 21:09:02 +0200

    Write out metadata for next step

--- dci/contained_source.rb
+++ dci/contained_source.rb
@@ -1,8 +1,11 @@
 #!/usr/bin/env ruby
+require 'yaml'
 
 require_relative '../ci-tooling/lib/ci/build_source'
 
 RELEASE = ENV.fetch('RELEASE')
 
 s = VcsSourceBuilder.new(series: RELEASE)
-s.run
+r = s.run
+# Write out metadata
+open('build/source.yaml', 'w+') { |f| f.write(YAML.dump(r)) }
