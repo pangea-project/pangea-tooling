21ccc0fccaf725f4f6640b493be1a73b70e36a75
Harald Sitter <sitter@kde.org>
2015-07-10 14:39:08 +0200

    force amd64 architecture tag

--- kci/imager/mobster-config-hooks/repo.sh
+++ kci/imager/mobster-config-hooks/repo.sh
@@ -2,4 +2,4 @@ keyfile="/tmp/tmp.key"
 rm -rf $keyfile
 wget -O $keyfile "http://mobile.kci.pangea.pub/Pangea%20CI.gpg.key"
 gpg --no-default-keyring --primary-keyring config/archives/ubuntu-defaults.key --import $keyfile
-echo "deb http://mobile.kci.pangea.pub $SUITE main" >> config/archives/ubuntu-defaults.list
+echo "deb [arch=amd64] http://mobile.kci.pangea.pub $SUITE main" >> config/archives/ubuntu-defaults.list
