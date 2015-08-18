530858a9c8e2c4ab9d28bb79cedb6139e855235e
Harald Sitter <sitter@kde.org>
2015-08-18 10:37:57 +0200

    fix proposed sources line

--- kci/imager/kubuntu-config-hooks/proposed.sh
+++ kci/imager/kubuntu-config-hooks/proposed.sh
@@ -1,1 +1,1 @@
-echo "deb ${LB_APT_HTTP_PROXY} ${SUITE}-proposed main" >> config/archives/ubuntu-defaults.list
+echo "deb ${LB_APT_HTTP_PROXY}/archive.ubuntu.com/ubuntu ${SUITE}-proposed main restricted universe multiverse" >> config/archives/ubuntu-defaults.list
