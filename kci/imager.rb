df6af74f6d01a831b5d4708afb2bc462a08efe44
Harald Sitter <sitter@kde.org>
2015-09-25 10:03:56 +0200

    nuke CNAME it isn't used and was replaced by JOB_NAME

--- kci/imager.rb
+++ kci/imager.rb
@@ -10,7 +10,6 @@ JOB_NAME = ENV.fetch('JOB_NAME')
 DIST = ENV.fetch('DIST')
 TYPE = ENV.fetch('TYPE')
 ARCH = ENV.fetch('ARCH')
-CNAME = "jenkins-imager-#{DIST}-#{TYPE}-#{ARCH}"
 
 Docker.options[:read_timeout] = 4 * 60 * 60 # 4 hours.
 
