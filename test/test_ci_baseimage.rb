0458f9050f523883498dfe55576197b3114d7dc0
Harald Sitter <sitter@kde.org>
2015-09-28 09:23:37 +0200

    refactor image assertation into its own method

--- test/test_ci_baseimage.rb
+++ test/test_ci_baseimage.rb
@@ -6,23 +6,26 @@ class BaseImageTest < TestCase
     ENV.delete('TESTING')
   end
 
+  def assert_image(flavor, series, image, testing: ENV.fetch('TESTING', false))
+    prefix = testing ? 'pangea-testing' : 'pangea'
+    assert_equal("#{prefix}/#{flavor}:#{series}", image.to_s)
+    assert_equal("#{prefix}/#{flavor}", image.repo)
+    assert_equal(series, image.tag)
+  end
+
   def test_name
     flavor = 'ubuntu'
     series = 'wily'
     i = CI::BaseImage.new(flavor, series)
-    assert_equal("pangea/#{flavor}:#{series}", i.to_s)
-    assert_equal("pangea/#{flavor}", i.repo)
-    assert_equal(series, i.tag)
+    assert_image(flavor, series, i)
   end
 
   def test_testing_env
     ENV['TESTING'] = 'true'
     flavor = 'ubuntu'
     series = 'wily'
-    i = CI::BaseImage.new(flavor, serise)
-    assert_equal("pangea-testing/#{flavor}:#{series}", i.to_s)
-    assert_equal("pangea-testing/#{flavor}", i.repo)
-    assert_equal(series, i.tag)
+    i = CI::BaseImage.new(flavor, series)
+    assert_image(flavor, series, i)
     ENV.delete('TESTING')
   end
 end
