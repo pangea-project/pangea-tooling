cca9a2c0a6de38eb11fdcd8222561e472ece5db2
Harald Sitter <sitter@kde.org>
2015-09-28 14:00:27 +0200

    omit the test_cleanup_on_new as it can break live data ... shadeslayer!

--- test/test_jenkins_jobs_mgmt_docker_cleanup.rb
+++ test/test_jenkins_jobs_mgmt_docker_cleanup.rb
@@ -12,12 +12,17 @@ class MGMTDockerCleanupTest < TestCase
       }
     end
   end
+
   def test_render
     r = MGMTDockerCleanupJob.new(arch: 'arch')
     assert_equal(File.read("#{@datadir}/test_render.xml"), r.render_template)
   end
 
   def test_cleanup_on_new
+    omit('This test has no testing mode, it would tinker with live images if' \
+         ' there is no test data. This *must* not work on live data!')
+    pend('This needs a standalone test. It does not actually have anything to' \
+         ' to do with the job test.')
     VCR.use_cassette(__method__) do
       assert_nothing_raised do
         require_relative '../mgmt/docker_cleanup.rb'
