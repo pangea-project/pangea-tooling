4d3786c4bd21107072dc201b8bb912c085a67fb7
Harald Sitter <sitter@kde.org>
2015-09-28 09:18:32 +0200

    style++

--- test/test_ci_baseimage.rb
+++ test/test_ci_baseimage.rb
@@ -7,18 +7,22 @@ class BaseImageTest < TestCase
   end
 
   def test_name
-    i = CI::BaseImage.new('ubuntu', 'wily')
-    assert_equal("pangea/ubuntu:wily", i.to_s)
-    assert_equal("pangea/ubuntu", i.repo)
-    assert_equal("wily", i.tag)
+    flavor = 'ubuntu'
+    series = 'wily'
+    i = CI::BaseImage.new(flavor, series)
+    assert_equal("pangea/#{flavor}:#{series}", i.to_s)
+    assert_equal("pangea/#{flavor}", i.repo)
+    assert_equal(series, i.tag)
   end
 
   def test_testing_env
     ENV['TESTING'] = 'true'
-    i = CI::BaseImage.new('ubuntu', 'wily')
-    assert_equal("pangea-testing/ubuntu:wily", i.to_s)
-    assert_equal("pangea-testing/ubuntu", i.repo)
-    assert_equal("wily", i.tag)
+    flavor = 'ubuntu'
+    series = 'wily'
+    i = CI::BaseImage.new(flavor, serise)
+    assert_equal("pangea-testing/#{flavor}:#{series}", i.to_s)
+    assert_equal("pangea-testing/#{flavor}", i.repo)
+    assert_equal(series, i.tag)
     ENV.delete('TESTING')
   end
 end
