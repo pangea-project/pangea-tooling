928a02a3e21b65125a48c46fbffbe7825c57753b
Harald Sitter <sitter@kde.org>
2015-09-28 14:06:23 +0200

    rename mgmt to mgmt_docker test & update test data for new template

--- test/test_jenkins_jobs_mgmt_docker.rb
+++ test/test_jenkins_jobs_mgmt_docker.rb
@@ -1,9 +1,10 @@
-require_relative '../jenkins-jobs/mgmt-docker'
 require_relative '../ci-tooling/test/lib/testcase'
+require_relative '../jenkins-jobs/mgmt_docker'
 
 class MGMTDockerTest < TestCase
   def test_render
-    r = MGMTDockerJob.new(type: 'unstable', distribution: 'vivid', dependees: [])
+    r = MGMTDockerJob.new(dependees: [])
+    puts r.render_template
     assert_equal(File.read("#{@datadir}/test_render.xml"), r.render_template)
   end
 end
