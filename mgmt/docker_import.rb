04cb9e17f61b89015feddc65c5169d006d9c1ef6
Jonathan Riddell <jr@jriddell.org>
2015-09-30 13:47:51 +0100

    switch to wily

--- mgmt/docker_import.rb
+++ mgmt/docker_import.rb
@@ -12,5 +12,5 @@ Docker.options[:read_timeout] = 3 * 60 * 60 # 3 hours.
 
 @log.info "Importing #{ARGV[0]}"
 image = Docker::Image.import(ARGV[0])
-image.tag(repo: 'jenkins/vivid_unstable', tag: 'latest', force: true)
+image.tag(repo: 'jenkins/wily_unstable', tag: 'latest', force: true)
 @log.info 'Done'
