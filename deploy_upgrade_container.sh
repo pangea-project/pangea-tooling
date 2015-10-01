be618a5712f962c7bfe4e067739ea1b1fb22ff7e
Harald Sitter <sitter@kde.org>
2015-10-01 12:33:02 +0200

    new script deploy_upgrade_container
    this bugger sits on top of deploy_in_container and can move a container
    forward from version to version. this is needed during early development
    when a new version is not yet available in docker as well as for arm
    up until release as arm dockers are quite a bit behind.
    
    the script takes a from and to argument as argv1 and 2 respectively and
    essentially switches the sources.list about followed by a dist-upgrade

---
+++ deploy_upgrade_container.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+set -ex
+
+if [ -z "$1" ]; then
+  echo "$0 called with no argument from where to transition from (argument 1)"
+  exit 1
+fi
+if [ -z "$2" ]; then
+  echo "$0 called with no argument from where to transition to (argument 2)"
+  exit 1
+fi
+
+SCRIPTDIR=$(readlink -f $(dirname -- "$0"))
+
+export DEBIAN_FRONTEND=noninteractive
+export LANG=en_US.UTF-8
+
+sed -i "s/$1/$2/g" /etc/apt/sources.list
+sed -i "s/$1/$2/g" /etc/apt/sources.list.d/* || true
+
+apt-get update
+apt-get -y -o APT::Get::force-yes=true -o Debug::pkgProblemResolver=true dist-upgrade
+
+cd $SCRIPTDIR
+echo "Executing deploy_in_container.sh"
+if [ -z "$TESTING" ]; then
+  exec deploy_in_container.sh
+else # Testing
+  if grep vivid /etc/apt/sources.list; then
+    echo "Testing :: Found vivid in sources.list still!"
+    exit 1
+  fi
+  . /etc/lsb-release
+  if [ "$DISTRIB_CODENAME" = "vivid" ]; then
+    echo "Testing :: DISTRIB_CODENAME=vivid"
+    exit 1
+  fi
+fi
---
+++ test/test_deploy_upgrade_container.rb
@@ -0,0 +1,102 @@
+require 'vcr'
+
+require_relative '../lib/ci/containment.rb'
+require_relative '../ci-tooling/test/lib/testcase'
+
+Docker.options[:read_timeout] = 4 * 60 * 60 # 4 hours.
+
+class DeployUpgradeTest < TestCase
+  self.file = __FILE__
+
+  # :nocov:
+  def cleanup_container
+    # Make sure the default container name isn't used, it can screw up
+    # the vcr data.
+    c = Docker::Container.get(@job_name)
+    c.stop
+    c.kill!
+    c.remove
+  rescue Docker::Error::NotFoundError, Excon::Errors::SocketError
+  end
+
+  def cleanup_image
+    return unless Docker::Image.exist?(@image)
+    puts "Cleaning up image #{@image}"
+    image = Docker::Image.get(@image)
+    image.delete(force: true)
+  rescue Docker::Error::NotFoundError, Excon::Errors::SocketError
+  end
+
+  def create_container
+    puts "Creating new base image #{@image}"
+    Docker::Image.create(fromImage: 'ubuntu:vivid').tag(repo: @repo,
+                                                        tag: 'latest')
+  end
+
+  # :nocov:
+
+  def setup
+    VCR.configure do |config|
+      config.cassette_library_dir = @datadir
+      config.hook_into :excon
+      config.default_cassette_options = {
+        match_requests_on:  [:method, :uri, :body]
+      }
+    end
+    # Chdir to root, as Containment will set the working dir to PWD and this
+    # is slightly unwanted for tmpdir tests.
+    Dir.chdir('/')
+
+    @repo = self.class.to_s
+    @image = "#{@repo}:latest"
+
+    @job_name = @repo.tr(':', '_')
+    @tooling_path = File.expand_path("#{__dir__}/../")
+    @binds = ["#{@tooling_path}:/tooling-pending"]
+
+    VCR.turned_off do
+      cleanup_container
+      cleanup_image
+      create_container
+    end
+  end
+
+  def teardown
+    VCR.turned_off do
+      cleanup_container
+    end
+  end
+
+  def test_no_argv0
+    VCR.use_cassette(__method__) do
+      c = CI::Containment.new(@job_name, image: @image, binds: @binds)
+      cmd = ['sh', '/tooling-pending/deploy_upgrade_container.sh']
+      ret = c.run(Cmd: cmd)
+      assert_equal(1, ret)
+    end
+  end
+
+  def test_no_argv1
+    VCR.use_cassette(__method__) do
+      c = CI::Containment.new(@job_name, image: @image, binds: @binds)
+      cmd = ['sh', '/tooling-pending/deploy_upgrade_container.sh',
+             'vivid']
+      ret = c.run(Cmd: cmd)
+      assert_equal(1, ret)
+    end
+  end
+
+  def test_success
+    VCR.use_cassette(__method__) do
+      c = CI::Containment.new(@job_name, image: @image, binds: @binds)
+      cmd = ['sh', '/tooling-pending/deploy_upgrade_container.sh',
+             'vivid', 'wily']
+      ret = c.run(Cmd: cmd, Env: ['TESTING=true'])
+      assert_equal(0, ret)
+      # The script has testing capability built in since we have no proper
+      # provisioning to inspect containments post-run in any sort of reasonable
+      # way to make assertations. This is a bit of a tricky thing to get right
+      # so for the time being inside-testing will have to do.
+    end
+  end
+end
