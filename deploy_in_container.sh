5cbe05ce3b877ae9f03622ad12b1c48e085aec31
Rohan Garg <rohan@garg.io>
2015-10-01 16:02:37 +0200

    Use cloudfront for faster and more reliable access

--- deploy_in_container.sh
+++ deploy_in_container.sh
@@ -13,6 +13,9 @@ echo "LANG=$LANG" >> /etc/environment
 echo 'Acquire::Languages "none";' > /etc/apt/apt.conf.d/00aptitude
 echo 'APT::Color "1";' > /etc/apt/apt.conf.d/99color
 
+# Switch to cloudfront for debian
+sed -i 's,httpredir.debian.org,cloudfront.debian.net,g' /etc/apt/sources.list
+
 apt-get update
 apt-get -y -o APT::Get::force-yes=true -o Debug::pkgProblemResolver=true install rake ruby ruby-dev build-essential zlib1g-dev
 
