dde9ad34a0c427535206128a42756050b4267b07
Rohan Garg <rohan@garg.io>
2015-10-09 13:46:53 +0200

    Don't use CI::PangeaImage.namespace to evaluate string interpolation, it's better to use a internal var.

--- test/test_ci_pangeaimage.rb
+++ test/test_ci_pangeaimage.rb
@@ -3,11 +3,13 @@ require_relative '../lib/ci/pangeaimage'
 
 class PangeaImageTest < TestCase
   def setup
-    CI::PangeaImage.namespace = 'pangea-testing'
+    @namespace = 'pangea-testing'
+    CI::PangeaImage.namespace = @namespace
   end
+
   def assert_image(flavor, series, image)
-    assert_equal("#{CI::PangeaImage.namespace}/#{flavor}:#{series}", image.to_s)
-    assert_equal("#{CI::PangeaImage.namespace}/#{flavor}", image.repo)
+    assert_equal("#{@namespace}/#{flavor}:#{series}", image.to_s)
+    assert_equal("#{@namespace}/#{flavor}", image.repo)
     assert_equal("#{flavor}", image.flavor)
     assert_equal(series, image.tag)
   end
@@ -33,6 +35,6 @@ class PangeaImageTest < TestCase
     # Do not use assert_image here as we need to verify coercion from
     # :ubuntu to 'ubuntu' works as expected.
     # assert_image in fact relies on it.
-    assert_equal("#{CI::PangeaImage.namespace}/ubuntu:wily", image.to_s)
+    assert_equal("#{@namespace}/ubuntu:wily", image.to_s)
   end
 end
