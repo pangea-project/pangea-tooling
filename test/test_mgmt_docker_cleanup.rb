3d46df664047aa9553be069c60ba875fbbf1fad9
Harald Sitter <sitter@kde.org>
2015-10-29 11:04:36 +0100

    bring back part of the image avoidance for the cleanup test
    from c253f154690d479ac76fe9e4f15f5987b14800a8
    
    added a comment to clearify why we need this

--- test/test_mgmt_docker_cleanup.rb
+++ test/test_mgmt_docker_cleanup.rb
@@ -10,6 +10,16 @@ class MGMTDockerCleanupTest < TestCase
         match_requests_on:  [:method, :uri, :body]
       }
     end
+
+    # Clean up should only clean pangea-testing namespace. Testing itself needs
+    # to take care not to clean up images that shouldn't be cleaned though.
+    @oldnamespace = CI::PangeaImage.namespace
+    @namespace = 'pangea-testing'
+    CI::PangeaImage.namespace = @namespace
+  end
+
+  def teardown
+    CI::PangeaImage.namespace = @oldnamespace
   end
 
   def test_cleanup_on_new
