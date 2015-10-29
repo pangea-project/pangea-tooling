39d95e7c3b777fe31194705d65d1a8a8d978c06f
Harald Sitter <sitter@kde.org>
2015-10-29 11:06:41 +0100

    fixed pend by splitting

--- test/test_mgmt_docker_cleanup.rb
+++ test/test_mgmt_docker_cleanup.rb
@@ -25,8 +25,6 @@ class MGMTDockerCleanupTest < TestCase
   def test_cleanup_on_new
     omit('This test has no testing mode, it would tinker with live images if' \
          ' there is no test data. This *must* not work on live data!')
-    pend('This needs a standalone test. It does not actually have anything to' \
-         ' to do with the job test.')
     VCR.use_cassette(__method__) do
       assert_nothing_raised do
         require_relative '../mgmt/docker_cleanup.rb'
