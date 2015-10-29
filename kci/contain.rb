20eee22aea443b0fa246c4f061cff02dd089392f
Harald Sitter <sitter@kde.org>
2015-10-29 12:50:00 +0100

    remove TYPE access. we don't use it anymore

--- kci/contain.rb
+++ kci/contain.rb
@@ -5,7 +5,6 @@ require_relative '../lib/ci/containment'
 Docker.options[:read_timeout] = 4 * 60 * 60 # 4 hours.
 
 DIST = ENV.fetch('DIST')
-TYPE = ENV.fetch('TYPE')
 JOB_NAME = ENV.fetch('JOB_NAME')
 
 c = CI::Containment.new(JOB_NAME, image: CI::PangeaImage.new(:ubuntu, DIST))
