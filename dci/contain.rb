aa689d05ba08a70621f55973460866a9dc7e0bbd
Rohan Garg <rohan@garg.io>
2015-11-02 18:25:15 +0100

    Sanitize BUILD_TAG before storing

--- dci/contain.rb
+++ dci/contain.rb
@@ -5,8 +5,12 @@ require_relative '../lib/ci/containment'
 Docker.options[:read_timeout] = 4 * 60 * 60 # 4 hours.
 
 DIST = ENV.fetch('DIST')
-BUILD_TAG = ENV.fetch('BUILD_TAG')
 
-c = CI::Containment.new(BUILD_TAG, image: CI::PangeaImage.new(:debian, DIST))
+# This is required since some of the build tags have a invalid Docker
+# container name inside.
+# We can simply replace those characters with a valid char
+BUILD_TAG = ENV.fetch('BUILD_TAG').gsub(/[^a-zA-Z0-9._-]/, '-')
+
+c = CI::Containment.new(image: CI::PangeaImage.new(:debian, DIST))
 status_code = c.run(Cmd: ARGV)
 exit status_code
