2e41fb723bda5a46b701448fda2c2e1086c42258
Rohan Garg <rohan@garg.io>
2015-11-03 13:38:54 +0100

    Update test_parse

--- test/test_parse.rb
+++ test/test_parse.rb
@@ -1,31 +1,5 @@
 require 'test/unit'
-
-class Shebang
-  attr_reader :valid
-  attr_reader :parser
-
-  def initialize(line)
-    @valid = false
-    @parser = nil
-
-    return unless line
-    return unless line.start_with?('#!')
-
-    parts = line.split(' ')
-
-    return unless parts.size >= 1
-    if parts[0].end_with?('/env')
-      return unless parts.size >= 2
-      @parser = parts[1]
-    elsif !parts[0].include?('/') || parts[0].end_with?('/')
-      return # invalid
-    else
-      @parser = parts[0].split('/').pop
-    end
-
-    @valid = true
-  end
-end
+require_relative '../ci-tooling/test/lib/shebang'
 
 class ParseTest < Test::Unit::TestCase
   def test_shebang
