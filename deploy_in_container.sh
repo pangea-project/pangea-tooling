95ee467ae4c00621d3d1a56fcdd754802493b4bb
Harald Sitter <sitter@kde.org>
2015-11-11 09:44:38 +0100

    style++

--- deploy_in_container.sh
+++ deploy_in_container.sh
@@ -17,11 +17,12 @@ echo 'APT::Color "1";' > /etc/apt/apt.conf.d/99color
 
 apt-get update
 
+ESSENTIAL_PACKAGES="rake ruby ruby-dev build-essential zlib1g-dev"
 i="5"
-
 while [ $i -gt 0 ]; do
-  apt-get -y -o APT::Get::force-yes=true -o Debug::pkgProblemResolver=true install rake ruby ruby-dev build-essential zlib1g-dev && break
-i=$((i-1))
+  apt-get -y -o APT::Get::force-yes=true -o Debug::pkgProblemResolver=true \
+    install ${ESSENTIAL_PACKAGES} && break
+  i=$((i-1))
 done
 
 cd $SCRIPTDIR
