ac47f4ec47c2689789b7494aa88112892fe5712f
Rohan Garg <rohan@garg.io>
2015-11-19 20:35:44 +0100

    Update contain.rb

--- dci/contain.rb
+++ dci/contain.rb
@@ -6,11 +6,6 @@ Docker.options[:read_timeout] = 4 * 60 * 60 # 4 hours.
 
 DIST = ENV.fetch('DIST')
 
-# This is required since some of the build tags have a invalid Docker
-# container name inside.
-# We can simply replace those characters with a valid char
-BUILD_TAG = ENV.fetch('BUILD_TAG').gsub(/\W/, '-')
-
 c = CI::Containment.new(BUILD_TAG, image: CI::PangeaImage.new(:debian, DIST))
 status_code = c.run(Cmd: ARGV)
 exit status_code
