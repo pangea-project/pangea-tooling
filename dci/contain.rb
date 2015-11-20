38270f2128de166dfbfebdd5a12cffdd11addcda
Rohan Garg <rohan@garg.io>
2015-11-20 01:11:02 +0100

    Build tag fixes and update upload target

--- dci/contain.rb
+++ dci/contain.rb
@@ -5,6 +5,7 @@ require_relative '../lib/ci/containment'
 Docker.options[:read_timeout] = 4 * 60 * 60 # 4 hours.
 
 DIST = ENV.fetch('DIST')
+BUILD_TAG = ENV.fetch('BUILD_TAG')
 
 c = CI::Containment.new(BUILD_TAG, image: CI::PangeaImage.new(:debian, DIST))
 status_code = c.run(Cmd: ARGV)
--- dci/priv_contain.rb
+++ dci/priv_contain.rb
@@ -6,10 +6,7 @@ Docker.options[:read_timeout] = 4 * 60 * 60 # 4 hours.
 
 DIST = ENV.fetch('DIST')
 
-# This is required since some of the build tags have a invalid Docker
-# container name inside.
-# We can simply replace those characters with a valid char
-BUILD_TAG = ENV.fetch('BUILD_TAG').gsub(/\W/, '-')
+BUILD_TAG = ENV.fetch('BUILD_TAG')
 
 c = CI::Containment.new(BUILD_TAG,
                         image: CI::PangeaImage.new(:debian, DIST),
