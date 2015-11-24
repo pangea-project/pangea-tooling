71e17f386b8b9ba39f7c0e58fd7556b4cde62947
Bhushan Shah <bhush94@gmail.com>
2015-11-24 21:03:21 +0530

    double escape

--- kci/imager/mobster-config-hooks/20_package_list.sh
+++ kci/imager/mobster-config-hooks/20_package_list.sh
@@ -35,5 +35,5 @@ EOF
 cat << EOF > config/package-lists/livecd-rootfs.list.chroot_live
 lupin-casper
 linux-signed-generic
-!chroot chroot apt-cache dumpavail | grep-dctrl -nsPackage \\( -XFArchitecture amd64 -o -XFArchitecture all \\) -a -wFTask kubuntu-live | sed '/fcitx/d'
+!chroot chroot apt-cache dumpavail | grep-dctrl -nsPackage \\\\( -XFArchitecture amd64 -o -XFArchitecture all \\\\) -a -wFTask kubuntu-live | sed '/fcitx/d'
 EOF
