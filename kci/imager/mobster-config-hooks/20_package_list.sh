ae774bd41ce2502c50f44668c1bb3cac081af02c
Bhushan Shah <bhush94@gmail.com>
2015-11-24 17:50:49 +0530

    remove all of fcitx

--- kci/imager/mobster-config-hooks/20_package_list.sh
+++ kci/imager/mobster-config-hooks/20_package_list.sh
@@ -31,3 +31,9 @@ vlc
 gnome-chess
 firefox
 EOF
+
+cat << EOF > config/package-lists/livecd-rootfs.list.chroot_live
+lupin-casper
+linux-signed-generic
+!chroot chroot apt-cache dumpavail | grep-dctrl -nsPackage \\( -XFArchitecture amd64 -o -XFArchitecture all \\) -a -wFTask kubuntu-live | sed '/fcitx/d'
+EOF
