dd18c02a869406c074e8c6e1bf77aa2e43f5a687
Rohan Garg <rohan@garg.io>
2015-12-10 00:08:50 +0100

    Make monitoring more generic

--- mgmt/monitor_provisioning.rb
+++ mgmt/monitor_provisioning.rb
@@ -2,11 +2,13 @@
 
 require 'jenkins_api_client'
 
+JOB_NAME = ENV.fetch('JOB_NAME')
+
 client = JenkinsApi::Client.new(server_ip: 'mobile.kci.pangea.pub',
                                 server_port: 8080)
 
-while client.queue.list.include?('mgmt_test') ||
-      client.job.status('mgmt_test') == 'running'
+while client.queue.list.include?(JOB_NAME) ||
+      client.job.status(JOB_NAME) == 'running'
   puts 'Waiting for deployment to finish'
   sleep 10
 end
