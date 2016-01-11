64fa582944af81277ed8920c3c2a413fefcff9d4
Harald Sitter <sitter@kde.org>
2016-01-11 14:02:04 +0100

    more aggressive seed cleanup
    - kde-l10n-engb is marked as breaks/conflicts by plasma-desktop but we
      have no new enough version to resolve this, so simply dont seed it
    - fctix wants qt5, uses private abi, so it's broken by our newer api
    - k3b-i18n is useless as we are not seeding k3b

--- nci/imager/config-hooks/20_package_list.sh
+++ nci/imager/config-hooks/20_package_list.sh
@@ -1,5 +1,5 @@
 cat << EOF > config/package-lists/livecd-rootfs.list.chroot_live
 lupin-casper
 linux-signed-generic
-!chroot chroot apt-cache dumpavail | grep-dctrl -nsPackage \\\\( -XFArchitecture amd64 -o -XFArchitecture all \\\\) -a -wFTask kubuntu-live | sed '/kde-l10n/d'
+!chroot chroot apt-cache dumpavail | grep-dctrl -nsPackage \\\\( -XFArchitecture amd64 -o -XFArchitecture all \\\\) -a -wFTask kubuntu-live | sed '/fcitx/d' | sed '/kde-l10n/d' | sed '/k3b-i18n/d'
 EOF
