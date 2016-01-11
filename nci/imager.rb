3bc74b2e97e51a3d52c43b97cd099d2a5bb71260
Jonathan Riddell <jr@jriddell.org>
2016-01-11 15:55:50 +0000

    add header file

--- nci/imager.rb
+++ nci/imager.rb
@@ -27,7 +27,7 @@ status_code = c.run(Cmd: cmd)
 exit status_code unless status_code == 0
 
 DATE = File.read('result/date_stamp').strip
-PUB_PATH = "/var/www/nci/images/#{ARCH}/#{DATE}"
+PUB_PATH = "/home/nci/images/unstable-proposed/#{DATE}"
 FileUtils.mkpath(PUB_PATH)
 %w(iso manifest zsync).each do |type|
   unless system("cp -r --no-preserve=ownership result/*.#{type} #{PUB_PATH}/")
@@ -35,13 +35,7 @@ FileUtils.mkpath(PUB_PATH)
   end
 end
 FileUtils.chown_R('jenkins', 'www-data', PUB_PATH, verbose: true)
-unless system("cp -avr #{PUB_PATH} /mnt/s3/nci/images/#{ARCH}/")
-  abort 'Failed to copy to s3 bucket.'
-end
 
-generate_html_cmd = ["#{__dir__}/../generate_html.rb"]
-generate_html_cmd << '-o' << '/mnt/s3/nci/index.html'
-generate_html_cmd << 'nci'
-system(*generate_html_cmd) # Ignore return value as this is not too important.
+system("cp -r #{TOOLING_PATH}/nci/HEADER.html #{PUB_PATH}/")
 
 exit 0
