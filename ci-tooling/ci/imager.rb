01fd0adf0c5072c38ba881aecd8db19eb8d2cd32
Rohan Garg <rohan@garg.io>
2016-02-04 18:03:11 +0100

    Workaround broken debootstrap on debian

--- ci-tooling/ci/imager.rb
+++ ci-tooling/ci/imager.rb
@@ -2,6 +2,7 @@
 
 require_relative '../lib/apt'
 require_relative '../lib/retry'
+require_relative '../lib/os'
 require 'fileutils'
 
 fail 'No live-config found!' unless File.exist?('live-config')
@@ -11,6 +12,12 @@ Retry.retry_it(times: 5) do
   fail 'Apt upgrade failed' unless Apt.dist_upgrade
   fail 'Apt install failed' unless Apt.install(%w(aptitude))
   fail 'Aptitude install failed' unless system('aptitude install qemu-user-static live-build live-images')
+end
+
+# Workaround a broken debootstrap on Debian
+if OS::ID == 'debian'
+  system('wget -P /tmp ftp://ftp.debian.org/debian/pool/main/d/debootstrap/debootstrap_1.0.73~bpo8+1_all.deb')
+  system('dpkg -i /tmp/debootstrap_1.0.73~bpo8+1_all.deb')
 end
 
 begin
