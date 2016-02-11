d70e967c7cff9d5c2443b8df0b33dc0448f00d38
Rohan Garg <rohan@garg.io>
2016-02-12 00:17:23 +0100

    Rubocop style fixes

--- ci-tooling/ci/imager.rb
+++ ci-tooling/ci/imager.rb
@@ -5,13 +5,14 @@ require_relative '../lib/retry'
 require_relative '../lib/os'
 require 'fileutils'
 
-fail 'No live-config found!' unless File.exist?('live-config')
+raise 'No live-config found!' unless File.exist?('live-config')
 
 Retry.retry_it(times: 5) do
-  fail 'Apt update failed' unless Apt.update
-  fail 'Apt upgrade failed' unless Apt.dist_upgrade
-  fail 'Apt install failed' unless Apt.install(%w(aptitude))
-  fail 'Aptitude install failed' unless system('yes Y | aptitude -y install qemu-user-static live-build live-images')
+  raise 'Apt update failed' unless Apt.update
+  raise 'Apt upgrade failed' unless Apt.dist_upgrade
+  raise 'Apt install failed' unless Apt.install(%w(qemu-user-static
+                                                   live-build
+                                                   live-images))
 end
 
 # Workaround a broken debootstrap on Debian
@@ -25,7 +26,7 @@ begin
   Dir.chdir('live-config') do
     system('make clean')
     system('./configure')
-    fail unless system('make')
+    raise unless system('make')
     FileUtils.mv(Dir.glob('live*'), '../result', verbose: true)
     FileUtils.mv(Dir.glob('logfile*'), '../result', verbose: true)
   end
