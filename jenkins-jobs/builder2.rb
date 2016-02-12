a90b5fc0fb859ff7ed4a36516c64ec5429c87819
Harald Sitter <sitter@kde.org>
2016-02-12 15:39:28 +0100

    sync builder2 with nci branch

--- jenkins-jobs/builder2.rb
+++ jenkins-jobs/builder2.rb
@@ -3,21 +3,37 @@ require_relative 'multijob_phase'
 
 # Magic builder to create an array of build steps
 class Builder2 < JenkinsJob
-  def self.job(*args)
-    jobs = Builder.job(*args)
+  def self.job(*args, **kwords)
+    project = args[0]
+    dependees = project.dependees.collect do |d|
+      Builder.basename(kwords[:distribution], kwords[:type], project.component, d)
+    end
+    dependees.compact!
+    dependees.uniq!
+    dependees.sort!
+    project.dependees.clear
+
+    jobs = Builder.job(*args, kwords)
+    jobs.each do |j|
+      # Disable downstream triggers to prevent jobs linking to one another
+      # outside the phases.
+      j.send(:instance_variable_set, :@downstream_triggers, [])
+    end
     basename = jobs[0].job_name.rpartition('_')[0]
     puts basename
 
-    require 'ap'
-    ap jobs
-    jobs << new(basename, jobs: jobs.collect(&:job_name))
-    ap jobs
+    jobs << new(basename, jobs: jobs.collect(&:job_name), dependees: dependees)
     jobs
   end
 
-  def initialize(basename, jobs:)
+  # @! attribute [r] dependees
+  #   @return [Array<String>] name of jobs depending on this job
+  attr_reader :dependees
+
+  def initialize(basename, jobs:, dependees: [])
     super(basename, 'builder2.xml.erb')
     @jobs = jobs
+    @dependees = dependees
   end
 
   def render_phases
