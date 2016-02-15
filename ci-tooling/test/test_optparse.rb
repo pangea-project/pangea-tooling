92f4c1c75439096e290ef4b1f3b220135d95009f
Harald Sitter <sitter@kde.org>
2016-02-15 09:56:23 +0100

    add an option parser that helps with handling missing arguments

---
+++ ci-tooling/test/test_optparse.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+#
+# Copyright (C) 2015-2016 Harald Sitter <sitter@kde.org>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+require_relative '../lib/optparse'
+require_relative 'lib/testcase'
+
+class OptionParserTest < TestCase
+  def test_init
+    OptionParser.new do |opts|
+      opts.on('-l', '--long LONG', 'expected long', 'EXPECTED') do |v|
+      end
+    end
+  end
+
+  def test_present
+    parser = OptionParser.new do |opts|
+      opts.default_argv = %w(--long ABC)
+      opts.on('-l', '--long LONG', 'expected long', 'EXPECTED') do |v|
+      end
+    end
+    parser.parse!
+    assert_equal([], parser.missing_expected)
+  end
+
+  def test_missing
+    parser = OptionParser.new do |opts|
+      opts.default_argv = %w()
+      opts.on('-l', '--long LONG', 'expected long', 'EXPECTED') do |v|
+      end
+    end
+    parser.parse!
+    assert_equal(['--long'], parser.missing_expected)
+  end
+end
