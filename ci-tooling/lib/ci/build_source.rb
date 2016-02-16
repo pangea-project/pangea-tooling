702ae330e187f42e7e01030a021d5e5a359b30b7
Harald Sitter <sitter@kde.org>
2016-02-16 08:29:25 +0100

    literal freeze and license

--- ci-tooling/lib/ci/build_source.rb
+++ ci-tooling/lib/ci/build_source.rb
@@ -1,3 +1,21 @@
+# frozen_string_literal: true
+#
+# Copyright (C) 2015 Rohan Garg <rohan@garg.io>
+# Copyright (C) 2015-2016 Harald Sitter <sitter@kde.org>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
 require 'date'
 require 'fileutils'
 require 'yaml'
