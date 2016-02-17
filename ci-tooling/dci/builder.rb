843296a1567dfe6d1dd6f8cfeebde319e1511529
Rohan Garg <rohan@garg.io>
2016-02-17 21:09:29 +0100

    Add the right repos

--- ci-tooling/dci/builder.rb
+++ ci-tooling/dci/builder.rb
@@ -3,9 +3,14 @@
 require_relative '../lib/ci/build_binary'
 require_relative '../lib/apt'
 
+
+dist = ENV.fetch('DIST')
+repos = %w(frameworks plasma odroid netrunner)
+repos += %w(qt5) if dist == 'stable'
+
 # FIXME: Fix repo addition
-%w(frameworks plasma odroid netrunner).each do |repo|
-  Apt::Repository.add("deb http://dci.ds9.pub:8080/#{repo}/ unstable main")
+repos.each do |repo|
+  Apt::Repository.add("deb http://dci.ds9.pub:8080/#{repo}/ #{dist} main")
 end
 
 Apt::Key.add("#{__dir__}/dci_apt.key")
