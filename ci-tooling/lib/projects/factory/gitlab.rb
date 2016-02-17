d5cc45fed39d492d13b2f1fad186bae3b40ae5c8
Rohan Garg <rohan@garg.io>
2016-02-17 15:05:38 +0100

    Make sure we require bundler/setup before gitlab since we build from Git

--- ci-tooling/lib/projects/factory/gitlab.rb
+++ ci-tooling/lib/projects/factory/gitlab.rb
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
+require 'bundler/setup'
 require 'gitlab'
 
 require_relative 'base'
