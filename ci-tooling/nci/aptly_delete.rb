5448308b557ec3cef98bf82d2bcb2cd792a2bf4d
Harald Sitter <sitter@kde.org>
2016-02-18 17:07:35 +0100

    add aptly delete helper (forked out of expunge)

---
+++ ci-tooling/nci/aptly_delete.rb
@@ -0,0 +1,60 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+#
+# Copyright (C) 2016 Harald Sitter <sitter@kde.org>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+require 'aptly'
+require 'logger'
+require 'logger/colors'
+require 'net/ssh/gateway'
+
+options = OpenStruct.new
+parser = OptionParser.new do |opts|
+  opts.banner = "Usage: #{opts.program_name} SOURCENAME"
+end
+parser.parse!
+
+abort parser.help unless ARGV[0]
+options.name = ARGV[0]
+
+log = Logger.new(STDOUT)
+log.level = Logger::DEBUG
+log.progname = $PROGRAM_NAME
+
+# SSH tunnel so we can talk to the repo
+gateway = Net::SSH::Gateway.new('drax', 'root')
+gateway.open('localhost', 9090, 9090)
+
+Aptly.configure do |config|
+  config.host = 'localhost'
+  config.port = 9090
+end
+
+log.info 'APTLY'
+Aptly::Repository.list.each do |repo|
+  # next unless options.types.include?(repo.Name)
+
+  # Query all relevant packages.
+  # Any package with source and any architecture (not arch:source)
+  query = "($Source (#{options.name}), $Architecture (any))"
+  # Or the source itself
+  query += " | #{options.name} {source}"
+  packages = repo.packages(q: query)
+  next if packages.empty?
+
+  log.info "Deleting packages from repo #{repo.Name}: #{packages}"
+  repo.delete_packages(packages)
+end
