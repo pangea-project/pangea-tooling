8fc9dcc5e82f86ca1c949d4e43bf02121fc12c00
Harald Sitter <sitter@kde.org>
2016-02-18 11:40:24 +0100

    make sure we do not yield out of serve before we can connect to the server

--- ci-tooling/test/lib/serve.rb
+++ ci-tooling/test/lib/serve.rb
@@ -1,5 +1,7 @@
 require 'webrick'
 
+require_relative '../../lib/retry'
+
 module Test
   @children = []
 
@@ -7,6 +9,15 @@ module Test
   # 9474 KCI::OrigSourcerTestCase
   # 9475 TarFetcherTest
   # 111999 PangeaDPutTest
+
+  def self.wait_for_connect(port:)
+    # 5 times every second try to open a connection to our child.
+    # Leave error raising if this fails as we then failed to fire up our
+    # serving test.
+    Retry.retry_it(times: 5, sleep: 1, errors: [Errno::ECONNREFUSED]) do
+      Net::HTTP.start('localhost', port) {}
+    end
+  end
 
   def self.http_serve(dir, port: '0')
     case pid = fork
@@ -21,6 +32,7 @@ module Test
     else # parent
       @children << pid
       at_exit { nuke } # Make sure the child dies even on raised error exits.
+      wait_for_connect(port: port)
       yield
     end
   ensure
