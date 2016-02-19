f772b3b14418cab37739ebf5dc9580d634ea90dd
Harald Sitter <sitter@kde.org>
2016-02-19 14:55:45 +0100

    refactor setup_repo a bit to make it style compliant

--- ci-tooling/nci/lib/setup_repo.rb
+++ ci-tooling/nci/lib/setup_repo.rb
@@ -7,15 +7,20 @@ require_relative '../../lib/lsb'
 module NCI
   module_function
 
-  def setup_repo!
-    debline = "deb http://archive.neon.kde.org.uk/unstable #{LSB::DISTRIB_CODENAME} main"
-    abort 'adding repo failed' unless Apt::Repository.add(debline)
-    # FIXME: this needs to be in the module!
+  def setup_repo_key!
+    # FIXME: this needs to be in the apt module!
     IO.popen(['apt-key', 'add', '-'], 'w') do |io|
       io.puts open('http://archive.neon.kde.org.uk/public.key').read
       io.close_write
       puts io
     end
+  end
+
+  def setup_repo!
+    debline = format('deb http://archive.neon.kde.org.uk/unstable %s main',
+                     LSB::DISTRIB_CODENAME)
+    abort 'adding repo failed' unless Apt::Repository.add(debline)
+    setup_repo_key!
     abort 'Failed to import key' unless $? == 0
     Retry.retry_it(times: 5, sleep: 2) { raise unless Apt.update }
     abort 'failed to install deps' unless Apt.install(%w(pkg-kde-tools))
