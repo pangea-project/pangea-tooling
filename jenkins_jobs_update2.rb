982eda5dfbdad6048593f978c5dbf8e69ec43a53
Harald Sitter <sitter@kde.org>
2016-02-22 09:57:00 +0100

    base updater2 on common base

--- jenkins_jobs_update2.rb
+++ jenkins_jobs_update2.rb
@@ -3,6 +3,7 @@
 require_relative 'ci-tooling/lib/nci'
 require_relative 'ci-tooling/lib/projects'
 require_relative 'ci-tooling/lib/thread_pool'
+require_relative 'lib/jenkins/project_updater'
 
 Dir.glob(File.expand_path('jenkins-jobs/*.rb', __dir__)).each do |file|
   require file
@@ -13,7 +14,7 @@ Dir.glob(File.expand_path('jenkins-jobs/nci/*.rb', __dir__)).each do |file|
 end
 
 # Updates Jenkins Projects
-class ProjectUpdater
+class ProjectUpdater < Jenkins::ProjectUpdater
   def initialize
     @job_queue = Queue.new
     @flavor = 'nci'
@@ -21,52 +22,12 @@ class ProjectUpdater
       "#{File.expand_path(File.dirname(__FILE__))}/jenkins-jobs/#{@flavor}"
   end
 
-  def update
-    populate_queue
-    run_queue
-  end
-
-  def plugins_to_install
-    plugins = []
-    installed_plugins = Jenkins.plugin_manager.list_installed.keys
-    Dir.glob('jenkins-jobs/templates/**/**.xml.erb').each do |path|
-      next if path.include?('/dci/') || path.include?('/mci/')
-      File.readlines(path).each do |line|
-        match = line.match(/.*plugin="(.+)".*/)
-        next unless match && match.size == 2
-        plugin = match[1].split('@').first
-        next if installed_plugins.include?(plugin)
-        plugins << plugin
-      end
-    end
-    plugins.uniq.compact
-  end
-
-  def install_plugins
-    # Autoinstall all possibly used plugins.
-    installed_plugins = Jenkins.plugin_manager.list_installed.keys
-    plugins_to_install.each do |plugin|
-      next if installed_plugins.include?(plugin)
-      puts "--- Installing #{plugin} ---"
-      Jenkins.plugin_manager.install(plugin)
-    end
+  def all_template_files
+    files = super
+    files + Dir.glob("#{JenkinsJob.flavor_dir}/templates/**.xml.erb")
   end
 
   private
-
-  def enqueue(obj)
-    @job_queue << obj
-    obj
-  end
-
-  def run_queue
-    BlockingThreadPool.run do
-      until @job_queue.empty?
-        job = @job_queue.pop(true)
-        job.update
-      end
-    end
-  end
 
   def populate_queue
     NCI.series.each_key do |distribution|
