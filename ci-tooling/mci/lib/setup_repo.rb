064324cf43db6635aed0b2d14060520811fe4c9e
Bhushan Shah <bhush94@gmail.com>
2016-02-23 15:30:36 +0530

    Switch to new binarier for mci

---
+++ ci-tooling/mci/lib/setup_repo.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+#
+# Copyright (C) 2016 Harald Sitter <sitter@kde.org>
+# Copyright (C) 2016 Bhushan Shah <bshah@kde.org>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) version 3, or any
+# later version accepted by the membership of KDE e.V. (or its
+# successor approved by the membership of KDE e.V.), which shall
+# act as a proxy defined in Section 6 of version 3 of the license.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+require 'open-uri'
+
+require_relative '../../lib/apt'
+require_relative '../../lib/dpkg'
+require_relative '../../lib/lsb'
+require_relative '../../lib/retry'
+
+# Mobile CI specific helpers.
+module MCI
+  module_function
+
+  def setup_repo_key!
+    # FIXME: this needs to be in the apt module!
+    IO.popen(['apt-key', 'add', '-'], 'w') do |io|
+      io.puts open('http://mobile.neon.pangea.pub/Pangea%20CI.gpg.key').read
+      io.close_write
+    end
+  end
+
+  def setup_repo!
+    debline = format('deb http://mobile.neon.pangea.pub %s main',
+                     LSB::DISTRIB_CODENAME)
+    raise 'adding repo failed' unless Apt::Repository.add(debline)
+    setup_repo_key!
+    raise 'Failed to import key' unless $? == 0
+
+    srcline = 'deb %<host>s %<series>s %<pockets>s'
+    debline2 = format(srcline,
+                      host: if DPKG::BUILD_ARCH == 'armhf'
+                              'http://ports.ubuntu.com/ubuntu-ports'
+                            else
+                              'http://archive.ubuntu.com/ubuntu'
+                            end,
+                      series: "#{LSB::DISTRIB_CODENAME}-backports",
+                      pockets: 'main restricted universe multiverse')
+    Apt::Repository.add(debline2)
+    Apt::Repository.add('ppa:plasma-phone/ppa')
+
+    Retry.retry_it(times: 5, sleep: 2) { raise unless Apt.update }
+    raise 'failed to install deps' unless Apt.install(%w(pkg-kde-tools))
+  end
+end
---
+++ ci-tooling/test/test_mci_setup_repo.rb
@@ -0,0 +1,85 @@
+# frozen_string_literal: true
+#
+# Copyright (C) 2016 Harald Sitter <sitter@kde.org>
+# Copyright (C) 2016 Bhushan Shah <bshah@kde.org>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) version 3, or any
+# later version accepted by the membership of KDE e.V. (or its
+# successor approved by the membership of KDE e.V.), which shall
+# act as a proxy defined in Section 6 of version 3 of the license.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+require_relative '../mci/lib/setup_repo'
+require_relative 'lib/testcase'
+
+require 'mocha/test_unit'
+require 'webmock/test_unit'
+
+class MCISetupRepoTest < TestCase
+  def setup
+    LSB.instance_variable_set(:@hash, DISTRIB_CODENAME: 'vivid')
+
+    # Reset caching.
+    Apt::Repository.send(:reset)
+    # Disable automatic update
+    Apt::Abstrapt.send(:instance_variable_set, :@last_update, Time.now)
+    # Make sure $? is fine before we start!
+    reset_child_status!
+    # Disable all system invocation.
+    Object.any_instance.expects(:`).never
+    Object.any_instance.expects(:system).never
+    # Disable all web (used for key).
+    WebMock.disable_net_connect!
+  end
+
+  def teardown
+    WebMock.allow_net_connect!
+    LSB.reset
+  end
+
+  def test_setup_repo
+    system_calls = [
+      ['apt-get', '-y', '-o', 'APT::Get::force-yes=true', '-o', 'Debug::pkgProblemResolver=true', 'install', 'software-properties-common'],
+      ['add-apt-repository', '-y', 'deb http://mobile.neon.pangea.pub vivid main'],
+      ['add-apt-repository', '-y', 'deb http://archive.ubuntu.com/ubuntu vivid-backports main restricted universe multiverse'],
+      ['add-apt-repository', '-y', 'ppa:plasma-phone/ppa'],
+      ['apt-get', '-y', '-o', 'APT::Get::force-yes=true', '-o', 'Debug::pkgProblemResolver=true', 'update'],
+      ['apt-get', '-y', '-o', 'APT::Get::force-yes=true', '-o', 'Debug::pkgProblemResolver=true', 'install', 'pkg-kde-tools']
+    ]
+
+    system_sequence = sequence('system-calls')
+    system_calls.each do |cmd|
+      Object.any_instance.expects(:system)
+            .with(*cmd)
+            .returns(true)
+            .in_sequence(system_sequence)
+    end
+
+    Object.any_instance.stubs(:`)
+          .with('dpkg-architecture -qDEB_BUILD_ARCH')
+          .returns('amd64')
+
+    key_catcher = StringIO.new
+    IO.expects(:popen)
+      .with(['apt-key', 'add', '-'], 'w')
+      .yields(key_catcher)
+      .returns(true)
+
+    stub_request(:get, 'http://mobile.neon.pangea.pub/Pangea%20CI.gpg.key')
+      .to_return(status: 200, body: 'abc')
+
+    MCI.setup_repo!
+
+    assert_equal("abc\n", key_catcher.string)
+  end
+end
