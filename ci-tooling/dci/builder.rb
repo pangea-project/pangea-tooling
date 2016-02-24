02f088b89c549d5c54f43feacf52da7a5e64686e
Rohan Garg <rohan@garg.io>
2016-02-24 17:18:15 +0100

    Fix builder

--- ci-tooling/dci/builder.rb
+++ ci-tooling/dci/builder.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/env ruby
 
 require_relative '../lib/ci/build_binary'
-require_relative 'lib/setup'
+require_relative 'lib/setup_repo'
 
+DCI.setup_repo!
 builder = CI::PackageBuilder.new
 builder.build
