a562037ae1297177c9cea30a3b4572a7c69c846b
Rohan Garg <rohan@garg.io>
2016-02-25 13:25:47 +0100

    No need to add another trailing / to gitlab's base url

--- ci-tooling/lib/projects/factory/gitlab.rb
+++ ci-tooling/lib/projects/factory/gitlab.rb
@@ -52,7 +52,7 @@ class ProjectsFactory
       default_params.merge(
         name: name,
         component: component,
-        url_base: "#{self.class.url_base}/"
+        url_base: self.class.url_base
       )
     end
 
