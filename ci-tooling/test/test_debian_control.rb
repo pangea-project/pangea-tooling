b7f6ad73808c5a6a11ad1d944557e974181ddaa8
Harald Sitter <sitter@kde.org>
2016-02-25 12:08:51 +0100

    allow debian/control to open files outside pwd

--- ci-tooling/test/test_debian_control.rb
+++ ci-tooling/test/test_debian_control.rb
@@ -1,10 +1,30 @@
+# frozen_string_literal: true
+#
+# Copyright (C) 2016 Harald Sitter <sitter@kde.org>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) version 3, or any
+# later version accepted by the membership of KDE e.V. (or its
+# successor approved by the membership of KDE e.V.), which shall
+# act as a proxy defined in Section 6 of version 3 of the license.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
 require_relative '../lib/debian/control'
 require_relative 'lib/testcase'
 
 # Test debian/source/format
 class DebianControlFormatTest < TestCase
   def setup
-    Dir.chdir(@datadir)
+    FileUtils.cp_r("#{@datadir}/.", Dir.pwd)
   end
 
   def test_parse
@@ -27,10 +47,10 @@ class DebianControlFormatTest < TestCase
     assert_nil(c.source.fetch('magic', nil))
   end
 
-  def test_no_build_deps
-    Dir.chdir(data)
-    c = DebianControl.new
+  def test_no_build_deps # Also tests !pwd opening
+    c = DebianControl.new(__method__)
     c.parse!
-    assert_equal(0, c.source.fetch('build-depends', []).size)
+    assert_equal(nil, c.source.fetch('build-depends', nil),
+                 "Found a build dep #{c.source.fetch('build-depends', nil)}")
   end
 end
