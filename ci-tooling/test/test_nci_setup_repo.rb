4b0dd0fa19a5fb58478e6133b3819bd226dd1440
Jonathan Riddell <jr@jriddell.org>
2016-03-07 15:42:48 +0000

    add pep8 pyflakes for ubiquity

--- ci-tooling/test/test_nci_setup_repo.rb
+++ ci-tooling/test/test_nci_setup_repo.rb
@@ -51,7 +51,7 @@ class NCISetupRepoTest < TestCase
       ['apt-get', '-y', '-o', 'APT::Get::force-yes=true', '-o', 'Debug::pkgProblemResolver=true', 'install', 'software-properties-common'],
       ['add-apt-repository', '-y', 'deb http://archive.neon.kde.org.uk/unstable vivid main'],
       ['apt-get', '-y', '-o', 'APT::Get::force-yes=true', '-o', 'Debug::pkgProblemResolver=true', 'update'],
-      ['apt-get', '-y', '-o', 'APT::Get::force-yes=true', '-o', 'Debug::pkgProblemResolver=true', 'install', 'pkg-kde-tools']
+      ['apt-get', '-y', '-o', 'APT::Get::force-yes=true', '-o', 'Debug::pkgProblemResolver=true', 'install', 'pkg-kde-tools pep8 pyflakes3']
     ]
 
     system_sequence = sequence('system-calls')
