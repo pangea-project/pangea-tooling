7993390a264f2f980c4ee9096e2c0336626526fd
Harald Sitter <sitter@kde.org>
2016-03-08 11:52:35 +0100

    style++ (freeze consts and line break)

--- nci/imager.rb
+++ nci/imager.rb
@@ -1,4 +1,24 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
+#
+# Copyright (C) 2015-2016 Harald Sitter <sitter@kde.org>
+# Copyright (C) 2016 Jonathan Riddell <jr@jriddell.org>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) version 3, or any
+# later version accepted by the membership of KDE e.V. (or its
+# successor approved by the membership of KDE e.V.), which shall
+# act as a proxy defined in Section 6 of version 3 of the license.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
 require 'fileutils'
 
@@ -24,13 +44,14 @@ c = CI::Containment.new(JOB_NAME,
                         image: CI::PangeaImage.new(:ubuntu, DIST),
                         binds: binds,
                         privileged: true)
-cmd = ["#{TOOLING_PATH}/nci/imager/build.sh", Dir.pwd, DIST, ARCH, TYPE, METAPACKAGE, IMAGENAME]
+cmd = ["#{TOOLING_PATH}/nci/imager/build.sh",
+       Dir.pwd, DIST, ARCH, TYPE, METAPACKAGE, IMAGENAME]
 status_code = c.run(Cmd: cmd)
 exit status_code unless status_code == 0
 
 DATE = File.read('result/date_stamp').strip
-WEBSITE_PATH = "/var/www/images/#{IMAGENAME}-#{TYPE}-proposed/"
-PUB_PATH = "#{WEBSITE_PATH}#{DATE}"
+WEBSITE_PATH = "/var/www/images/#{IMAGENAME}-#{TYPE}-proposed/".freeze
+PUB_PATH = "#{WEBSITE_PATH}#{DATE}".freeze
 FileUtils.mkpath(PUB_PATH)
 %w(iso manifest zsync).each do |type|
   unless system("cp -r --no-preserve=ownership result/*.#{type} #{PUB_PATH}/")
@@ -38,7 +59,7 @@ FileUtils.mkpath(PUB_PATH)
   end
 end
 
-FileUtils.rm("#{WEBSITE_PATH}current", :force => true)
+FileUtils.rm("#{WEBSITE_PATH}current", force: true)
 FileUtils.ln_s(PUB_PATH, "#{WEBSITE_PATH}current")
 
 exit 0
