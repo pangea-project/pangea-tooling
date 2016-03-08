13adee9cbffc14d6d8218b3dc35918cda5065344
Harald Sitter <sitter@kde.org>
2016-03-08 11:07:35 +0100

    allow params to be set for age and count when pruning

--- lib/jenkins/jobdir.rb
+++ lib/jenkins/jobdir.rb
@@ -1,3 +1,23 @@
+# frozen_string_literal: true
+#
+# Copyright (C) 2016 Harald Sitter <sitter@kde.org>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) version 3, or any
+# later version accepted by the membership of KDE e.V. (or its
+# successor approved by the membership of KDE e.V.), which shall
+# act as a proxy defined in Section 6 of version 3 of the license.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
 require 'fileutils'
 
 module Jenkins
@@ -25,7 +45,7 @@ module Jenkins
       abs_link == abs_file
     end
 
-    def self.prune(dir)
+    def self.prune(dir, min_count: 6, max_age: 14)
       buildsdir = "#{dir}/builds"
       return unless File.exist?(buildsdir)
       content = Dir.glob("#{buildsdir}/*")
@@ -45,8 +65,7 @@ module Jenkins
       content.reject! { |d| locked.include?(File.realpath(d)) }
 
       content.sort_by! { |c| File.basename(c).to_i }
-      content[0..-6].each do |d| # Always keep the last 6 builds.
-        max_age = 14
+      content[0..-min_count].each do |d| # Always keep the last N builds.
         log = "#{d}/log"
         archive = "#{d}/archive"
         if File.exist?(log)
--- mgmt/jenkins_prune.rb
+++ mgmt/jenkins_prune.rb
@@ -1,7 +1,54 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
+#
+# Copyright (C) 2016 Harald Sitter <sitter@kde.org>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) version 3, or any
+# later version accepted by the membership of KDE e.V. (or its
+# successor approved by the membership of KDE e.V.), which shall
+# act as a proxy defined in Section 6 of version 3 of the license.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+require 'optparse'
 
 require_relative '../lib/jenkins/jobdir.rb'
 
+options = {}
+
+parser = OptionParser.new do |opts|
+  opts.banner = <<EOF
+Usage: #{opts.program_name} --max-age INTEGER --min-count INTEGER
+
+Prunes all Jenkins job dirs it can find by removing both logs and archives
+EOF
+  opts.separator('')
+
+  opts.on('--max-age INTEGER',
+          'The maximum age (in days) of builds to keep.',
+          'This presents the upper limit.',
+          'Any build exceeding this age will be pruned') do |v|
+    options[:max_age] = v.to_i
+  end
+
+  opts.on('--min-count INTEGER',
+          'The minium amount of builds to keep.',
+          'This presents the lower limit of builds to keep.',
+          'Builds below this limit are also kept if they are too old') do |v|
+    options[:min_count] = v.to_i
+  end
+end
+parser.parse!
+
 Dir.glob("#{Dir.home}/jobs/*").each do |jobdir|
-  Jenkins::JobDir.prune(jobdir)
+  Jenkins::JobDir.prune(jobdir, options)
 end
