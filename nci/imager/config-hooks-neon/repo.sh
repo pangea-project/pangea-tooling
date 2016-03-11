e8861ae8600027d9807bd90d2cc43bf8f8115d10
Jonathan Riddell <jr@jriddell.org>
2016-03-11 14:35:38 +0000

    use .uk domain name

--- nci/imager/config-hooks-neon/repo.sh
+++ nci/imager/config-hooks-neon/repo.sh
@@ -1,6 +1,6 @@
 keyfile="/tmp/tmp.key"
 rm -rf $keyfile
-wget -O $keyfile "http://archive.neon.kde.org.uk/public.key"
+wget -O $keyfile "http://archive.neon.kde.org/public.key"
 gpg --no-default-keyring --primary-keyring config/archives/ubuntu-defaults.key --import $keyfile
-echo "deb http://archive.neon.kde.org.uk/unstable $SUITE main" >> config/archives/neon.list
-echo "deb-src http://archive.neon.kde.org.uk/unstable $SUITE main" >> config/archives/neon.list
+echo "deb http://archive.neon.kde.org/unstable $SUITE main" >> config/archives/neon.list
+echo "deb-src http://archive.neon.kde.org/unstable $SUITE main" >> config/archives/neon.list
