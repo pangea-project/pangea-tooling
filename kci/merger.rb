da6cc28b8d6e09f84c0043c2a21dc7fe0507393f
Harald Sitter <sitter@kde.org>
2016-03-11 11:32:34 +0100

    base kcimerger on a new base merger

--- kci/merger.rb
+++ kci/merger.rb
@@ -1,60 +1,35 @@
 #!/usr/bin/env ruby
-
-require 'git'
-require 'logger'
-require 'logger/colors'
+# frozen_string_literal: true
+#
+# Copyright (C) 2014-2016 Harald Sitter <sitter@kde.org>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) version 3, or any
+# later version accepted by the membership of KDE e.V. (or its
+# successor approved by the membership of KDE e.V.), which shall
+# act as a proxy defined in Section 6 of version 3 of the license.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
 require_relative '../ci-tooling/lib/kci'
-
-# Stdlib Logger. Monkey patch with factory methods.
-class Logger
-  def self.new_for_merger
-    l = Logger.new(STDOUT)
-    l.progname = 'merger'
-    l.level = Logger::INFO
-    l.formatter = proc { |severity, _datetime, progname, msg|
-      max_line = 80
-      white_space_count = 2
-      spacers = (max_line - msg.size - white_space_count) / 2
-      spacers = ' ' * spacers
-      if severity == 'ANY'
-        "\n\e[1m#{spacers} #{msg} #{spacers}\e[0m\n"
-      else
-        "[#{severity[0]}] #{progname}: #{msg}\n"
-      end
-    }
-    l
-  end
-
-  def self.new_for_git
-    l = Logger.new(STDOUT)
-    l.progname = 'git'
-    l.level = Logger::WARN
-    l
-  end
-end
+require_relative '../lib/merger'
 
 # Merger merges delpoyment branches into KCI integration branches and KCI
 # integration branches into one another.
-class KCIMerger
-  # Logger instance used by the Merger.
-  attr_reader :log
-
+class KCIMerger < Merger
   # Creates a new KCIMerger. Creates a logger, sets up dpkg-mergechangelogs and
   # opens Dir.pwd as a Git::Base.
   def initialize
-    @log = Logger.new_for_merger
-
-    # :nocov:
-    if File.exist?('/var/lib/jenkins/tooling3/git')
-      Git.configure { |c| c.binary_path = '/var/lib/jenkins/tooling3/git' }
-    end
-    # :nocov:
-    @git = Git.open(Dir.pwd, log: Logger.new_for_git)
-    @git.config('merge.dpkg-mergechangelogs.name',
-                'debian/changelog merge driver')
-    @git.config('merge.dpkg-mergechangelogs.driver',
-                'dpkg-mergechangelogs -m %O %A %B %A')
+    super
+    @git = @repo
     @push_pending = []
   end
 
@@ -162,8 +137,6 @@ class KCIMerger
     @git.reset(nil, hard: true)
     @git.gc
     @git.config('remote.origin.prune', true)
-    File.write('.gitattributes',
-               "debian/changelog merge=dpkg-mergechangelogs\n")
   end
 
   # Merges source into target and pushes the merge result.
@@ -203,15 +176,6 @@ class KCIMerger
     @log.info msg
     @git.merge(source.full, msg)
     @push_pending << target
-  end
-
-  def noci_merge?(source)
-    log = @git.log.between('', source.full)
-    return false unless log.size >= 1
-    log.each do |commit|
-      return false unless commit.message.include?('NOCI')
-    end
-    true
   end
 
   def push_all_pending
---
+++ lib/merger.rb
@@ -0,0 +1,128 @@
+# frozen_string_literal: true
+#
+# Copyright (C) 2014-2016 Harald Sitter <sitter@kde.org>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) version 3, or any
+# later version accepted by the membership of KDE e.V. (or its
+# successor approved by the membership of KDE e.V.), which shall
+# act as a proxy defined in Section 6 of version 3 of the license.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+require 'fileutils'
+require 'git'
+require 'logger'
+require 'logger/colors'
+require 'tmpdir'
+
+# Stdlib Logger. Monkey patch with factory methods.
+class Logger
+  def self.new_for_merger
+    l = Logger.new(STDOUT)
+    l.progname = 'merger'
+    l.level = Logger::INFO
+    l.formatter = proc { |severity, _datetime, progname, msg|
+      max_line = 80
+      white_space_count = 2
+      spacers = (max_line - msg.size - white_space_count) / 2
+      spacers = ' ' * spacers
+      if severity == 'ANY'
+        "\n\e[1m#{spacers} #{msg} #{spacers}\e[0m\n"
+      else
+        "[#{severity[0]}] #{progname}: #{msg}\n"
+      end
+    }
+    l
+  end
+
+  def self.new_for_git
+    l = Logger.new(STDOUT)
+    l.progname = 'git'
+    l.level = Logger::WARN
+    l
+  end
+end
+
+# A Merger.
+class Merger
+  class << self
+    # Workign directory used by merger.
+    attr_reader :workdir
+    # Logger instance used by the Merger.
+    attr_reader :log
+
+    def cleanup(workdir)
+      proc { FileUtils.remove_entry_secure(workdir) }
+    end
+
+    def static_init(instance)
+      @workdir = Dir.mktmpdir(to_s).freeze
+      # Workaround for Git::Base not correctly creating .git for index.lock.
+      FileUtils.mkpath("#{@workdir}/.git")
+      ObjectSpace.define_finalizer(instance, cleanup(@workdir))
+    end
+  end
+
+  # Creates a new Merger. Creates a logger, sets up dpkg-mergechangelogs and
+  # opens Dir.pwd as a Git::Base.
+  def initialize(repo_path = Dir.pwd)
+    self.class.static_init(self)
+
+    @log = Logger.new_for_merger
+
+    if File.exist?('/var/lib/jenkins/tooling3/git')
+      Git.configure { |c| c.binary_path = '/var/lib/jenkins/tooling3/git' }
+    end
+
+    @repo = open_repo(repo_path)
+    configure_repo(@repo)
+  end
+
+  def configure_repo(repo)
+    repo.config('merge.dpkg-mergechangelogs.name',
+                'debian/changelog merge driver')
+    repo.config('merge.dpkg-mergechangelogs.driver',
+                'dpkg-mergechangelogs -m %O %A %B %A')
+    repo_path = repo.repo.path
+    FileUtils.mkpath("#{repo_path}/info")
+    File.write("#{repo_path}/info/attributes",
+               "debian/changelog merge=dpkg-mergechangelogs\n")
+  end
+
+  def sequence(starting_point)
+    BranchSequence.new(starting_point, repo: @repo)
+  end
+
+  private
+
+  def open_repo(repo_path)
+    repo = Git.open(self.class.workdir,
+                    repository: repo_path,
+                    log: Logger.new_for_git)
+    repo.branches # Trigger an execution to possibly raise an error.
+    configure_repo(repo)
+    repo
+  rescue Git::GitExecuteError => e
+    raise e if repo_path.end_with?('.git', '.git/')
+    repo_path = "#{repo_path}/.git"
+    retry
+  end
+
+  def noci_merge?(source)
+    log = @git.log.between('', source.full)
+    return false unless log.size >= 1
+    log.each do |commit|
+      return false unless commit.message.include?('NOCI')
+    end
+    true
+  end
+end
