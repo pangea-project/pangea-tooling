2d78ae4c48ef65f1b889aa978947e8092d474bff
Harald Sitter <sitter@kde.org>
2016-03-17 17:26:17 +0100

    implement debian version compare
    based on dpkg --compare-versions because implementing from scratch while
    rather straight forward is an uncomfortable amount of gooey code
    
    seems decently fast enough to simply go through dpkg

--- ci-tooling/lib/debian/version.rb
+++ ci-tooling/lib/debian/version.rb
@@ -1,3 +1,23 @@
+# frozen_string_literal: true
+#
+# Copyright (C) 2015-2016 Harald Sitter <sitter@kde.org>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) version 3, or any
+# later version accepted by the membership of KDE e.V. (or its
+# successor approved by the membership of KDE e.V.), which shall
+# act as a proxy defined in Section 6 of version 3 of the license.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
 module Debian
   # A debian policy version handling class.
   class Version
@@ -18,7 +38,27 @@ module Debian
       @full
     end
 
+    # We could easily reimplement version comparision from Version.pm, but
+    # it's mighty ugh because of string components, so in order to not run into
+    # problems down the line, let's just consult with dpkg for now.
+    def <=>(other)
+      return 0 if full == other.full
+      return 1 if compare_version(full, 'gt', other.full)
+      return -1 if compare_version(full, 'lt', other.full)
+      # A version can be stringwise different but have the same weight.
+      # Make sure we cover that.
+      return 0 if compare_version(full, 'eq', other.full)
+    end
+
     private
+
+    def compare_version(ours, op, theirs)
+      run('--compare-versions', ours, op, theirs)
+    end
+
+    def run(*args)
+      system('dpkg', *args)
+    end
 
     def parse
       regex = /^(?:(?<epoch>\d+):)?
--- ci-tooling/test/test_debian_version.rb
+++ ci-tooling/test/test_debian_version.rb
@@ -3,6 +3,8 @@ require_relative 'lib/testcase'
 
 # Test debian version
 class DebianVersionTest < TestCase
+  required_binaries(%w(dpkg))
+
   def test_native
     s = '5.0'
     v = Debian::Version.new(s)
@@ -25,5 +27,58 @@ class DebianVersionTest < TestCase
     assert_equal('1', v.epoch)
     assert_equal('5.0', v.upstream)
     assert_equal('0ubuntu1', v.revision)
+  end
+
+  def assert_v_greater(a, b, message = nil)
+    message = build_message(message,
+                            '<?> is not greater than <?>.',
+                            a.full, b.full)
+    assert_block message do
+      1 == (a <=> b)
+    end
+  end
+
+  def assert_v_lower(a, b, message = nil)
+    message = build_message(message,
+                            '<?> is not lower than <?>.',
+                            a.full, b.full)
+    assert_block message do
+      -1 == (a <=> b)
+    end
+  end
+
+  def assert_v_equal(a, b, message = nil)
+    message = build_message(message,
+                            '<?> is not equal to <?>.',
+                            a.full, b.full)
+    assert_block message do
+      0 == (a <=> b)
+    end
+  end
+
+  def assert_v_flip(x, y)
+    assert_v_greater(x, y)
+    assert_v_lower(y, x)
+  end
+
+  def test_greater_and_lower
+    assert_v_flip(Debian::Version.new('1:0'), Debian::Version.new('1'))
+    assert_v_flip(Debian::Version.new('1.1'), Debian::Version.new('1'))
+    assert_v_flip(Debian::Version.new('1+'), Debian::Version.new('1'))
+    assert_v_flip(Debian::Version.new('1.1~'), Debian::Version.new('1'))
+    assert_v_flip(Debian::Version.new('2~'), Debian::Version.new('1'))
+    assert_v_flip(Debian::Version.new('1-1'), Debian::Version.new('1'))
+    assert_v_flip(Debian::Version.new('1-0.1'), Debian::Version.new('1'))
+    assert_v_flip(Debian::Version.new('1-0+'), Debian::Version.new('1'))
+    assert_v_flip(Debian::Version.new('1-1~'), Debian::Version.new('1'))
+    assert_v_flip(Debian::Version.new('0:1-0.'), Debian::Version.new('1'))
+  end
+
+  def test_equal
+    assert_v_equal(Debian::Version.new('1-0'), Debian::Version.new('1'))
+    assert_v_equal(Debian::Version.new('1'), Debian::Version.new('1'))
+    assert_v_equal(Debian::Version.new('0:1'), Debian::Version.new('1'))
+    assert_v_equal(Debian::Version.new('0:1-0'), Debian::Version.new('1'))
+    assert_v_equal(Debian::Version.new('0:1-0'), Debian::Version.new('1'))
   end
 end
