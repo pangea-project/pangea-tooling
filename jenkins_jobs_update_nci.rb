197d8fd4cbd0fc3613776b2fa4fda83428cbc78d
Jonathan Riddell <jr@jriddell.org>
2016-03-21 16:15:45 +0000

    rubocop compliance

--- jenkins_jobs_update_nci.rb
+++ jenkins_jobs_update_nci.rb
@@ -106,20 +106,20 @@ class ProjectUpdater < Jenkins::ProjectUpdater
         # ISOs
         NCI.architectures.each do |architecture|
           dev_unstable_isoargs = { type: 'dev-unstable',
-                      distribution: distribution,
-                      architecture: architecture,
-                      metapackage: 'neon-desktop',
-                      imagename: 'neon',
-                      neonarchive: 'unstable' }
+                                   distribution: distribution,
+                                   architecture: architecture,
+                                   metapackage: 'neon-desktop',
+                                   imagename: 'neon',
+                                   neonarchive: 'unstable' }
           enqueue(NeonIsoJob.new(dev_unstable_isoargs))
           dev_stable_isoargs = { type: 'dev-stable',
-                      distribution: distribution,
-                      architecture: architecture,
-                      metapackage: 'neon-desktop',
-                      imagename: 'neon',
-                      neonarchive: 'stable' }
+                                 distribution: distribution,
+                                 architecture: architecture,
+                                 metapackage: 'neon-desktop',
+                                 imagename: 'neon',
+                                 neonarchive: 'stable' }
           enqueue(NeonIsoJob.new(dev_stable_isoargs))
-          wayland_isoargs = { type: "unstable",
+          wayland_isoargs = { type: 'unstable',
                               distribution: distribution,
                               architecture: architecture,
                               metapackage: 'plasma-wayland-desktop',
