eea1fdb0ae6520e60423abce55b1e79d97ece59a
Harald Sitter <sitter@kde.org>
2016-03-23 12:32:55 +0100

    do not uscan in a tmpdir to enable builtin re-use ability of uscan

--- ci-tooling/lib/ci/tar_fetcher.rb
+++ ci-tooling/lib/ci/tar_fetcher.rb
@@ -1,3 +1,23 @@
+# frozen_string_literal: true
+#
+# Copyright (C) 2015-2016 Harald Sitter <sitter@kde.org>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) version 3, or any
+# later version accepted by the membership of KDE e.V. (or its
+# successor approved by the membership of KDE e.V.), which shall
+# act as a proxy defined in Section 6 of version 3 of the license.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
 require 'open-uri'
 require 'tmpdir'
 
@@ -17,21 +37,16 @@ module CI
     end
 
     def fetch(destdir)
-      Dir.mktmpdir do |tmpdir|
-        Dir.chdir(@dir) do
-          system('uscan',
-                 '--verbose',
-                 '--download-current-version',
-                 "--destdir=#{tmpdir}",
-                 '--rename')
-        end
-        tar = Dir.glob("#{tmpdir}/*.orig.tar*")
-        return nil unless tar.size == 1
-        tar = tar[0]
-        FileUtils.cp(tar, destdir)
-        return Tarball.new("#{destdir}/#{File.basename(tar)}")
-      end
-      nil
+      system('uscan',
+             '--verbose',
+             '--download-current-version',
+             "--destdir=#{destdir}",
+             '--rename',
+             chdir: @dir)
+      tar = Dir.glob("#{destdir}/*.orig.tar*")
+      return nil unless tar.size == 1
+      tar = tar[0]
+      Tarball.new("#{destdir}/#{File.basename(tar)}")
     end
   end
 
