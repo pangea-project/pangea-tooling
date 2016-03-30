ef7fd70f5785f43dc137e95558f8b3b68161add6
Harald Sitter <sitter@kde.org>
2016-03-30 09:58:42 +0200

    fix nci port

--- ci-tooling/nci/repo_console.rb
+++ ci-tooling/nci/repo_console.rb
@@ -23,7 +23,7 @@ require 'net/ssh/gateway'
 
 # SSH tunnel so we can talk to the repo
 gateway = Net::SSH::Gateway.new('drax', 'root')
-gateway_port = gateway.open('localhost', 8081)
+gateway_port = gateway.open('localhost', 9090)
 
 Aptly.configure do |config|
   config.host = 'localhost'
