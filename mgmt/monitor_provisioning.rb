23f8164fc0857142315bfae397a8b29a41e345ff
Rohan Garg <rohan@garg.io>
2016-04-05 19:16:29 +0200

    Update hostname

--- mgmt/monitor_provisioning.rb
+++ mgmt/monitor_provisioning.rb
@@ -4,7 +4,7 @@ require 'jenkins_api_client'
 
 JOB_NAME = ENV.fetch('JOB_NAME')
 
-client = JenkinsApi::Client.new(server_ip: 'mobile.kci.pangea.pub',
+client = JenkinsApi::Client.new(server_ip: 'mobile.neon.pangea.pub',
                                 server_port: 8080)
 
 while client.queue.list.include?(JOB_NAME) ||
