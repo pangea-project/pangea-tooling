6408f19454060df0928e9dd137a0db6c49189e51
Harald Sitter <sitter@kde.org>
2016-04-12 10:50:47 +0200

    allow resolver to only install arch binary dependencies only

--- ci-tooling/lib/ci/build_binary.rb
+++ ci-tooling/lib/ci/build_binary.rb
@@ -1,3 +1,24 @@
+# frozen_string_literal: true
+#
+# Copyright (C) 2015-2016 Harald Sitter <sitter@kde.org>
+# Copyright (C) 2015 Rohan Garg <rohan@kde.org>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) version 3, or any
+# later version accepted by the membership of KDE e.V. (or its
+# successor approved by the membership of KDE e.V.), which shall
+# act as a proxy defined in Section 6 of version 3 of the license.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
 require 'fileutils'
 
 require_relative 'source'
@@ -13,13 +34,16 @@ module CI
     class DependencyResolver
       RESOLVER_BIN = '/usr/lib/pbuilder/pbuilder-satisfydepends'.freeze
 
-      def self.resolve(dir)
+      def self.resolve(dir, bin_only: false)
         unless File.executable?(RESOLVER_BIN)
           raise "Can't find #{RESOLVER_BIN}!"
         end
 
         Retry.retry_it(times: 5) do
-          system('sudo', RESOLVER_BIN, '--control', "#{dir}/debian/control")
+          opts = []
+          opts << '--binary-arch' if bin_only
+          opts << '--control' << "#{dir}/debian/control"
+          system('sudo', RESOLVER_BIN, *opts)
           raise 'Failed to satisfy depends' unless $? == 0
         end
       end
--- ci-tooling/test/test_ci_build_binary.rb
+++ ci-tooling/test/test_ci_build_binary.rb
@@ -1,6 +1,29 @@
+# frozen_string_literal: true
+#
+# Copyright (C) 2016 Harald Sitter <sitter@kde.org>
+# Copyright (C) 2015 Rohan Garg <rohan@kde.org>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) version 3, or any
+# later version accepted by the membership of KDE e.V. (or its
+# successor approved by the membership of KDE e.V.), which shall
+# act as a proxy defined in Section 6 of version 3 of the license.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
 require_relative '../lib/ci/build_binary'
 require_relative '../lib/debian/changes'
 require_relative 'lib/testcase'
+
+require 'mocha/test_unit'
 
 # Test ci/build_binary
 module CI
@@ -20,6 +43,23 @@ module CI
       changes.parse!
       assert_equal(['hello_2.10-1_amd64.deb'],
                    changes.fields['files'].map(&:name))
+    end
+
+    def test_dep_resolve_bin_only
+      Object.any_instance.expects(:system).never
+
+      File.expects(:executable?)
+          .with(PackageBuilder::DependencyResolver::RESOLVER_BIN)
+          .returns(true)
+
+      Object.any_instance
+            .expects(:system)
+            .with('sudo', '/usr/lib/pbuilder/pbuilder-satisfydepends',
+                  '--binary-arch',
+                  '--control', "#{Dir.pwd}/debian/control")
+            .returns(true)
+
+      PackageBuilder::DependencyResolver.resolve(Dir.pwd, bin_only: true)
     end
   end
 end
