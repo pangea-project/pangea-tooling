d8219bab82c81a036871a131d77ca9a7a6dea03d
Bhushan Shah <bhush94@gmail.com>
2016-04-19 17:33:56 +0530

    Adapt mci sourcer to recent changes
    now takes argument about sourcer type, similar to what nci/dci does

--- ci-tooling/mci/sourcer.rb
+++ ci-tooling/mci/sourcer.rb
@@ -1,16 +1,53 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
+#
+# Copyright (C) 2016 Harald Sitter <sitter@kde.org>
+# Copyright (C) 2016 Rohan Garg <rohan@kde.org>
+# Copyright (C) 2016 Bhushan Shah <bshah@kde.org>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) version 3, or any
+# later version accepted by the membership of KDE e.V. (or its
+# successor approved by the membership of KDE e.V.), which shall
+# act as a proxy defined in Section 6 of version 3 of the license.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
 require_relative '../lib/ci/build_source'
-require_relative '../lib/apt'
+require_relative '../lib/ci/orig_source_builder'
+require_relative '../lib/ci/tar_fetcher'
+require_relative 'lib/setup_repo'
+require_relative 'lib/setup_env'
 
-DIST = ENV.fetch('DIST')
+MCI.setup_repo!
+MCI.setup_env!
 
-# Packages need a newer pkg-kde-tools which I've put in the mobile.kci archive, jr
-Apt::Repository.add("deb http://mobile.kci.pangea.pub #{DIST} main")
-Apt::Key.add("#{__dir__}/Pangea CI.gpg.key")
-Apt.update
-Apt.install(%w(pkg-kde-tools))
+def orig_source(fetcher)
+  tarball = fetcher.fetch('source')
+  raise 'Failed to fetch tarball' unless tarball
+  sourcer = CI::OrigSourceBuilder.new(release: ENV.fetch('DIST'),
+                                      strip_symbols: true)
+  sourcer.build(tarball.origify)
+end
 
-builder = CI::VcsSourceBuilder.new(release: DIST, strip_symbols: true)
-source = builder.run
-# Write out metadata
-File.write('build/source.yaml', YAML.dump(source))
+case ARGV.fetch(0, nil)
+when 'tarball'
+  puts 'Downloading tarball from URL'
+  orig_source(CI::URLTarFetcher.new(File.read('source/url').strip))
+when 'uscan'
+  puts 'Downloading tarball via uscan'
+  orig_source(CI::WatchTarFetcher.new('packaging/debian/watch'))
+else
+  puts 'Unspecified source type, defaulting to VCS build...'
+  builder = CI::VcsSourceBuilder.new(release: ENV.fetch('DIST'),
+                                     strip_symbols: true)
+  builder.run
+end
