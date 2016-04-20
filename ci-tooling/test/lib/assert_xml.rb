292dd3ddac8c21a6015067e7896c306cdd0bbd42
Harald Sitter <sitter@kde.org>
2016-04-20 12:17:28 +0200

    add some xml assertations based on old test_xml.rb

---
+++ ci-tooling/test/lib/assert_xml.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+#
+# Copyright (C) 2015-2016 Harald Sitter <sitter@kde.org>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) version 3, or any
+# later version accepted by the membership of KDE e.V. (or its
+# successor approved by the membership of KDE e.V.), which shall
+# act as a proxy defined in Section 6 of version 3 of the license.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+require 'equivalent-xml'
+
+module EquivalentXmlAssertations
+  def assert_xml_equal(expected, actual, **kwords)
+    diff = Test::Unit::Assertions::AssertionMessage.delayed_diff(expected,
+                                                                 actual)
+    msg = build_message('XML was expected to be equal but was not!',
+                        "EXPECTED:\n--\n<?>\n--\nACTUAL:\n--\n<?>\n--\n?",
+                        expected, actual, diff)
+    assert_block(msg) do
+      EquivalentXml.equivalent?(expected, actual, **kwords)
+    end
+  end
+end
