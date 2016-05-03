49313c62d649901829602747917190f2162211ce
Jonathan Riddell <jr@jriddell.org>
2016-05-03 14:58:52 +0100

    only copy source tar to local site

--- nci/imager.rb
+++ nci/imager.rb
@@ -57,7 +57,7 @@ DATE = File.read('result/date_stamp').strip
 WEBSITE_PATH = "/var/www/images/#{IMAGENAME}-#{TYPE}-proposed/".freeze
 PUB_PATH = "#{WEBSITE_PATH}#{DATE}".freeze
 FileUtils.mkpath(PUB_PATH)
-%w(iso manifest zsync sha256sum tar.xz).each do |type|
+%w(tar.xz).each do |type|
   unless system("cp -r --no-preserve=ownership result/*.#{type} #{PUB_PATH}/")
     abort "File type #{type} failed to copy to public directory."
   end
