cbbf5dc0c476015350fe9e7b51a1eb326987824c
Rohan Garg <rohan@garg.io>
2016-05-06 21:41:35 +0200

    Simplify symlinking logic, remove not required packages

--- ci-tooling/ci/imager.rb
+++ ci-tooling/ci/imager.rb
@@ -9,9 +9,7 @@ raise 'No live-config found!' unless File.exist?('live-config')
 Retry.retry_it(times: 5) do
   raise 'Apt update failed' unless Apt.update
   raise 'Apt upgrade failed' unless Apt.dist_upgrade
-  raise 'Apt install failed' unless Apt.install(%w(qemu-user-static
-                                                   live-build
-                                                   live-images))
+  raise 'Apt install failed' unless Apt.install(%w(live-build live-images))
 end
 
 FileUtils.mkdir_p 'result'
@@ -27,7 +25,6 @@ Dir.chdir('result') do
   @images = Dir.glob('*.{iso,tar}*')
   raise 'Build failed!' unless @images.size == 1
 
-  # Symlink in the result folder so that S3 gets a generic entry pointing to the latest tar or ISO
-  FileUtils.ln_s(@images[0], 'latest.iso', verbose: true) if @images[0].end_with? '.iso'
-  FileUtils.ln_s(@images[0], 'latest.tar', verbose: true) if @images[0].end_with? '.tar'
+  # Symlink in the result folder so that S3 gets a generic entry pointing to the latest image
+  FileUtils.ln_s(@images[0], 'latest', verbose: true)
 end
