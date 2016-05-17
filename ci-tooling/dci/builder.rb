23cb7991652f6d40f3985895ec6c94c731fe4a0b
Rohan Garg <rohan@garg.io>
2016-05-17 17:08:17 +0200

    Fixed path, increase verbosity

--- ci-tooling/dci/builder.rb
+++ ci-tooling/dci/builder.rb
@@ -8,7 +8,7 @@ require_relative 'lib/setup_repo'
 DCI.setup_repo!
 @workspace = Dir.pwd
 Dir.mktmpdir do |tmpdir|
-  FileUtils.cp_r("#{@workspace}/*", tmpdir)
+  FileUtils.cp_r("#{@workspace}/.", tmpdir, verbose: true)
   Dir.chdir(tmpdir) do
     builder = CI::PackageBuilder.new
     builder.build
