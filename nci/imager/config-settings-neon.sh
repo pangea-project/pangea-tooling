9bbec4379cd864bda66bf5cbfa56675eb983b491
Jonathan Riddell <jr@jriddell.org>
2016-05-17 15:25:35 +0100

    iso name too long, try using neonarchive for a shorter one

--- nci/imager/config-settings-neon.sh
+++ nci/imager/config-settings-neon.sh
@@ -1,2 +1,2 @@
-export LB_ISO_VOLUME="${IMAGENAME} ${TYPE} \$(date +%Y%m%d-%H:%M)"
+export LB_ISO_VOLUME="${IMAGENAME} ${NEONARCHIvE} \$(date +%Y%m%d-%H:%M)"
 export LB_ISO_APPLICATION="KDE neon Live"
