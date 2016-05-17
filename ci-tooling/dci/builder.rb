ddc6b6390db552158d0fc8c18894341fe7ec282e
Rohan Garg <rohan@garg.io>
2016-05-17 16:09:44 +0200

    Update builder to use a temp dir for building

--- ci-tooling/dci/builder.rb
+++ ci-tooling/dci/builder.rb
@@ -1,8 +1,13 @@
 #!/usr/bin/env ruby
+require 'tmpdir'
+require 'fileutils'
 
 require_relative '../lib/ci/build_binary'
 require_relative 'lib/setup_repo'
 
 DCI.setup_repo!
-builder = CI::PackageBuilder.new
-builder.build
+Dir.mktmpdir do |tmpdir|
+  builder = CI::PackageBuilder.new(tmpdir)
+  builder.build
+  FileUtils.cp_r("#{tmpdir}/result", Dir.pwd, verbose: true)
+end
