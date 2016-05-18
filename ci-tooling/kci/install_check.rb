20fdffc5aac397d6ae60966373a195626b2fda9d
Harald Sitter <sitter@kde.org>
2016-05-18 15:36:22 +0200

    start refactoring install_check to make it more generic
    start with adding a simple success test

--- ci-tooling/kci/install_check.rb
+++ ci-tooling/kci/install_check.rb
@@ -12,12 +12,6 @@ require_relative 'lib/lp'
 require_relative 'lib/retry'
 require_relative 'lib/thread_pool'
 
-LOG = Logger.new(STDERR)
-LOG.level = Logger::INFO
-
-Project = Struct.new(:series, :stability)
-project = Project.new(ENV.fetch('DIST'), ENV.fetch('TYPE'))
-
 # Helper to add/remove/list PPAs
 class CiPPA
   attr_reader :type
@@ -158,103 +152,121 @@ class CiPPA
   end
 end
 
-def install_fake_pkg(name)
-  Dir.mktmpdir do |tmpdir|
-    Dir.chdir(tmpdir) do
-      Dir.mkdir(name)
-      Dir.mkdir("#{name}/DEBIAN")
-      File.write("#{name}/DEBIAN/control", <<-EOF.gsub(/^\s+/, ''))
-      Package: #{name}
-      Version: 999:999
-      Architecture: all
-      Maintainer: Harald Sitter <sitter@kde.org>
-      Description: fake override package for kubuntu ci install checks
-      EOF
-      system("dpkg-deb -b #{name} #{name}.deb")
-      DPKG.dpkg(['-i', "#{name}.deb"])
+class InstallCheck
+  def initialize
+    @log = Logger.new(STDOUT)
+    @log.level = Logger::INFO
+  end
+
+  def install_fake_pkg(name)
+    Dir.mktmpdir do |tmpdir|
+      Dir.chdir(tmpdir) do
+        Dir.mkdir(name)
+        Dir.mkdir("#{name}/DEBIAN")
+        File.write("#{name}/DEBIAN/control", <<-EOF.gsub(/^\s+/, ''))
+        Package: #{name}
+        Version: 999:999
+        Architecture: all
+        Maintainer: Harald Sitter <sitter@kde.org>
+        Description: fake override package for kubuntu ci install checks
+        EOF
+        system("dpkg-deb -b #{name} #{name}.deb")
+        DPKG.dpkg(['-i', "#{name}.deb"])
+      end
     end
+  end
+
+  def run(candidate_ppa, target_ppa)
+    if Process.uid == 0
+      # Disable invoke-rc.d because it is crap and causes useless failure on install
+      # when it fails to detect upstart/systemd running and tries to invoke a sysv
+      # script that does not exist.
+      File.write('/usr/sbin/invoke-rc.d', "#!/bin/sh\n")
+      # Speed up dpkg
+      File.write('/etc/dpkg/dpkg.cfg.d/02apt-speedup', "force-unsafe-io\n")
+      # Prevent xapian from slowing down the test.
+      # Install a fake package to prevent it from installing and doing anything.
+      # This does render it non-functional but since we do not require the database
+      # anyway this is the apparently only way we can make sure that it doesn't
+      # create its stupid database. The CI hosts have really bad IO performance making
+      # a full index take more than half an hour.
+      install_fake_pkg('apt-xapian-index')
+      File.open('/usr/sbin/update-apt-xapian-index', 'w', 0755) do |f|
+        f.write("#!/bin/sh\n")
+      end
+      # Also install a fake resolvconf because docker is a piece of shit cunt
+      # https://github.com/docker/docker/issues/1297
+      install_fake_pkg('resolvconf')
+      # Disable manpage database updates
+      Open3.popen3('debconf-set-selections') do |stdin, _stdout, stderr, wait_thr|
+        stdin.puts('man-db man-db/auto-update boolean false')
+        stdin.close
+        wait_thr.join
+        puts stderr.read
+      end
+      # Make sure everything is up-to-date.
+      abort 'failed to update' unless Apt.update
+      abort 'failed to dist upgrade' unless Apt.dist_upgrade
+      # Install ubuntu-minmal first to make sure foundations nonsense isn't going
+      # to make the test fail half way through.
+      abort 'failed to install minimal' unless Apt.install('ubuntu-minimal')
+      # Because dependencies are fucked
+      # [14:27] <sitter> dictionaries-common is a crap package
+      # [14:27] <sitter> it suggests a wordlist but doesn't pre-depend them or
+      # anything, intead it just craps out if a wordlist provider is installed but
+      # there is no wordlist -.-
+      system('apt-get install wamerican')
+    end
+
+    target_ppa.remove # remove live before attempting to use daily.
+
+    # Add the present daily snapshot, install everything.
+    # If this fails then the current snapshot is kaputsies....
+    if candidate_ppa.add
+      unless candidate_ppa.install
+        @log.info 'daily failed to install.'
+        daily_purged = candidate_ppa.purge
+        unless daily_purged
+          @log.info 'daily failed to install and then failed to purge. Maybe check' \
+                   ' maintscripts?'
+        end
+      end
+    end
+    @log.unknown 'done with daily'
+
+    # NOTE: If daily failed to install, no matter if we can upgrade live it is
+    # an improvement just as long as it can be installed...
+    # So we purged daily again, and even if that fails we try to install live
+    # to see what happens. If live is ok we are good, otherwise we would fail anyway
+
+    target_ppa.add
+    unless target_ppa.install
+      @log.error 'all is vain! live PPA is not installing!'
+      exit 1
+    end
+
+    # All is lovely. Let's make sure all live packages uninstall again
+    # (maintscripts!) and then start the promotion.
+    unless target_ppa.purge
+      @log.error 'live PPA installed just fine, but can not be uninstalled again.' \
+                ' Maybe check maintscripts?'
+      exit 1
+    end
+
+    @log.info "writing package list in #{Dir.pwd}"
+    File.write('sources-list.json', JSON.generate(target_ppa.sources))
   end
 end
 
-LOG.info `bash -c 'ulimit -n'`
+if __FILE__ == $PROGRAM_NAME
+  LOG = Logger.new(STDERR)
+  LOG.level = Logger::INFO
 
-# Disable invoke-rc.d because it is crap and causes useless failure on install
-# when it fails to detect upstart/systemd running and tries to invoke a sysv
-# script that does not exist.
-File.write('/usr/sbin/invoke-rc.d', "#!/bin/sh\n")
-# Speed up dpkg
-File.write('/etc/dpkg/dpkg.cfg.d/02apt-speedup', "force-unsafe-io\n")
-# Prevent xapian from slowing down the test.
-# Install a fake package to prevent it from installing and doing anything.
-# This does render it non-functional but since we do not require the database
-# anyway this is the apparently only way we can make sure that it doesn't
-# create its stupid database. The CI hosts have really bad IO performance making
-# a full index take more than half an hour.
-install_fake_pkg('apt-xapian-index')
-File.open('/usr/sbin/update-apt-xapian-index', 'w', 0755) do |f|
-  f.write("#!/bin/sh\n")
+  Project = Struct.new(:series, :stability)
+  project = Project.new(ENV.fetch('DIST'), ENV.fetch('TYPE'))
+
+  candiate_ppa = CiPPA.new("#{project.stability}-daily", project.series)
+  target_ppa = CiPPA.new(project.stability.to_s, project.series)
+  InstallCheck.new.run(candiate_ppa, target_ppa)
+  exit 0
 end
-# Also install a fake resolvconf because docker is a piece of shit cunt
-# https://github.com/docker/docker/issues/1297
-install_fake_pkg('resolvconf')
-# Disable manpage database updates
-Open3.popen3('debconf-set-selections') do |stdin, _stdout, stderr, wait_thr|
-  stdin.puts('man-db man-db/auto-update boolean false')
-  stdin.close
-  wait_thr.join
-  puts stderr.read
-end
-# Make sure everything is up-to-date.
-abort 'failed to update' unless Apt.update
-abort 'failed to dist upgrade' unless Apt.dist_upgrade
-# Install ubuntu-minmal first to make sure foundations nonsense isn't going
-# to make the test fail half way through.
-abort 'failed to install minimal' unless Apt.install('ubuntu-minimal')
-# Because dependencies are fucked
-# [14:27] <sitter> dictionaries-common is a crap package
-# [14:27] <sitter> it suggests a wordlist but doesn't pre-depend them or
-# anything, intead it just craps out if a wordlist provider is installed but
-# there is no wordlist -.-
-system('apt-get install wamerican')
-
-daily_ppa = CiPPA.new("#{project.stability}-daily", project.series)
-live_ppa = CiPPA.new(project.stability.to_s, project.series)
-live_ppa.remove # remove live before attempting to use daily.
-
-# Add the present daily snapshot, install everything.
-# If this fails then the current snapshot is kaputsies....
-if daily_ppa.add
-  unless daily_ppa.install
-    LOG.info 'daily failed to install.'
-    daily_purged = daily_ppa.purge
-    unless daily_purged
-      LOG.info 'daily failed to install and then failed to purge. Maybe check' \
-               ' maintscripts?'
-    end
-  end
-end
-LOG.unknown 'done with daily'
-
-# NOTE: If daily failed to install, no matter if we can upgrade live it is
-# an improvement just as long as it can be installed...
-# So we purged daily again, and even if that fails we try to install live
-# to see what happens. If live is ok we are good, otherwise we would fail anyway
-
-live_ppa.add
-unless live_ppa.install
-  LOG.error 'all is vain! live PPA is not installing!'
-  exit 1
-end
-
-# All is lovely. Let's make sure all live packages uninstall again
-# (maintscripts!) and then start the promotion.
-unless live_ppa.purge
-  LOG.error 'live PPA installed just fine, but can not be uninstalled again.' \
-            ' Maybe check maintscripts?'
-  exit 1
-end
-
-LOG.info "writing package list in #{Dir.pwd}"
-File.write('sources-list.json', JSON.generate(live_ppa.sources))
-
-exit 0
