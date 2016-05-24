8e4e1d337e81328d31c8143d19c5a51372522bcf
Harald Sitter <sitter@kde.org>
2016-05-24 12:44:56 +0200

    add asgen scripts to attempt appstream data generation

---
+++ ci-tooling/nci/asgen_build.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+#
+# Copyright (C) 2016 Harald Sitter <sitter@kde.org>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) version 3, or any
+# later version accepted by the membership of KDE e.V. (or its
+# successor approved by the membership of KDE e.V.), which shall
+# act as a proxy defined in Section 6 of version 3 of the license.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+require_relative '../lib/apt'
+
+Apt.install(%w(install dub libappstream-dev libgdk-pixbuf2.0-dev libarchive-dev
+               librsvg2-dev liblmdb-dev libglib2.0-dev libcairo2-dev
+               libcurl4-gnutls-dev)) || raise
+
+system('make update-submodule') || raise
+system('dub build --parallel') || raise
---
+++ ci-tooling/nci/asgen_run.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+#
+# Copyright (C) 2016 Harald Sitter <sitter@kde.org>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) version 3, or any
+# later version accepted by the membership of KDE e.V. (or its
+# successor approved by the membership of KDE e.V.), which shall
+# act as a proxy defined in Section 6 of version 3 of the license.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+require_relative '../lib/apt'
+
+Apt.install(%w(npm optipng)) || raise
+
+system('npm install -g bower') || raise
+system('make js') || raise
+
+config = ASGEN::Conf.new('KDE neon')
+config.ArchiveRoot = '/home/nci/public/user'
+# config.MediaBaseUrl
+# config.HtmlBaseUrl
+config.Backend = 'debian'
+config.Features['validateMetainfo'] = true
+config.Suites << ASGEN::Suite.new('xenial', ['main'], ['amd64'])
+
+run_dir = File.absolute_path('run')
+FileUtils.mkpath(run_dir) unless Dir.exist?(run_dir)
+config.write("#{run_dir}/asgen-config.json")
+system(%w(appstream-generator process xenial), chdir: run_dir) || raise
+
+puts FileUtils.glob("#{run_dir}/**/**").join("\n")
