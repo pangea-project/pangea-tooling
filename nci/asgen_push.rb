6fa41ed8250e4c9779d7b00511862914df3418c3
Harald Sitter <sitter@kde.org>
2016-06-01 13:56:56 +0200

    add missing require

--- nci/asgen_push.rb
+++ nci/asgen_push.rb
@@ -19,6 +19,7 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
+require 'fileutils'
 require 'net/sftp'
 require 'net/ssh'
 
