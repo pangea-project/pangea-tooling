65961df74f50826e29df1171d20501603f086610
Harald Sitter <sitter@kde.org>
2016-06-01 12:01:50 +0200

    move a whole slew of stuff out of asgen into asgen_push
    ideally we should be able to push existing data into aptly at some point
    without having to regenerate the data first.
    
    so, release mangling goes into push part

--- ci-tooling/nci/asgen.rb
+++ ci-tooling/nci/asgen.rb
@@ -44,8 +44,8 @@ system(*%w(make js)) || raise
 config = ASGEN::Conf.new('neon/user')
 config.dataPriority = 1
 config.ArchiveRoot = File.absolute_path('public/user')
-config.MediaBaseUrl = 'http://metadata.tanglu.org/appstream/media'
-config.HtmlBaseUrl = 'http://metadata.tanglu.org/appstream/'
+config.MediaBaseUrl = 'http://archive.neon.kde.org/appstream/media'
+config.HtmlBaseUrl = 'http://archive.neon.kde.org/appstream/html'
 config.Backend = 'debian'
 config.Features['validateMetainfo'] = true
 config.Suites << ASGEN::Suite.new('xenial', ['main'], ['amd64'])
@@ -60,40 +60,3 @@ system("#{build_dir}/appstream-generator", 'process', 'xenial',
 # TODO
 # [15:03] <ximion> sitter: the version number changing isn't an issue - it does nothing with one architecture, and it's an optimization if you have at least one other architecture.
 # [15:03] <ximion> sitter: you should run ascli cleanup every once in a while though, to collect garbage
-
-export_dir = "#{run_dir}/export"
-repo_dir = "#{export_dir}/repo"
-dep11_dir = "#{repo_dir}/main/dep11"
-FileUtils.rm_r(repo_dir) if Dir.exist?(repo_dir)
-FileUtils.mkpath(dep11_dir)
-FileUtils.cp_r("#{export_dir}/data/xenial/main/.", dep11_dir, verbose: true)
-
-release = Debian::Release.new("#{config.ArchiveRoot}/dists/xenial/Release")
-release.parse!
-
-def checksum(tool, f)
-  puts "#{tool} #{f}"
-  sum = `#{tool} #{f}`.strip.split(' ')[0]
-  raise unless $? == 0
-  size = File.size(f)
-  name = f.split('main/dep11/')[-1]
-  Debian::Release::Checksum.new(sum, size, "main/dep11/#{name}")
-end
-
-Dir.glob("#{dep11_dir}/*").each do |f|
-  %w(MD5Sum SHA1 SHA256 SHA512).each do |s|
-    tool = "#{s.downcase}sum"
-    tool = tool.gsub('sumsum', 'sum') # make sure md5sumsum becomes md5sum
-    release.fields[s] << checksum(tool, f)
-    next unless f.end_with?('.gz')
-    Dir.mktmpdir do |tmpdir|
-      Dir.chdir(tmpdir) do
-        FileUtils.cp(f, Dir.pwd)
-        basename = File.basename(f)
-        system("gunzip #{basename}") || raise
-        release.fields[s] << checksum(tool, basename.gsub('.gz', ''))
-      end
-    end
-  end
-end
-File.write("#{repo_dir}/Release", release.dump)
--- nci/asgen_push.rb
+++ nci/asgen_push.rb
@@ -22,6 +22,47 @@
 require 'net/sftp'
 require 'net/ssh'
 
+archive_dir = File.absolute_path('public/user')
+run_dir = File.absolute_path('run')
+
+# Mangle the Release file.
+export_dir = "#{run_dir}/export"
+repo_dir = "#{export_dir}/repo"
+dep11_dir = "#{repo_dir}/main/dep11"
+FileUtils.rm_r(repo_dir) if Dir.exist?(repo_dir)
+FileUtils.mkpath(dep11_dir)
+FileUtils.cp_r("#{export_dir}/data/xenial/main/.", dep11_dir, verbose: true)
+
+release = Debian::Release.new("#{archive_dir}/dists/xenial/Release")
+release.parse!
+
+def checksum(tool, f)
+  puts "#{tool} #{f}"
+  sum = `#{tool} #{f}`.strip.split(' ')[0]
+  raise unless $? == 0
+  size = File.size(f)
+  name = f.split('main/dep11/')[-1]
+  Debian::Release::Checksum.new(sum, size, "main/dep11/#{name}")
+end
+
+Dir.glob("#{dep11_dir}/*").each do |f|
+  %w(MD5Sum SHA1 SHA256 SHA512).each do |s|
+    tool = "#{s.downcase}sum"
+    tool = tool.gsub('sumsum', 'sum') # make sure md5sumsum becomes md5sum
+    release.fields[s] << checksum(tool, f)
+    next unless f.end_with?('.gz')
+    Dir.mktmpdir do |tmpdir|
+      Dir.chdir(tmpdir) do
+        FileUtils.cp(f, Dir.pwd)
+        basename = File.basename(f)
+        system("gunzip #{basename}") || raise
+        release.fields[s] << checksum(tool, basename.gsub('.gz', ''))
+      end
+    end
+  end
+end
+File.write("#{repo_dir}/Release", release.dump)
+
 repodir = File.absolute_path('run/export/repo')
 tmpdir = '/home/nci/asgen_push'
 targetdir = '/home/nci/aptly/public/release/dists/xenial'
