b92745173d7e25d4ff08a8b8c9f9f4ff502ec635
Harald Sitter <sitter@kde.org>
2016-06-02 14:25:50 +0200

    add missing require

--- ci-tooling/nci/asgen.rb
+++ ci-tooling/nci/asgen.rb
@@ -19,6 +19,8 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
+require 'fileutils'
+
 require_relative '../lib/apt'
 require_relative '../lib/asgen'
 
