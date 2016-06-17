4a3ddb8e7eb0cd14164fec1ae7b300079b9baacb
Jonathan Riddell <jr@jriddell.org>
2016-06-17 11:17:44 +0100

    remove / from edition name in hope it'll fix name appearing in ubiquity .desktop file as set in 25adduser script

--- nci/imager/config-settings-neon.sh
+++ nci/imager/config-settings-neon.sh
@@ -1,2 +1,3 @@
-export LB_ISO_VOLUME="${IMAGENAME} ${NEONARCHIVE} \$(date +%Y%m%d-%H:%M)"
+EDITION=$(echo $NEONARCHIVE | sed 's,/, ,')
+export LB_ISO_VOLUME="${IMAGENAME} ${EDITION} \$(date +%Y%m%d-%H:%M)"
 export LB_ISO_APPLICATION="KDE neon Live"
