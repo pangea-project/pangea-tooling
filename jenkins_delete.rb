33ddf54fd12fbdb9665088306d71cc3793fc8a54
Harald Sitter <sitter@kde.org>
2016-07-05 15:30:04 +0200

    revise delete to actually be reliable
    - use new shared job class
    - split into two steps as delete has some preconditions
      - disable & wipe & mangle config
      - actual delete
    - use our queue monkeypatch so we can build them from arrays
    - introduce new config mangling based on nokogiri. research suggests that
      jenkins refuses to delete jobs from-disk if they still have downstreams
      which then makes the jobs reappear the next time the configs are
      reloaded (or restarted). to prevent this we now mangle the job configs
      in the pre-condition loop by stripping all nodes that could potentially
      contain linkage information towards an downstream. we do this for all
      in the pre-condition loop so that we ultimately resolve both up
      and downstream relationships between the jobs to delete

--- jenkins_delete.rb
+++ jenkins_delete.rb
@@ -1,4 +1,23 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
+#
+# Copyright (C) 2015-2016 Harald Sitter <sitter@kde.org>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) version 3, or any
+# later version accepted by the membership of KDE e.V. (or its
+# successor approved by the membership of KDE e.V.), which shall
+# act as a proxy defined in Section 6 of version 3 of the license.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
 require 'date'
 require 'logger'
@@ -6,8 +25,10 @@ require 'logger/colors'
 require 'optparse'
 
 require_relative 'ci-tooling/lib/jenkins'
+require_relative 'ci-tooling/lib/queue'
 require_relative 'ci-tooling/lib/thread_pool'
 require_relative 'ci-tooling/lib/retry'
+require_relative 'lib/jenkins/job'
 
 OptionParser.new do |opts|
   opts.banner = <<-EOS
@@ -37,56 +58,48 @@ raise 'Need ruby pattern as argv0' if ARGV.empty?
 pattern = Regexp.new(ARGV[0])
 @log.info pattern
 
-job_name_queue = Queue.new
-job_names = Jenkins.job.list_all
-job_names.each do |name|
-  next unless pattern.match(name)
-  job_name_queue << name
+def mangle_xml(xml)
+  doc = Nokogiri::XML(xml)
+  doc.at('*/triggers').children.remove
+  doc.at('*/builders').children.remove
+  doc.at('*/publishers').children.remove
+  doc.at('*/buildWrappers').children.remove
+  doc.to_xml
 end
 
-module Jenkins
-  # A Jenkins Job.
-  # Gives jobs a class so one can use it like a bloody OOP construct rather than
-  # I don't even know what the default api client thing does...
-  class Job
-    attr_reader :name
+job_names = Jenkins.job.list_all.select { |name| pattern.match(name) }
 
-    def initialize(name)
-      @name = name
-    end
-
-    def delete!
-      Jenkins.job.delete(@name)
-    end
-
-    def wipe!
-      Jenkins.job.wipe_out_workspace(@name)
-    end
-
-    def enable!
-      Jenkins.job.enable(@name)
-    end
-  end
-end
-
+# First wipe and disable them.
+# In an effort to improve reliability of delets we attempt to break dep
+# chains as much as possible by breaking the job configs to force unlinks of
+# downstreams.
+job_name_queue = Queue.new(job_names)
 BlockingThreadPool.run do
   until job_name_queue.empty?
     name = job_name_queue.pop(true)
     job = Jenkins::Job.new(name)
-    @log.info "Deleting #{name}"
+    @log.info "Disabling #{name}"
+    Retry.retry_it(times: 5) do
+      job.disable!
+    end
+    Retry.retry_it(times: 5) do
+      job.update(mangle_xml(job.get_config))
+    end
     begin
-      Retry.retry_it(times: 5) do
-        job.wipe!
-      end
+      job.wipe!
     rescue
-      @log.info "Wiping of #{name} failed. Continue without wipe."
+      @log.warn "Wiping of #{name} failed. Continue without wipe."
     end
-    Retry.retry_it(times: 5) do
-      job.enable!
-    end
-    Retry.retry_it(times: 5) do
-      @log.info "Deleting #{name}"
-      job.delete!
-    end
+  end
+end
+
+# Once all are disabled, proceed with deleting.
+job_name_queue = Queue.new(job_names)
+BlockingThreadPool.run do
+  until job_name_queue.empty?
+    name = job_name_queue.pop(true)
+    @log.info "Deleting #{name}"
+    job = Jenkins::Job.new(name)
+    job.delete!
   end
 end
