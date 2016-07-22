129fcd5fa120b3216c9ea28b8df6561c6507e0e4
Harald Sitter <sitter@kde.org>
2016-07-22 11:56:13 +0200

    add some smarts to find the right desktop file

--- ci-tooling/nci/snap_internal.rb
+++ ci-tooling/nci/snap_internal.rb
@@ -31,10 +31,22 @@ snap.version = '16.04.1'
 snap.stagedepends = ['plasma-integration']
 p snap.stagedepends
 
-desktopfile = "org.kde.#{snap.name}.desktop"
-helpdesktopfile = 'org.kde.Help.desktop'
+## pull & get necessary data bits
 
-### appstream
+Snapcraft.pull
+
+matches = Dir.glob("parts/#{snap.name}/install/" \
+                   "usr/share/applications/*#{snap.name}*.desktop")
+if matches.size > 1
+  matches.select! do |match|
+    match.end_with?("org.kde.#{snap.name}.desktop")
+  end
+end
+raise "can't find right desktop file #{matches}" if matches.size != 1
+desktop_url = matches[0]
+desktopfile = File.basename(desktopfile)
+
+## extract appstream data
 
 appstreamer = AppStreamer.new(desktopfile)
 appstreamer.expand(snap)
@@ -46,17 +58,13 @@ Dir.chdir('snapcraft')
 File.write('snapcraft.yaml', snap.render)
 FileUtils.cp("#{__dir__}/data/qt5-launch", '.')
 
-Snapcraft.pull
-
-desktop_url = "parts/#{snap.name}/install/usr/share/applications/#{desktopfile}"
-help_desktop_url = "parts/#{snap.name}/install/usr/share/applications/#{helpdesktopfile}"
+## copy data into place for snapcraft to find it
 
 FileUtils.mkpath('setup/gui/')
 FileUtils.cp(icon_url, 'setup/gui/icon') if icon_url
 FileUtils.cp(desktop_url, "setup/gui/#{desktopfile}") if desktop_url
-if File.exist?(help_desktop_url)
-  FileUtils.cp(help_desktop_url, "setup/gui/#{helpdesktopfile}")
-end
+
+## finalize
 
 Snapcraft.run
 
