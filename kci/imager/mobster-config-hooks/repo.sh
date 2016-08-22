a29c2c64282b0c9cb382a935355cb8a383b469a6
Bhushan Shah <bhush94@gmail.com>
2016-08-22 12:00:15 +0530

    More clean up of old mci imaging stuffs

--- kci/imager/mobster-config-hooks/repo.sh
+++
@@ -1,5 +0,0 @@
-keyfile="/tmp/tmp.key"
-rm -rf $keyfile
-wget -O $keyfile "http://mobile.kci.pangea.pub/Pangea%20CI.gpg.key"
-gpg --no-default-keyring --primary-keyring config/archives/ubuntu-defaults.key --import $keyfile
-echo "deb [arch=amd64] http://mobile.kci.pangea.pub $SUITE main" >> config/archives/ubuntu-defaults.list
