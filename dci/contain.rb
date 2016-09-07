c902f764d997a29c642d6674fc7ab9cc43c0d3ac
Rohan Garg <rohan@garg.io>
2016-09-07 19:59:03 +0200

    Bump to 20 hours for QtWebengine

--- dci/contain.rb
+++ dci/contain.rb
@@ -2,7 +2,7 @@
 
 require_relative '../lib/ci/containment'
 
-Docker.options[:read_timeout] = 15 * 60 * 60 # 15 hours.
+Docker.options[:read_timeout] = 20 * 60 * 60 # 20 hours.
 
 DIST = ENV.fetch('DIST')
 BUILD_TAG = ENV.fetch('BUILD_TAG')
