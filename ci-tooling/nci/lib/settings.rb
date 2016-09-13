09160d12eb2279c8c9be50dfbdd7f0f7941e416e
Harald Sitter <sitter@kde.org>
2016-09-13 14:50:53 +0200

    add a new settings system for runtime configuration via configs (nci only)

---
+++ ci-tooling/nci/lib/settings.rb
@@ -0,0 +1,97 @@
+# frozen_string_literal: true
+#
+# Copyright (C) 2016 Harald Sitter <sitter@kde.org>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) version 3, or any
+# later version accepted by the membership of KDE e.V. (or its
+# successor approved by the membership of KDE e.V.), which shall
+# act as a proxy defined in Section 6 of version 3 of the license.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+require 'deep_merge'
+require 'yaml'
+
+require_relative '../../lib/ci/pattern'
+
+module NCI
+  # NCI settings
+  class Settings
+    DEFAULT_FILES = [
+      File.expand_path("#{__dir__}/../../data/settings/nci.yaml")
+    ].freeze
+
+    class << self
+      def for_job
+        @settings ||= instance.for_job
+      end
+
+      def default_files
+        @default_files ||= DEFAULT_FILES
+      end
+
+      attr_writer :default_files
+
+      private
+
+      def instance
+        @instance ||= new
+      end
+    end
+
+    def initialize(files = self.class.default_files)
+      @default_paths = files
+    end
+
+    def for_job
+      return {} unless job?
+      job_patterns = CI::FNMatchPattern.filter(job, settings)
+      job_patterns = CI::FNMatchPattern.sort_hash(job_patterns)
+      return {} if job_patterns.empty?
+      merge(job_patterns)
+    end
+
+    private
+
+    def merge(job_patterns)
+      folded = {}
+      job_patterns.each do |patterns|
+        patterns.each do |pattern|
+          folded = folded.deep_merge(pattern)
+        end
+      end
+      folded
+    end
+
+    def job?
+      @job_exist ||= File.exist?('job_name')
+    end
+
+    def job
+      job? # Make sure exist has been chached.
+      @job ||= File.read('job_name').strip
+    end
+
+    def settings
+      @settings ||= load
+    end
+
+    def load
+      hash = {}
+      @default_paths.each do |path|
+        hash.deep_merge!(YAML.load(File.read(path)))
+      end
+      hash = CI::FNMatchPattern.convert_hash(hash, recurse: false)
+      hash
+    end
+  end
+end
---
+++ ci-tooling/test/test_nci_settings.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+#
+# Copyright (C) 2016 Harald Sitter <sitter@kde.org>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) version 3, or any
+# later version accepted by the membership of KDE e.V. (or its
+# successor approved by the membership of KDE e.V.), which shall
+# act as a proxy defined in Section 6 of version 3 of the license.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+require_relative 'lib/testcase'
+require_relative '../nci/lib/settings'
+
+require 'mocha/test_unit'
+
+class NCISettingsTest < TestCase
+  def setup
+    NCI::Settings.default_files = []
+  end
+
+  def teardown
+    NCI::Settings.default_files = nil
+  end
+
+  def test_init
+    NCI::Settings.new
+  end
+
+  def test_settings
+    NCI::Settings.default_files << fixture_file('.yml')
+    File.write('job_name', 'xenial_unstable_libkolabxml_src')
+    settings = NCI::Settings.new
+    settings = settings.for_job
+    assert_equal({"sourcer"=>{"restricted_packaging_copy"=>true}}, settings)
+  end
+
+  def test_settings_singleton
+    NCI::Settings.default_files << fixture_file('.yml')
+    File.write('job_name', 'xenial_unstable_libkolabxml_src')
+    assert_equal({"sourcer"=>{"restricted_packaging_copy"=>true}}, NCI::Settings.for_job)
+  end
+end
