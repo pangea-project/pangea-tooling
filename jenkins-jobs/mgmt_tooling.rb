53e9d5dbb87b0d3c65a59c68c1923133c9733f28
Harald Sitter <sitter@kde.org>
2016-09-14 13:08:50 +0200

    extend tooling with dependee magic
    downstreams trigger, dependees only dep

--- jenkins-jobs/mgmt_tooling.rb
+++ jenkins-jobs/mgmt_tooling.rb
@@ -3,10 +3,12 @@ require_relative 'job'
 # Tooling management job.
 class MGMTToolingJob < JenkinsJob
   attr_reader :downstreams
+  attr_reader :dependees
 
-  def initialize(downstreams:)
+  def initialize(downstreams:, dependees:)
     name = 'mgmt_tooling'
     super(name, "#{name}.xml.erb")
     @downstreams = downstreams
+    @dependees = dependees
   end
 end
