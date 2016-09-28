cdfbeb12c52645ce382a34e92a62b2a453939f6a
Harald Sitter <sitter@kde.org>
2016-09-28 16:00:57 +0200

    attempt to make iso generation more logable

--- nci/imager/config-hooks-neon-development/apt.conf.sh
+++ nci/imager/config-hooks-neon-development/apt.conf.sh
@@ -1,3 +1,4 @@
 rm config/chroot_apt/apt.conf || true
 echo 'Debug::pkgProblemResolver "true";' >> config/chroot_apt/apt.conf
 echo 'Acquire::Languages "none";' >> config/chroot_apt/apt.conf
+echo 'quiet "1";' >> config/chroot_apt/apt.conf
