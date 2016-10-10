01133ee60bc755dc75554c7f6cb0e4b4c7f12d3c
Harald Sitter <sitter@kde.org>
2016-10-10 14:59:55 +0200

    add tag detective for automatic debian merge
    tag detective gets the latest tag version from ECM and verifies that all
    other frameworks have a consistent tag
    
    if the tags are consistent a dump of urls is produced for merge

---
+++ nci/debian-merge/tagdetective.rb
@@ -0,0 +1,80 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+#
+# Copyright (C) 2016 Harald Sitter <sitter@kde.org>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) version 3, or any
+# later version accepted by the membership of KDE e.V. (or its
+# successor approved by the membership of KDE e.V.), which shall
+# act as a proxy defined in Section 6 of version 3 of the license.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+require 'git'
+require 'json'
+require 'tmpdir'
+
+require_relative '../../ci-tooling/lib/projects/factory/neon'
+
+class TagDetective
+  ORIGIN = 'origin/master'.freeze
+  ExCLUSION = %w(frameworks/prison frameworks/kactivities frameworks/purpose
+                 frameworks/syntax-highlighting).freeze
+
+  def list_frameworks
+    ProjectsFactory::Neon.ls.select do |x|
+      x.start_with?('frameworks/') && !ExCLUSION.include?(x)
+    end
+  end
+
+  def frameworks
+    @frameworks ||= list_frameworks.collect do |x|
+      File.join(ProjectsFactory::Neon.url_base, x)
+    end
+  end
+
+  def last_tag_base
+    @last_tag_base ||= begin
+      ecm = frameworks.find { |x| x.include?('frameworks/extra-cmake-modules') }
+      raise unless ecm
+      last_tag_base = nil
+      Dir.mktmpdir do |tmpdir|
+        git = Git.clone(ecm, tmpdir)
+        last_tag = git.describe(ORIGIN, tags: true, abbrev: 0)
+        last_tag_base = last_tag.reverse.split('-', 2)[-1].reverse
+      end
+      last_tag_base
+    end
+  end
+
+  def investigation_data
+    data = {}
+    data[:tag_base] = last_tag_base
+    data[:repos] = frameworks.each do |url|
+      valid = Git.ls_remote(url).fetch('tags').keys.any? do |x|
+        x.start_with?(last_tag_base)
+      end
+      raise "found no #{last_tag_base} tag in #{url}" unless valid
+    end
+    data
+  end
+
+  def investigate
+    File.write('data.json', JSON.generate(investigation_data))
+  end
+end
+
+# :nocov:
+if __FILE__ == $PROGRAM_NAME
+  TagDetective.new.investigate
+end
+# :nocov:
---
+++ test/test_nci_tagdetective.rb
@@ -0,0 +1,94 @@
+# frozen_string_literal: true
+#
+# Copyright (C) 2016 Harald Sitter <sitter@kde.org>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) version 3, or any
+# later version accepted by the membership of KDE e.V. (or its
+# successor approved by the membership of KDE e.V.), which shall
+# act as a proxy defined in Section 6 of version 3 of the license.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+require_relative '../ci-tooling/test/lib/testcase'
+
+require 'mocha/test_unit'
+require 'rugged'
+
+require_relative '../nci/debian-merge/tagdetective.rb'
+
+class NCITagDetectiveTest < TestCase
+  def setup
+  end
+
+  def test_last_tag_base
+    remote_dir = File.join(Dir.pwd, 'frameworks/extra-cmake-modules')
+    FileUtils.mkpath(remote_dir)
+    Dir.chdir(remote_dir) do
+      `git init --bare .`
+    end
+    Dir.mktmpdir do |tmpdir|
+      Dir.chdir(tmpdir) do
+        `git clone #{remote_dir} clone`
+        Dir.chdir('clone') do
+          File.write('c1', '')
+          `git add c1`
+          `git commit --all -m 'commit'`
+          `git tag debian/1-0`
+
+          File.write('c2', '')
+          `git add c2`
+          `git commit --all -m 'commit'`
+          `git tag debian/2-0`
+
+          `git push --all`
+          `git push --tags`
+        end
+      end
+    end
+    ProjectsFactory::Neon.stubs(:ls).returns(%w(frameworks/extra-cmake-modules))
+    ProjectsFactory::Neon.stubs(:url_base).returns(Dir.pwd)
+
+    assert_equal('debian/2', TagDetective.new.last_tag_base)
+  end
+
+  def test_investigate
+    remote_dir = File.join(Dir.pwd, 'frameworks/meow')
+    FileUtils.mkpath(remote_dir)
+    Dir.chdir(remote_dir) do
+      `git init --bare .`
+    end
+    Dir.mktmpdir do |tmpdir|
+      Dir.chdir(tmpdir) do
+        `git clone #{remote_dir} clone`
+        Dir.chdir('clone') do
+          File.write('c2', '')
+          `git add c2`
+          `git commit --all -m 'commit'`
+          `git tag debian/2-0`
+
+          `git push --all`
+          `git push --tags`
+        end
+      end
+    end
+
+    ProjectsFactory::Neon.stubs(:ls).returns(%w(frameworks/meow))
+    ProjectsFactory::Neon.stubs(:url_base).returns(Dir.pwd)
+
+    TagDetective.any_instance.stubs(:last_tag_base).returns('debian/2')
+
+    TagDetective.new.investigate
+    assert_path_exist('data.json')
+    assert_equal({'tag_base' => 'debian/2', 'repos' => [remote_dir]},
+                 JSON.parse(File.read('data.json')))
+  end
+end
