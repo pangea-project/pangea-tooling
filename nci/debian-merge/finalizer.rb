407398cd718c46d4c9f2ee5af400162d1aa9d428
Harald Sitter <sitter@kde.org>
2016-11-02 13:25:04 +0100

    import credentials handling code from other repo class

--- nci/debian-merge/finalizer.rb
+++ nci/debian-merge/finalizer.rb
@@ -19,6 +19,9 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
+require 'json'
+require 'git_clone_url'
+require 'net/ssh'
 require 'rugged'
 require 'tmpdir'
 
@@ -62,11 +65,36 @@ module NCI
         end
 
         def push
+          mangle_push_path!
           remote = @rug.remotes['origin']
           remote.push(["#{pending.canonical_name}:refs/heads/Neon/unstable"],
-                      update_tips: ->(*args) { puts "tip:: #{args}" })
+                      update_tips: ->(*args) { puts "tip:: #{args}" },
+                      credentials: method(:credentials))
           remote.push([':refs/heads/Neon/pending-merge'],
-                      update_tips: ->(*args) { puts "tip:: #{args}" })
+                      update_tips: ->(*args) { puts "tip:: #{args}" },
+                      credentials: method(:credentials))
+        end
+
+        def mangle_push_path!
+          remote = @rug.remotes['origin']
+          puts "pull url #{remote.url}"
+          return unless remote.url.include?('anongit.neon.kde')
+          pull_path = GitCloneUrl.parse(remote.url).path[1..-1]
+          puts "mangle to neon@git.neon.kde.org:#{pull_path}"
+          remote.push_url = "neon@git.neon.kde.org:#{pull_path}"
+        end
+
+        def credentials(url, username, types)
+          raise unless types.include?(:ssh_key)
+          config = Net::SSH::Config.for(GitCloneUrl.parse(url).host)
+          default_key = "#{Dir.home}/.ssh/id_rsa"
+          key = File.expand_path(config.fetch(:keys, [default_key])[0])
+          Rugged::Credentials::SshKey.new(
+            username: username,
+            publickey: key + '.pub',
+            privatekey: key,
+            passphrase: ''
+          )
         end
       end
 
