1bfb85a0efaf7d17704ec345b38c06c5a80d2e9c
Harald Sitter <sitter@kde.org>
2016-11-04 11:49:12 +0100

    json usage is now isolated

--- nci/debian-merge/data.rb
+++ nci/debian-merge/data.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 module NCI
   module DebianMerge
     class Data
--- nci/debian-merge/finalizer.rb
+++ nci/debian-merge/finalizer.rb
@@ -19,7 +19,6 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
-require 'json'
 require 'git_clone_url'
 require 'net/ssh'
 require 'rugged'
