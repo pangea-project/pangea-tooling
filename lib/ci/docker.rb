e8116b6cf0818a189daae3f35eafac67cabc08b9
Rohan Garg <rohan@garg.io>
2016-11-07 14:37:18 +0100

    Make sure the version we want is actually provided

--- lib/ci/docker.rb
+++ lib/ci/docker.rb
@@ -1,5 +1,8 @@
 require 'docker'
 
-if Gem::Version.new(Docker::API_VERSION) < Gem::Version.new(1.24)
-  Docker::API_VERSION = '1.24'.freeze
+REQUIRED_DOCKER_API_VERSION = 1.24
+
+if (Gem::Version.new(Docker::API_VERSION) < Gem::Version.new(REQUIRED_DOCKER_API_VERSION)) &&
+   (Docker.version.fetch('Version').to_f >= REQUIRED_DOCKER_API_VERSION)
+  Docker::API_VERSION = REQUIRED_DOCKER_API_VERSION
 end
