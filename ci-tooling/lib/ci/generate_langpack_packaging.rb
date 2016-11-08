72318393aaebd9de63e3a4af24e36fdf01b7f83f
Rohan Garg <rohan@garg.io>
2016-11-08 17:34:05 +0100

    Substitute @ for - in packaging

--- ci-tooling/lib/ci/generate_langpack_packaging.rb
+++ ci-tooling/lib/ci/generate_langpack_packaging.rb
@@ -22,6 +22,7 @@ module CI
   # Class to generate packaging for kde-l10n packages
   class LangPack
     def self.generate_packaging!(lang)
+      lang.gsub(/@/, '-')
       match_pattern = /aaa(KDELANGNAME|UBUNTULANGCODE|KDELANGCODE|UBUNTULANGDEP)bbb/
       Dir.glob('debian/*').each do |file|
         next unless File.file? file
