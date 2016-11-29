9c93bde709cdec7b285a0ec13bc5447f5f8daeaf
Rohan Garg <rohan@garg.io>
2016-11-29 11:57:23 +0100

    Fix key

--- lib/ci/docker.rb
+++ lib/ci/docker.rb
@@ -1,7 +1,7 @@
 require 'docker'
 
 REQUIRED_DOCKER_API_VERSION = Gem::Version.new(1.24)
-DOCKER_MAX_API_VERSION = Gem::Version.new(Docker.version.fetch('Version'))
+DOCKER_MAX_API_VERSION = Gem::Version.new(Docker.version.fetch('ApiVersion'))
 if (Gem::Version.new(Docker::API_VERSION) < REQUIRED_DOCKER_API_VERSION) &&
    (DOCKER_MAX_API_VERSION >= REQUIRED_DOCKER_API_VERSION)
   Docker::API_VERSION = REQUIRED_DOCKER_API_VERSION
