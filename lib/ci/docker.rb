46a70654b67e9bff702b56709919e8a958edfa9f
Rohan Garg <rohan@garg.io>
2016-11-30 13:30:39 +0100

    Ensure that we reset the connection after query'ing docker

--- lib/ci/docker.rb
+++ lib/ci/docker.rb
@@ -6,3 +6,7 @@ if (Gem::Version.new(Docker::API_VERSION) < REQUIRED_DOCKER_API_VERSION) &&
    (DOCKER_MAX_API_VERSION >= REQUIRED_DOCKER_API_VERSION)
   Docker::API_VERSION = REQUIRED_DOCKER_API_VERSION
 end
+
+# Reset connection in order to pick up any connection options one might set
+# after requiring this file
+Docker.reset_connection!
