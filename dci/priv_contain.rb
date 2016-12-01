19e8ff2525f7af3af743995150f3abad8c814e14
Rohan Garg <rohan@garg.io>
2016-12-01 01:20:37 +0100

    Move Cmd into create for DCI too

--- dci/priv_contain.rb
+++ dci/priv_contain.rb
@@ -11,6 +11,7 @@ BUILD_TAG = ENV.fetch('BUILD_TAG')
 c = CI::Containment.new(BUILD_TAG,
                         image: CI::PangeaImage.new(:debian, DIST),
                         privileged: true,
-                        no_exit_handlers: false)
-status_code = c.run(Cmd: ARGV)
+                        no_exit_handlers: false,
+                        Cmd: ARGV)
+status_code = c.run
 exit status_code
