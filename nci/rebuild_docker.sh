4047d952ac8fb3799db30209f75048278fa537b0
Jonathan Riddell <jr@jriddell.org>
2017-01-10 12:17:09 +0000

    docker trigger URL

---
+++ nci/rebuild_docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Trigger Docker image rebuilds https://hub.docker.com/r/kdeneon/plasma/~/settings/automated-builds/
+# To be run by daily mgmt job
+
+. ~/docker-token
+
+echo curl --data build=true -X POST https://registry.hub.docker.com/u/kdeneon/plasma/trigger/${PLASMA_TOKEN}/
+
+echo curl --data build=true -X POST https://registry.hub.docker.com/u/kdeneon/all/trigger/${ALL_TOKEN}/
