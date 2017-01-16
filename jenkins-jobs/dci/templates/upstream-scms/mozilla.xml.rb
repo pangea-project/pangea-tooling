0920408c28b5bcdb75d775c727c1b048696af9a9
Rohan Garg <rohan@garg.io>
2017-01-16 18:19:48 +0100

    Fix xml

--- jenkins-jobs/dci/templates/upstream-scms/mozilla.xml.rb
+++ jenkins-jobs/dci/templates/upstream-scms/mozilla.xml.rb
@@ -1,10 +1,8 @@
 <hudson.plugins.mercurial.MercurialSCM plugin="mercurial@1.57">
   <source>https://www.rosenauer.org/hg/mozilla/</source>
-  <modules/>
   <revisionType>BRANCH</revisionType>
   <revision>firefox50</revision>
   <subdir>suse</subdir>
   <clean>false</clean>
-  <credentialsId/>
   <disableChangeLog>false</disableChangeLog>
 </hudson.plugins.mercurial.MercurialSCM>
