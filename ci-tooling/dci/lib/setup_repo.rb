dbceffce995a32b79be5f06cbcc8e79a3379e3b9
Rohan Garg <rohan@garg.io>
2017-01-18 14:03:42 +0100

    Add backports when using testing

--- ci-tooling/dci/lib/setup_repo.rb
+++ ci-tooling/dci/lib/setup_repo.rb
@@ -40,7 +40,7 @@ module DCI
       repos += %w(frameworks plasma kde-applications extras backports qt5)
       repos += %w(odroid) if DPKG::BUILD_ARCH == 'armhf'
     when 'testing'
-      repos += %w(frameworks)
+      repos += %w(frameworks backports)
       repos += %w(odroid) if DPKG::BUILD_ARCH == 'armhf'
     end
 
