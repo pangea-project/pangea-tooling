3bd7e8ad2d96210499842d003cd8635ddd797780
Rohan Garg <rohan@garg.io>
2017-02-07 12:21:58 +0100

    Add plasma to list of repos

--- ci-tooling/dci/lib/setup_repo.rb
+++ ci-tooling/dci/lib/setup_repo.rb
@@ -40,7 +40,7 @@ module DCI
       repos += %w(frameworks plasma kde-applications extras backports qt5)
       repos += %w(odroid) if DPKG::BUILD_ARCH == 'armhf'
     when 'testing'
-      repos += %w(frameworks backports)
+      repos += %w(frameworks backports plasma)
       repos += %w(odroid) unless DPKG::BUILD_ARCH == 'amd64'
     end
 
