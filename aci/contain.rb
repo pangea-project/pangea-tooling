c722d60ea8783cff0ce1e34630ffdee9af455887
Scarlett Clark <sgclark@kde.org>
2017-03-26 10:07:17 -0700

    put back source bind. ( aci )

--- aci/contain.rb
+++ aci/contain.rb
@@ -53,6 +53,7 @@ c = CI::Containment.new(
     Dir.pwd + ":/in",
     Dir.pwd + "/app.Dir:/app.Dir",
     Dir.pwd + "/appimages:/appimages",
+    Dir.pwd + "/source:/source",
     '/home/jenkinst/.gnupg:/home/jenkins/.gnupg'],
   privileged: true,
   no_exit_handlers: false
