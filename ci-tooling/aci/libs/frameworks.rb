eccfad4532b0ee3282fb30e1e4a1002d2655b385
Scarlett Clark <sgclark@kde.org>
2017-03-29 15:15:26 -0700

    debug

--- ci-tooling/aci/libs/frameworks.rb
+++ ci-tooling/aci/libs/frameworks.rb
@@ -35,7 +35,7 @@ module Frameworks
       dep_list = KF5[f].select { |k| k['distro_packages'] }
       package_list = dep_list['distro_packages']
       if package_list
-        package_list.is_a?
+        package_list.is_a?(String)
         p package_list.gsub(/\,|\[|\]|\"/, '')
         #kf5_packages << package_list
       #kf5_packages.merge(dep_list) if dep_list
