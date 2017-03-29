14ad86992e96dc28a304af7279d2f955f633e330
Scarlett Clark <sgclark@kde.org>
2017-03-29 16:24:35 -0700

    use dup on new var

--- ci-tooling/aci/spec/dependencies_rspec.rb
+++ ci-tooling/aci/spec/dependencies_rspec.rb
@@ -33,7 +33,7 @@ p Metadata::FRAMEWORKS
 
 describe 'install_packages' do
   it 'Installs distribution packages' do
-    packages = Metadata::PROJECTPACKAGES
+    packages = Metadata::PROJECTPACKAGES.dup
     expect(
       Packages.install_packages(
         kde: Metadata::BUILDKF5, projectpackages: packages
