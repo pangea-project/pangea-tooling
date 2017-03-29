8d0cec8f25393db80bdee9c7dfa2fd2c67c284cd
Scarlett Clark <sgclark@kde.org>
2017-03-29 15:33:14 -0700

    debug

--- ci-tooling/aci/libs/frameworks.rb
+++ ci-tooling/aci/libs/frameworks.rb
@@ -33,8 +33,9 @@ module Frameworks
     frameworks.each do |f|
       p f
       dep_list = KF5[f].select { |k| k['distro_packages'] }
-      package_list = dep_list['distro_packages'].join
+      package_list = dep_list['distro_packages']
       if package_list
+        package_list = package_list.join
         p package_list.gsub(/\,|\[|\]|\"/, '')
         #kf5_packages << package_list
       #kf5_packages.merge(dep_list) if dep_list
