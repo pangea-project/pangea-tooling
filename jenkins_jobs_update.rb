4fd1ea8c2c3955b730de36f339cb4d3e1919f162
Harald Sitter <sitter@kde.org>
2017-04-12 12:27:13 +0200

    death to useless arguments

--- jenkins_jobs_update.rb
+++ jenkins_jobs_update.rb
@@ -108,7 +108,7 @@ class ProjectUpdater < Jenkins::ProjectUpdater
     tooling_deploy = enqueue(MGMTToolingDeployJob.new(downstreams: [docker]))
     tooling_test =
       enqueue(MGMTToolingTestJob.new(downstreams: [tooling_deploy]))
-    enqueue(MGMTGitJewellerJob.new(downstreams: [tooling_test]))
+    enqueue(MGMTGitJewellerJob.new)
     enqueue(MGMTToolingProgenitorJob.new(downstreams: [tooling_test]))
     enqueue(MgmtProgenitorJob.new(downstream_jobs: all_meta_builds))
     enqueue(MGMTPauseIntegrationJob.new(downstreams: all_meta_builds))
