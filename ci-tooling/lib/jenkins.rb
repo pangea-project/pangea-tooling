f311adbf67aae802d686e975230df52903b783da
Harald Sitter <sitter@kde.org>
2017-04-25 12:25:03 +0200

    workaround broken view handling

--- ci-tooling/lib/jenkins.rb
+++ ci-tooling/lib/jenkins.rb
@@ -1,3 +1,23 @@
+# frozen_string_literal: true
+#
+# Copyright (C) 2014-2017 Harald Sitter <sitter@kde.org>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) version 3, or any
+# later version accepted by the membership of KDE e.V. (or its
+# successor approved by the membership of KDE e.V.), which shall
+# act as a proxy defined in Section 6 of version 3 of the license.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
 require 'jenkins_api_client'
 
 # Monkey patch for Client to fold in our config data.
@@ -33,6 +53,17 @@ module JenkinsApi
     prepend AutoConfigJenkinsClient
 
     attr_reader :server_ip
+
+    # Monkey patch to not be broken.
+    class View
+      # Upstream version applies a filter via list(filter) which means view_name
+      # is in fact view_regex as list() internally regexes the name. So if the
+      # name includes () things explode if they call exists?.
+      # https://github.com/arangamani/jenkins_api_client/issues/232
+      def exists?(view_name)
+        list.include?(view_name)
+      end
+    end
   end
 end
 
