1c8bfe71d1c275fab9b3ececa3d662b2eff469bb
Harald Sitter <sitter@kde.org>
2017-05-24 12:56:30 +0200

    add some debuggerino to track down an issue in DO deployment

--- mgmt/docker.rb
+++ mgmt/docker.rb
@@ -10,11 +10,16 @@ require_relative '../lib/mgmt/deployer'
 
 pid_map = {}
 
+p ENV
+warn "debian only: #{ENV.include?('PANGEA_DEBIAN_ONLY')}"
+warn "ubuntu only: #{ENV.include?('PANGEA_UBUNTU_ONLY')}"
+
 ubuntu_series = (MCI.series.keys | NCI.series.keys)
 ubuntu_series = [] if ENV.include?('PANGEA_DEBIAN_ONLY')
 ubuntu_series.each_index do |index|
   series = ubuntu_series[index]
   origins = ubuntu_series[index + 1..-1]
+  warn "building ubuntu #{series}"
   pid = fork do
     d = MGMT::Deployer.new('ubuntu', series, origins)
     d.run!
@@ -26,6 +31,7 @@ end
 debian_series = DCI.series.keys
 debian_series = [] if ENV.include?('PANGEA_UBUNTU_ONLY')
 debian_series.each do |k|
+  warn "building debian #{k}"
   pid = fork do
     d = MGMT::Deployer.new('debian', k)
     d.run!
