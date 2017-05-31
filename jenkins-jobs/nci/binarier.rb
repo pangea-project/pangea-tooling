74203fdb98cfaa87f692523221a434a3c6758a99
Harald Sitter <sitter@kde.org>
2017-05-31 16:42:53 +0200

    add core scoring system to binary jobs for nci

---
+++ jenkins-jobs/nci/binarier.rb
@@ -0,0 +1,10 @@
+require_relative '../job'
+require_relative '../binarier'
+
+# binary builder
+class BinarierJob
+  # Monkey patch cores in
+  def cores
+    JSON.parse(File.read("#{__dir__}/../../data/nci/jobs-to-cores.json")).fetch(job_name, '')
+  end
+end
