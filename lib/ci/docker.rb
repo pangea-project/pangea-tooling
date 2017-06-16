7a80da737e671daf1e5ef687c618ee0b857acf87
Rohan Garg <rohan@garg.io>
2017-06-16 15:13:03 +0200

    Get rid of the silly constant redifinition warning from Ruby

--- lib/ci/docker.rb
+++ lib/ci/docker.rb
@@ -2,9 +2,13 @@ require 'docker'
 
 REQUIRED_DOCKER_API_VERSION = Gem::Version.new(1.24)
 DOCKER_MAX_API_VERSION = Gem::Version.new(Docker.version.fetch('ApiVersion'))
+
 if (Gem::Version.new(Docker::API_VERSION) < REQUIRED_DOCKER_API_VERSION) &&
    (DOCKER_MAX_API_VERSION >= REQUIRED_DOCKER_API_VERSION)
-  Docker::API_VERSION = REQUIRED_DOCKER_API_VERSION
+   module Docker
+     remove_const :API_VERSION
+     API_VERSION = REQUIRED_DOCKER_API_VERSION
+   end
 end
 
 # Reset connection in order to pick up any connection options one might set
