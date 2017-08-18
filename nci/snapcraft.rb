6d705887d44197444fe34e37aede64f29ae0d10c
Harald Sitter <sitter@kde.org>
2017-08-18 12:53:38 +0200

    set env var to use our remote parts

--- nci/snapcraft.rb
+++ nci/snapcraft.rb
@@ -29,6 +29,8 @@ if $PROGRAM_NAME == __FILE__
   # KDoctools is rubbish and lets meinproc resolve asset paths through
   #  QStandardPaths *AT BUILD TIME*.
   ENV['XDG_DATA_DIRS'] = "#{Dir.pwd}/stage/usr/local/share:#{Dir.pwd}/stage/usr/share:/usr/local/share:/usr/share"
+  # Use our own remote parts file.
+  ENV['SNAPCRAFT_PARTS_URI'] = 'http://metadata.neon.kde.org/snap/parts.yaml'
   Apt.install('snapcraft')
   TTY::Command.new(uuid: false).run('snapcraft --debug')
 end
