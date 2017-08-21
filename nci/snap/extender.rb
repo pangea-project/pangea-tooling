91661f753b314d5ed8eb0f5de1f75067a778d4a7
Harald Sitter <sitter@kde.org>
2017-08-21 15:19:51 +0200

    add missing require
    no caught by test because we have so much tooling around tests :S

--- nci/snap/extender.rb
+++ nci/snap/extender.rb
@@ -18,6 +18,7 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
+require 'json'
 require 'rugged'
 require 'tmpdir'
 require 'yaml'
