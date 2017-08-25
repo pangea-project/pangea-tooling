5ae6cd89d74594d273b4e2c95add21b06e1f2e4d
Harald Sitter <sitter@kde.org>
2017-08-25 11:40:54 +0200

    support loading a file in debian::changelog

--- ci-tooling/lib/debian/changelog.rb
+++ ci-tooling/lib/debian/changelog.rb
@@ -1,3 +1,23 @@
+# frozen_string_literal: true
+#
+# Copyright (C) 2015-2017 Harald Sitter <sitter@kde.org>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) version 3, or any
+# later version accepted by the membership of KDE e.V. (or its
+# successor approved by the membership of KDE e.V.), which shall
+# act as a proxy defined in Section 6 of version 3 of the license.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
 # Debian changelog.
 class Changelog
   # This is a simplified parser that only reads the first line (latest entry)
@@ -15,7 +35,7 @@ class Changelog
   ALL        = 0b1111
 
   def initialize(pwd = Dir.pwd)
-    line = File.open("#{pwd}/debian/changelog", &:gets)
+    line = File.open(File.file?(pwd) ? pwd : "#{pwd}/debian/changelog", &:gets)
     # plasma-framework (5.3.0-0ubuntu1) utopic; urgency=medium
     match = line.match(/^(.*) \((.*)\) (.+); urgency=(\w+)/)
     # Need a  match and 5 elements.
