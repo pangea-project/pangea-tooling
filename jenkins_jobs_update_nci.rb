c6aaf71ff7d628695cd95e132182f8ba55359e06
Bhushan Shah <bhush94@gmail.com>
2017-08-28 09:19:43 +0530

    Enable plasma for the extra_architectures

--- jenkins_jobs_update_nci.rb
+++ jenkins_jobs_update_nci.rb
@@ -130,7 +130,7 @@ class ProjectUpdater < Jenkins::ProjectUpdater
             enqueue(AppSnapJob.new(project.name))
           end
           project_architectures = if type == 'unstable' &&
-                                     %w[qt frameworks forks
+                                     %w[qt frameworks forks plasma
                                      ].include?(project.component)
                                     NCI.all_architectures
                                   else
