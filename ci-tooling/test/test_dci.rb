e1869804eb852108be1e3fe62e78e0d39781a8b7
Rohan Garg <rohan@garg.io>
2017-09-04 17:11:47 +0200

    Update for Netrunner 17.09

--- ci-tooling/test/test_dci.rb
+++ ci-tooling/test/test_dci.rb
@@ -24,11 +24,12 @@ class DCITest < TestCase
   end
 
   def test_series
-    assert_equal_collection(%w(1703 1706 backports), DCI.series.keys)
-    assert_equal_collection(%w(20170206 20170530 20170531), DCI.series.values)
+    assert_equal_collection(%w(1703 1706 1709 backports), DCI.series.keys)
+    assert_equal_collection(%w(20170206 20170530 20170904 20170905), DCI.series.values)
     assert_equal('20170206', DCI.series['1703'])
     assert_equal('20170530', DCI.series['1706'])
-    assert_equal('20170531', DCI.series['backports'])
+    assert_equal('20170904', DCI.series['1709'])
+    assert_equal('20170905', DCI.series['backports'])
 
     # With sorting
     assert_equal('1703', DCI.series(sort: :ascending).keys.first)
