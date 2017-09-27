c7cb3ccaa7f88c9a723413ca9bae257dec33b026
Harald Sitter <sitter@kde.org>
2017-09-27 09:59:17 +0200

    fix indentation
    3 spaces. da fuq. how the heck does that even happen

--- lib/ci/docker.rb
+++ lib/ci/docker.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 require 'docker'
 
 REQUIRED_DOCKER_API_VERSION = Gem::Version.new(1.24)
@@ -6,10 +7,11 @@ DOCKER_MAX_API_VERSION = Gem::Version.new(Docker.version.fetch('ApiVersion'))
 
 if (Gem::Version.new(Docker::API_VERSION) < REQUIRED_DOCKER_API_VERSION) &&
    (DOCKER_MAX_API_VERSION >= REQUIRED_DOCKER_API_VERSION)
-   module Docker
-     remove_const :API_VERSION
-     API_VERSION = REQUIRED_DOCKER_API_VERSION
-   end
+  # Monkey patched docker
+  module Docker
+    remove_const :API_VERSION
+    API_VERSION = REQUIRED_DOCKER_API_VERSION
+  end
 end
 
 # Reset connection in order to pick up any connection options one might set
