0576dd887c66785aa651d9dda34daae4b0de23db
Jonathan Riddell <jr@jriddell.org>
2017-11-01 15:02:51 +0000

    allow cmake-ignore to be a yaml file to specify series

--- ci-tooling/lib/lint/linter.rb
+++ ci-tooling/lib/lint/linter.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 require_relative '../ci/pattern'
 require_relative 'result'
+require 'yaml'
 
 module Lint
   # Base class for all linters.
@@ -12,8 +13,28 @@ module Lint
       @ignores = []
     end
 
+    # Loads cmake-ignore as a YAML file and ignore series as specified
+    # or if it's not a YAML list revert back to basic style
     def load_include_ignores(file_path)
       return unless File.exist?(file_path)
+      begin
+        cmake_yaml = YAML.load(File.read(file_path))
+        pp cmake_yaml
+        #Our YAML has to be a list else we'll go back to basic style
+        if not cmake_yaml.kind_of?(Array)
+          load_include_ignores_basic(file_path)
+        else
+          cmake_yaml.each do |ignore_entry|
+            @ignores << CI::IncludePattern.new(ignore_entry.keys[0]) if ignore_entry['series'] == ENV['DIST']
+          end
+        end
+      rescue
+        load_include_ignores_basic(file_path)
+      end
+    end
+
+    # it's not YAML, load it old school
+    def load_include_ignores_basic(file_path)
       File.read(file_path).strip.split($/).each do |line|
         next if line.strip.start_with?('#') || line.empty?
         @ignores << CI::IncludePattern.new(line)
--- ci-tooling/test/test_lint_cmake.rb
+++ ci-tooling/test/test_lint_cmake.rb
@@ -4,8 +4,8 @@ require_relative 'lib/testcase'
 # Test lint cmake
 class LintCMakeTest < TestCase
   def data
-    path = super
-    File.read(path)
+    @path = super
+    File.read(@path)
   end
 
   def test_init
@@ -19,19 +19,19 @@ class LintCMakeTest < TestCase
   def test_missing_package
     r = Lint::Log::CMake.new.lint(data)
     assert(r.valid)
-    assert_equal(%w(KF5Package), r.warnings)
+    assert_equal(%w[KF5Package], r.warnings)
   end
 
   def test_optional
     r = Lint::Log::CMake.new.lint(data)
     assert(r.valid)
-    assert_equal(%w(Qt5TextToSpeech), r.warnings)
+    assert_equal(%w[Qt5TextToSpeech], r.warnings)
   end
 
   def test_warning
     r = Lint::Log::CMake.new.lint(data)
     assert(r.valid)
-    assert_equal(%w(), r.warnings)
+    assert_equal(%w[], r.warnings)
   end
 
   def test_disabled_feature
@@ -44,5 +44,45 @@ class LintCMakeTest < TestCase
     r = Lint::Log::CMake.new.lint(data)
     assert(r.valid)
     assert_equal(['Qt5Multimedia'], r.warnings)
+  end
+
+  def test_ignore_warning_by_release
+    data
+    ENV['DIST'] = 'xenial'
+    r = Lint::Log::CMake.new.tap do |cmake|
+      cmake.load_include_ignores("#{@path}-cmake-ignore")
+    end.lint(data)
+    assert(r.valid)
+    assert_equal(['XCB-CURSOR , Required for XCursor support'], r.warnings)
+  end
+
+  def test_ignore_warning_by_release_basic
+    data
+    ENV['DIST'] = 'xenial'
+    r = Lint::Log::CMake.new.tap do |cmake|
+      cmake.load_include_ignores("#{@path}-cmake-ignore")
+    end.lint(data)
+    assert(r.valid)
+    assert_equal(['QCH , API documentation in QCH format (for e.g. Qt Assistant, Qt Creator & KDevelop)'], r.warnings)
+  end
+
+  def test_ignore_warning_by_release_basic_multiline
+    data
+    ENV['DIST'] = 'xenial'
+    r = Lint::Log::CMake.new.tap do |cmake|
+      cmake.load_include_ignores("#{@path}-cmake-ignore")
+    end.lint(data)
+    assert(r.valid)
+    assert_equal([], r.warnings)
+  end
+
+  def test_ignore_warning_by_release_bionic
+    data
+    ENV['DIST'] = 'bionic'
+    r = Lint::Log::CMake.new.tap do |cmake|
+      cmake.load_include_ignores("#{@path}-cmake-ignore")
+    end.lint(data)
+    assert(r.valid)
+    assert_equal(['XCB-CURSOR , Required for XCursor support', 'QCH , API documentation in QCH format (for e.g. Qt Assistant, Qt Creator & KDevelop)'], r.warnings)
   end
 end
