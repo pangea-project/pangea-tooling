1197e2d54649537bfc7ac54796a6aebfa4d40021
Harald Sitter <sitter@kde.org>
2017-11-15 13:10:02 +0100

    split yaml loading into own method as well
    more uniform with basic loading

--- ci-tooling/lib/lint/linter.rb
+++ ci-tooling/lib/lint/linter.rb
@@ -19,21 +19,28 @@ module Lint
     def load_include_ignores(file_path)
       return unless File.exist?(file_path)
       cmake_yaml = YAML.load(File.read(file_path))
-      # Our YAML has to be a list else we'll go back to basic style
-      if cmake_yaml.instance_of?(Array) # new yaml format
-        cmake_yaml.each do |ignore_entry|
-          if ignore_entry.is_a?(String)
-            @ignores << CI::IncludePattern.new(ignore_entry)
-          elsif ignore_entry['series'] == ENV.fetch('DIST')
-            @ignores << CI::IncludePattern.new(ignore_entry.keys[0])
-          end
-        end
+      # Our YAML has to be an Array else we'll go back to basic style
+      if cmake_yaml.instance_of?(Array)
+        load_include_ignores_yaml(cmake_yaml)
       else # compat old files
         load_include_ignores_basic(file_path)
       end
     end
 
-    # it's not YAML, load it old school
+    private
+
+    # It's YAML, load it as such.
+    def load_include_ignores_yaml(data)
+      data.each do |ignore_entry|
+        if ignore_entry.is_a?(String)
+          @ignores << CI::IncludePattern.new(ignore_entry)
+        elsif ignore_entry['series'] == ENV.fetch('DIST')
+          @ignores << CI::IncludePattern.new(ignore_entry.keys[0])
+        end
+      end
+    end
+
+    # It's not YAML, load it old school
     def load_include_ignores_basic(file_path)
       File.read(file_path).strip.split($/).each do |line|
         next if line.strip.start_with?('#') || line.empty?
