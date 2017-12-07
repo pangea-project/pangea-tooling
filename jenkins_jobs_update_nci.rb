fa45985694e0c5319da79ec470cad82f4d4e07b8
Bhushan Shah <bhush94@gmail.com>
2017-12-07 17:31:30 +0530

    Enable arm64 as proper extra_architectures

--- jenkins_jobs_update_nci.rb
+++ jenkins_jobs_update_nci.rb
@@ -181,10 +181,6 @@ class ProjectUpdater < Jenkins::ProjectUpdater
                                   else
                                     NCI.architectures
                                   end
-          if type == 'unstable' && %w[qt frameworks forks plasma neon kde-std
-              ].include?(project.component)
-            project_architectures += ['arm64']
-          end
           jobs = ProjectJob.job(project,
                                 distribution: distribution,
                                 type: type,
