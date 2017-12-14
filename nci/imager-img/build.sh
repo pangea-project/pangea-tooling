51199d4d3a0f8622315fe7760ccffcec28265caa
Jonathan Riddell <jr@jriddell.org>
2017-12-14 10:40:30 +0000

    build with arm emulation on master

--- nci/imager-img/build.sh
+++ nci/imager-img/build.sh
@@ -2,6 +2,7 @@
 
 wget http://weegie.edinburghlinux.co.uk/~neon/debs/live-build_20170920_all.deb
 dpkg --install live-build_20170920_all.deb
+apt-get -y install qemu-user-static # for arm emulation
 
 lb clean --all
 rm -rf config
