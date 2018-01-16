44be40a2ea87732d61d23d9b126cbeeca75c89e2
Harald Sitter <sitter@kde.org>
2018-01-16 11:48:14 +0100

    attach a logger to our socket gateway so we can debug problems
    sometimes a session gets stuck on close and we do not know why, this
    should allow us to figure out why

--- lib/net/ssh/socket_gateway.rb
+++ lib/net/ssh/socket_gateway.rb
@@ -1,11 +1,31 @@
 # frozen_string_literal: true
+#
+# Copyright (C) 2017-2018 Harald Sitter <sitter@kde.org>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) version 3, or any
+# later version accepted by the membership of KDE e.V. (or its
+# successor approved by the membership of KDE e.V.), which shall
+# act as a proxy defined in Section 6 of version 3 of the license.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
-require 'thread'
+require 'logger'
 require 'net/ssh'
+require 'thread'
 
 class Net::SSH::SocketGateway
   def initialize(host, user, options={})
     @session = Net::SSH.start(host, user, options)
+    attach_logger(@session)
     @session_mutex = Mutex.new
     @loop_wait = options.delete(:loop_wait) || 0.001
     initiate_event_loop!
@@ -54,6 +74,22 @@ class Net::SSH::SocketGateway
   end
 
   private
+
+  def attach_logger(netsshobj)
+    # No littering when testing please.
+    return if ENV.include?('PANGEA_UNDER_TEST')
+    # :nocov:
+    log_file = "/tmp/net-ssh-#{$$}-#{netsshobj.object_id.abs}.log"
+    File.write(log_file, '')
+    File.chmod(0o600, log_file)
+    netsshobj.logger = Logger.new(log_file).tap do |l|
+      l.progname = $PROGRAM_NAME.split(' ', 2)[0]
+      l.level = Logger::DEBUG
+    end
+    netsshobj.logger.warn(ARGV.inspect)
+    warn(log_file)
+    # :nocov:
+  end
 
   # Fires up the gateway session's event loop within a thread, so that it
   # can run in the background. The loop will run for as long as the gateway
