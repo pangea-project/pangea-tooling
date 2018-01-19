9b2f8a3ad80d0535d0c5e8052583a4db5f235953
Harald Sitter <sitter@kde.org>
2018-01-19 15:47:11 +0100

    compress some jobs to conserve disk space of particularly verbose logs

--- jenkins-jobs/nci/binarier.rb
+++ jenkins-jobs/nci/binarier.rb
@@ -8,4 +8,11 @@ class BinarierJob
   def cores
     JSON.parse(File.read("#{__dir__}/../../data/nci/jobs-to-cores.json")).fetch(job_name, '2')
   end
+
+  def compress?
+    %w[qt5webkit qtwebengine mgmt_job-updater appstream-generator
+       kdevelop].any? do |x|
+      job_name.include?(x)
+    end
+  end
 end
