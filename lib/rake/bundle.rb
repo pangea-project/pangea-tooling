d75b5ba9e62bc15df10a7ee8b6a7804c279b3fc1
Harald Sitter <sitter@kde.org>
2018-01-25 09:22:18 +0100

    fix new image deployment by not requiring bundler in global scope
    require bundler on-demand instead.
    
    during deployment we start rake and then only later install bundler, since
    rake globally requires the helper lib we therefore can't use a global
    require in the lib as that'd make rake fail to resolve the missing bundler.
    instead simply require on-demand and handle LoadErrors same as NameErrors.

--- lib/rake/bundle.rb
+++ lib/rake/bundle.rb
@@ -1,6 +1,26 @@
 # frozen_string_literal: true
+#
+# Copyright (C) 2016-2018 Harald Sitter <sitter@kde.org>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) version 3, or any
+# later version accepted by the membership of KDE e.V. (or its
+# successor approved by the membership of KDE e.V.), which shall
+# act as a proxy defined in Section 6 of version 3 of the license.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
-require 'bundler'
+# NB: this is used during deployment. Do not require non-core gems globally!
+#   require during execution, and make sure the gems are actually installed or
+#   fallback logic is in place.
 
 # Bundler can have itself injected in the env preventing bundlers forked from
 # ruby to work correctly. This helper helps with running bundlers in a way
@@ -8,8 +28,9 @@ require 'bundler'
 module RakeBundleHelper
   class << self
     def run(*args)
+      require 'bundler'
       Bundler.clean_system(*args)
-    rescue NameError
+    rescue NameError, LoadError
       system(*args)
     end
   end
--- test/test_rake_bundle.rb
+++ test/test_rake_bundle.rb
@@ -18,6 +18,10 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
+# the lib itself doesn't require globally as it is used during deployment
+# and bundler isn't installed up until later
+require 'bundler'
+
 require_relative '../ci-tooling/test/lib/testcase'
 require_relative '../lib/rake/bundle'
 
