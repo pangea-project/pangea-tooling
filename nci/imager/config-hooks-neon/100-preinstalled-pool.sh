ada9dc95cca6d8c2daa6c8f26cbd8f802cee77b8
Harald Sitter <sitter@kde.org>
2018-02-13 16:26:43 +0100

    make sure to use a non-weak digets algorithm for the preinstalled-pool

--- nci/imager/config-hooks-neon/100-preinstalled-pool.sh
+++ nci/imager/config-hooks-neon/100-preinstalled-pool.sh
@@ -1,6 +1,14 @@
 mkdir -vp config/gnupg
 mkdir -vp config/indices
 
+# Make sure we usea  suitably strong digest algorithm. SHA1 is deprecated and
+# makes apt angry.
+cat > config/gnupg/gpg.conf <<EOF
+personal-digest-preferences SHA512
+cert-digest-algo SHA512
+default-preference-list SHA512 SHA384 SHA256 SHA224 AES256 AES192 AES CAST5 ZLIB BZIP2 ZIP Uncompressed
+EOF
+
 for component in $COMPONENTS; do
    (cd config/indices && \
     wget http://archive.ubuntu.com/ubuntu/indices/override.$SUITE.$component && \
