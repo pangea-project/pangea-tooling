78ed30dc18c68fe174f216efa449061ba66c82e4
Rohan Garg <rohan@garg.io>
2018-02-26 22:42:56 +0100

    Update for DCI

--- ci-tooling/test/test_dci.rb
+++ ci-tooling/test/test_dci.rb
@@ -24,15 +24,13 @@ class DCITest < TestCase
   end
 
   def test_series
-    assert_equal_collection(%w(1710 1801 1803 backports), DCI.series.keys)
-    assert_equal_collection(%w(20170904 20180115 20180315 20180316), DCI.series.values)
-    assert_equal('20170904', DCI.series['1710'])
-    assert_equal('20180115', DCI.series['1801'])
+    assert_equal_collection(%w(1803 backports), DCI.series.keys)
+    assert_equal_collection(%w(20180315 20180316), DCI.series.values)
     assert_equal('20180315', DCI.series['1803'])
     assert_equal('20180316', DCI.series['backports'])
 
     # With sorting
-    assert_equal('1710', DCI.series(sort: :ascending).keys.first)
+    assert_equal('1803', DCI.series(sort: :ascending).keys.first)
   end
 
   def test_latest_series
