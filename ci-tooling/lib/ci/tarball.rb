7b361055b8b194f290315883459532193bd15184
Harald Sitter <sitter@kde.org>
2018-03-02 14:14:31 +0100

    add new tarball variant which implements extract through a dsc file
    useful for when we have a full deb source already and want to unpack it
    via the generic tarball api

--- ci-tooling/lib/ci/tarball.rb
+++ ci-tooling/lib/ci/tarball.rb
@@ -1,5 +1,25 @@
 # frozen_string_literal: true
+#
+# Copyright (C) 2016-2018 Harald Sitter <sitter@kde.org>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) version 3, or any
+# later version accepted by the membership of KDE e.V. (or its
+# successor approved by the membership of KDE e.V.), which shall
+# act as a proxy defined in Section 6 of version 3 of the license.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
 require 'tmpdir'
+require 'tty/command'
 
 module CI
   # A tarball handling class.
@@ -83,6 +103,19 @@ module CI
     def list_content(path)
       content = Dir.glob("#{path}/*", File::FNM_DOTMATCH)
       content.reject { |c| %w[. ..].include?(File.basename(c)) }
+    end
+  end
+
+  # Special variant of tarball which has an associated dsc already, extracing
+  # will go through the dpkg-source intead of manually extracting the tar.
+  class DSCTarball < Tarball
+    def initialize(tar, dsc:)
+      super(tar)
+      @dsc = dsc
+    end
+
+    def extract(dest)
+      TTY::Command.new.run('dpkg-source', '-x', @dsc, dest)
     end
   end
 end
---
+++ ci-tooling/test/data/test_ci_tarball/test_dsc_extract/test_2.10.dsc
@@ -0,0 +1,16 @@
+Format: 3.0 (native)
+Source: test
+Binary: test
+Architecture: any
+Version: 2.10
+Maintainer: Santiago Vila <sanvila@debian.org>
+Standards-Version: 3.9.6
+Build-Depends: debhelper (>= 9.20120311)
+Package-List:
+ test deb devel optional arch=any
+Checksums-Sha1:
+ 5ed8b7fcdeec7099948bb2208e9afa91eb5eba1d 719 test_2.10.tar.gz
+Checksums-Sha256:
+ fddb642fc34e8884faae409f9397a6ac93fb5f24edf13b557be22ebef39088a8 719 test_2.10.tar.gz
+Files:
+ 56a4fdaefb25a580adc38108cc7155f4 719 test_2.10.tar.gz
