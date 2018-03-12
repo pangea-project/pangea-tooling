6770342415f65dcd98aaf2e29c9464022bb400d9
Bhushan Shah <bhush94@gmail.com>
2018-03-12 17:09:11 +0530

    Drop un-needed with in the stub_request

--- ci-tooling/test/test_projects_factory.rb
+++ ci-tooling/test/test_projects_factory.rb
@@ -42,13 +42,10 @@ class ProjectsFactoryTest < TestCase
     # would not be met.
     CI::UpstreamSCM.any_instance.stubs(:releaseme_adjust!).returns(true)
     stub_request(:get, 'https://projects.kde.org/api/v1/projects/frameworks').
-        with(headers: {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
         to_return(status: 200, body: '["frameworks/attica","frameworks/baloo","frameworks/bluez-qt"]', headers: {'Content-Type'=> 'text/json'})
     stub_request(:get, 'https://projects.kde.org/api/v1/projects/kde/workspace').
-        with(headers: {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
         to_return(status: 200, body: '["kde/workspace/khotkeys","kde/workspace/plasma-workspace"]', headers: {'Content-Type'=> 'text/json'})
     stub_request(:get, 'https://projects.kde.org/api/v1/projects/kde').
-        with(headers: {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
         to_return(status: 200, body: '["kde/workspace/khotkeys","kde/workspace/plasma-workspace"]', headers: {'Content-Type'=> 'text/json'})
   end
 
