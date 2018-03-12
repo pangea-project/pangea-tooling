9accf6861bc4cb4b24f900cfec421ff763035668
Bhushan Shah <bhush94@gmail.com>
2018-03-12 14:39:16 +0530

    Fix the test_kdeproject_component

--- test/test_kdeproject_component.rb
+++ test/test_kdeproject_component.rb
@@ -1,8 +1,26 @@
 require 'test/unit'
+
+require 'webmock'
+require 'webmock/test_unit'
+
 require_relative '../lib/kdeproject_component'
+
+require 'mocha/test_unit'
 
 class KDEProjectComponentTest < Test::Unit::TestCase
   def test_kdeprojectcomponent
+    stub_request(:get, 'https://projects.kde.org/api/v1/projects/frameworks').
+        with(headers: {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
+        to_return(status: 200, body: '["frameworks/attica","frameworks/baloo","frameworks/bluez-qt"]', headers: {'Content-Type'=> 'text/json'})
+
+    stub_request(:get, 'https://projects.kde.org/api/v1/projects/kde/workspace').
+        with(headers: {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
+        to_return(status: 200, body: '["kde/workspace/khotkeys","kde/workspace/plasma-workspace"]', headers: {'Content-Type'=> 'text/json'})
+
+    stub_request(:get, 'https://projects.kde.org/api/v1/projects/kde').
+        with(headers: {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
+        to_return(status: 200, body: '["kde/workspace/khotkeys","kde/kdesdk/umbrello"]', headers: {'Content-Type'=> 'text/json'})
+
     f = KDEProjectsComponent.frameworks
     p = KDEProjectsComponent.plasma
     a = KDEProjectsComponent.applications
