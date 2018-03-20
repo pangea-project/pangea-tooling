1a270a7160dd330722b910e77ad01b1b08bcca62
Harald Sitter <sitter@kde.org>
2018-03-20 11:39:31 +0100

    add some additional args to gpg to aid in debugging
    on bionic gpg is now gpg2 which wants a gpgagent (dirmgr) which for
    unknown reasons doesn't want to start just now.
    add some maybe useful options to figure out why

--- nci/imager/config-hooks-neon/repo.sh
+++ nci/imager/config-hooks-neon/repo.sh
@@ -1,4 +1,17 @@
-gpg --no-default-keyring \
+. /etc/os-release
+
+ARGS=""
+if [ "$VERSION_CODENAME" = "bionic" ]; then
+  ls -lah /root
+  ls -lah /
+  apt install -y dirmngr
+  ARGS="--batch --verbose"
+fi
+
+gpg --list-keys
+gpg \
+  $ARGS \
+  --no-default-keyring \
   --primary-keyring config/archives/ubuntu-defaults.key \
   --keyserver keyserver.ubuntu.com \
   --recv-keys '444D ABCF 3667 D028 3F89  4EDD E6D4 7362 5575 1E5D'
