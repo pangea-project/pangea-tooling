7e697805b6e8297cad121e4bbf30f674ef508be1
Harald Sitter <sitter@kde.org>
2018-03-20 15:42:48 +0100

    make sure gpg key can be read by _apt
    lb has an ubuntu patch which cp's instead of apt-key add, which then
    ends up having the wrong ownership. probably should be dropped. for now
    it's easier to simply chown the file

--- nci/imager/config-hooks-neon/repo.sh
+++ nci/imager/config-hooks-neon/repo.sh
@@ -19,3 +19,6 @@ $GPG \
   --recv-keys '444D ABCF 3667 D028 3F89  4EDD E6D4 7362 5575 1E5D'
 echo "deb http://archive.neon.kde.org/${NEONARCHIVE} $SUITE main" >> config/archives/neon.list
 echo "deb-src http://archive.neon.kde.org/${NEONARCHIVE} $SUITE main" >> config/archives/neon.list
+
+# make sure _apt can read this file. it may get copied into the chroot
+chmod 644 config/archives/ubuntu-defaults.key || true
