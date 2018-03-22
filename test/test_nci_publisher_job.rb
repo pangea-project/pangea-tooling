9c0fd170af9abd1d8c5978a9964d03ed082296da
Jonathan Riddell <jr@jriddell.org>
2018-03-22 12:59:28 +0000

    test publisher job does push to unstable and stable for frameworks

---
+++ test/test_nci_publisher_job.rb
@@ -0,0 +1,23 @@
+require 'test/unit'
+
+require 'webmock'
+require 'webmock/test_unit'
+
+require_relative '../jenkins-jobs/nci/publisher'
+
+require 'mocha/test_unit'
+
+class NeonPublisherJobTest < Test::Unit::TestCase
+  def test_frameworks_push_to_stable
+    JenkinsJob.flavor_dir = "../jenkins-jobs/nci/"
+    job = NeonPublisherJob.new('xenial_unstable_kde_attica',
+                               type: 'unstable',
+                               distribution: 'xenial',
+                               dependees: nil,
+                               component: 'kde',
+                               upload_map: nil,
+                               architectures: 'amd64',
+                               kdecomponent: 'frameworks')
+    assert_equal(job.repo_names, ["unstable_xenial", "stable_xenial"])
+  end
+end
