396450ef97d4c90e9483762c8422ae21614f9313
Jonathan Riddell <jr@jriddell.org>
2018-05-17 13:35:53 +0100

    Plasma is now at
    Revert "merge Plasma branches into Neon/release-lts while doing lts release"
    
    This reverts commit 13e7d3a5627dbb7330e04c97d79ba74be46a912b.

--- jenkins-jobs/nci/watcher.rb
+++ jenkins-jobs/nci/watcher.rb
@@ -41,13 +41,11 @@ class WatcherJob < JenkinsJob
     @scm_writable.url.gsub!('https://anongit.neon.kde.org/',
                             'neon@git.neon.kde.org:')
     # Don't touch release-lts for Plasma jobs
-    # (commented out while Plasma 5.12 LTS is current release, re-enable when it's not)
-    #if project.component == 'plasma'
-    #  @scm_writable.branch.replace('Neon/release')
-    #else
-    #  @scm_writable.branch.replace('Neon/release-lts')
-    #end
-    @scm_writable.branch.replace('Neon/release-lts')
+    if project.component == 'plasma'
+      @scm_writable.branch.replace('Neon/release')
+    else
+      @scm_writable.branch.replace('Neon/release-lts')
+    end
     @nci = NCI
     periodic_watch_components = %w[kde-extras kde-req kde-std neon-packaging
                                    forks calligra]
