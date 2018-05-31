9f4870ec23d00135a277d1fcf13bce9615eb0534
Jonathan Riddell <jr@jriddell.org>
2018-05-31 19:48:10 +0100

    Revert "update test"
    This reverts commit f014cd24073ba394564a9c94e6675f5065f83ca8.

--- ci-tooling/test/test_nci_sourcer.rb
+++ ci-tooling/test/test_nci_sourcer.rb
@@ -62,7 +62,7 @@ class NCISourcerTest < TestCase
     CI::WatchTarFetcher
       .expects(:new)
       .with('packaging/debian/watch',
-            mangle_download: true)
+            series: NCI.series.keys, mangle_download: true)
       .returns(fake_fetcher)
 
     fake_builder = mock('fake_builder')
