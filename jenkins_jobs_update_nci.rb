7dbe30d3d8d99917b457558e03021370a93d9648
Bhushan Shah <bhush94@gmail.com>
2018-06-01 14:24:42 +0530

    include ksystraycmd in bionic

--- jenkins_jobs_update_nci.rb
+++ jenkins_jobs_update_nci.rb
@@ -151,6 +151,7 @@ FUTURE_INCLUDE = %w[
   _neon-packaging_distro-release-notifier
   _neon-packaging_falkon
   _neon-packaging_kio-stash
+  _neon-packaging_ksystraycmd
 ].freeze
 # NB: iso_neon_bionic_devedition-gitunstable_amd64 is currently manually
 #   mangled to use iso-hackery branch.
