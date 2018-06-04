9750d105cbad3d653f1b516bce8478e2fbb23b31
Bhushan Shah <bhush94@gmail.com>
2018-06-04 13:33:58 +0530

    revert pm back to building iso images

--- nci/imager/config-hooks-neon-pm/10_hdd.sh
+++
@@ -1,2 +0,0 @@
-sed -i "s/^\\(LB_BINARY_IMAGES=\\).*/\\1\"hdd\"/" config/binary
-sed -i "s/^\\(LB_BINARY_FILESYSTEM=\\).*/\\1\"ext4\"/" config/binary
--- nci/imager/config-hooks-neon-pm/kernel.sh
+++
@@ -1,2 +0,0 @@
-# rename kernel and initrd to what syslinux expects
-rm config/hooks/rename-kernel.binary
