7e42f1346f2770f74747cc238a68ddbb266778cf
Bhushan Shah <bhush94@gmail.com>
2018-06-08 14:15:22 +0530

    Switch to tmp prefix
    Earlier this used to fail, but now I realized that there is weird
    htaccess rule on server which redirects /(un)stable/ to /dev/(un)stable/
    
    Added similar htaccess rule to redirect /tmp/(un)stable/ to
    /tmp/dev/(un)stable.

--- ci-tooling/nci/lib/setup_repo.rb
+++ ci-tooling/nci/lib/setup_repo.rb
@@ -159,14 +159,25 @@ APT::Default-Release "#{LSB::DISTRIB_CODENAME}";
 
     def debline(type: ENV.fetch('TYPE'), dist: LSB::DISTRIB_CODENAME)
       type = type.tr('-', '/')
-      format('deb http://archive.neon.kde.org/%<type>s %<dist>s main',
+      puts "type is : #{type}\n"
+      if type == 'stable' || type == 'unstable'
+        format('deb http://archive.neon.kde.org/tmp/%<type>s %<dist>s main',
              type: type, dist: dist)
+      else
+        format('deb http://archive.neon.kde.org/%<type>s %<dist>s main',
+             type: type, dist: dist)
+      end
     end
 
     def debsrcline(type: ENV.fetch('TYPE'), dist: LSB::DISTRIB_CODENAME)
       type = type.tr('-', '/')
-      format('deb-src http://archive.neon.kde.org/%<type>s %<dist>s main',
+      if type == 'stable' || type == 'unstable'
+        format('deb-src http://archive.neon.kde.org/tmp/%<type>s %<dist>s main',
              type: type, dist: dist)
+      else
+        format('deb-src http://archive.neon.kde.org/%<type>s %<dist>s main',
+             type: type, dist: dist)
+      end
     end
 
     def add_repo!
