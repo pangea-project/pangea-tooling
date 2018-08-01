396c1355965ac03d98e3aca5a6063fe492669a7c
Jonathan Riddell <jr@jriddell.org>
2018-08-01 11:14:55 +0100

    comment about more efficient way to rename jobs

--- jenkins_rename.rb
+++ jenkins_rename.rb
@@ -20,6 +20,11 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
+# mass renames are more efficient done on command line
+# https://wiki.jenkins.io/display/JENKINS/Administering+Jenkins 'Moving/copying/renaming jobs'
+# go to Jenkins GUI "Manage Jenkins" page and "Reload Configuration from Disk"
+# run job_updater
+
 require 'date'
 require 'logger'
 require 'logger/colors'
