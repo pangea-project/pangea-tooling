bdf9e92a07a8dac6520f9fd2fa4c4d42903db886
Bhushan Shah <bhush94@gmail.com>
2018-09-26 15:20:45 +0530

    Add xenon-mycroft image

---
+++ nci/imager/config-hooks-xenon-mycroft/30_no_source.sh
@@ -0,0 +1,1 @@
+echo 'LB_SOURCE=false' > config/source
---
+++ nci/imager/config-hooks-xenon-mycroft/repo.sh
@@ -0,0 +1,14 @@
+gpg --no-default-keyring \
+  --primary-keyring config/archives/ubuntu-defaults.key \
+  --keyserver pool.sks-keyservers.net \
+  --recv-keys '444D ABCF 3667 D028 3F89  4EDD E6D4 7362 5575 1E5D'
+echo "deb http://archive.neon.kde.org/${NEONARCHIVE} $SUITE main" >> config/archives/neon.list
+echo "deb-src http://archive.neon.kde.org/${NEONARCHIVE} $SUITE main" >> config/archives/neon.list
+
+gpg --no-default-keyring \
+  --primary-keyring config/archives/ubuntu-defaults.key \
+  --keyserver pool.sks-keyservers.net \
+  --recv-keys 'E47F 5011 FA60 FC1D EBB1  9989 3305 6FA1 4AD3 A421'
+
+echo "deb http://archive.xenon.pangea.pub/${NEONARCHIVE} $SUITE main" >> config/archives/neon.list
+echo "deb-src http://archive.xenon.pangea.pub/${NEONARCHIVE} $SUITE main" >> config/archives/neon.list
