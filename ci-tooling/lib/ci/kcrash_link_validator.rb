d2bb382cb1ce10008138b5249414555fc4a9ecfd
Harald Sitter <sitter@kde.org>
2018-11-17 12:23:32 +0100

    style++

--- ci-tooling/lib/ci/kcrash_link_validator.rb
+++ ci-tooling/lib/ci/kcrash_link_validator.rb
@@ -28,7 +28,7 @@ module CI
     BLACKLIST = [
       # Uses the same link list for the bin and a plugin. Unreasonable to expect
       # a change there.
-      '_kmail-account-wizard_',
+      '_kmail-account-wizard_'
     ].freeze
 
     def self.run(&block)
