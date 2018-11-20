0468f2cc1b5a37fd5f977b4f40ae3f7f2131850d
Jonathan Riddell <jr@jriddell.org>
2018-11-20 15:10:19 +0000

    better email info

--- ci-tooling/nci/lib/watcher.rb
+++ ci-tooling/nci/lib/watcher.rb
@@ -139,16 +139,17 @@ module NCI
       if ENV['JOB_NAME'].include?('_kde_') and CAUSE_ENVS.any? { |v| ENV[v] == 'TIMERTRIGGER' }
         puts 'KDE Plasma/Apps/Framework watcher should be run manually not by timer, quitting'
         puts 'sending notification mail'
+        package = newer.values[-1][0] # group_by results in an array
         Pangea::SMTP.start do |smtp|
             mail = <<-MAIL
     From: Neon CI <no-reply@kde.org>
     To: neon-notifications@kde.org
-    Subject: #{newest_dehs_package.name} new version #{newest}
+    Subject: #{package.name} new version #{newest}
 
     New release being prepped, run jenkins_retry for this release near to release day.
     #{ENV['RUN_DISPLAY_URL']}
 
-    #{newer.to_s}
+    #{package.inspect}
             MAIL
             smtp.send_message(mail,
                             'no-reply@kde.org',
