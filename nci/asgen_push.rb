d35010d22755f4b17c54d792a310392c5d1a900c
Bhushan Shah <bhush94@gmail.com>
2018-12-03 20:36:37 +0530

    Disable strict host key checking

--- nci/asgen_push.rb
+++ nci/asgen_push.rb
@@ -80,5 +80,6 @@ system("rm -rf #{export_dir}/data")
 #   to tmp path, removing pubdir and moving tmpdir to pubdir, while rsync will
 #   be faster.
 remote_dir = "metadataneon@drax.kde.org:#{pubdir}"
-rsync_opts = "-av --delete -e 'ssh -i #{ENV.fetch('SSH_KEY_FILE')}'"
+ssh_command = "ssh -o StrictHostKeyChecking=no -i #{ENV.fetch('SSH_KEY_FILE')}"
+rsync_opts = "-av --delete -e '#{ssh_command}'"
 system("rsync #{rsync_opts} #{export_dir} #{remote_dir}") || raise
