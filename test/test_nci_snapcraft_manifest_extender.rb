c8005e70da153d5897e981126389deaf557eea79
Harald Sitter <sitter@kde.org>
2018-12-12 13:18:40 +0100

    extend the manifest extender with a way of dealing with build deps
    snapcraft build-package are not broken up by manifest.txt, so we need
    a different way of dealing with them. we'll simply install fake packages
    for all the packages we have in the content-snap/build-snap. thus allowing
    us to get away with not installing stuff we already have (or such is
    the intent anyway)

--- test/test_nci_snapcraft_manifest_extender.rb
+++ test/test_nci_snapcraft_manifest_extender.rb
@@ -26,6 +26,7 @@ require 'mocha/test_unit'
 module NCI::Snap
   class ManifestExtendertest < TestCase
     def setup
+      ManifestExtender.install_fakes = false
       ManifestExtender.manifest_path = "#{Dir.pwd}/man"
       ENV['APPNAME'] = 'kolourpaint'
       ENV['DIST'] = 'bionic'
@@ -34,6 +35,10 @@ module NCI::Snap
         .to_return(status: 200, body: JSON.generate(['meep']))
       stub_request(:get, Extender::Core18::STAGED_DEV_PATH)
         .to_return(status: 200, body: JSON.generate(['meep-dev']))
+    end
+
+    def teardown
+      ManifestExtender.install_fakes = false
     end
 
     def test_run
