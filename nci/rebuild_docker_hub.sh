22a52a3fdbd67d264d3dad5c1ff950c87c5816cb
Jonathan Riddell <jr@jriddell.org>
2019-01-16 13:02:18 +0000

    note where docker token comes from

--- nci/rebuild_docker_hub.sh
+++ nci/rebuild_docker_hub.sh
@@ -5,6 +5,12 @@ set -ex
 # Trigger Docker image rebuilds https://hub.docker.com/r/kdeneon/plasma/~/settings/automated-builds/
 # To be run by daily mgmt job
 
+# This file is in format
+# PLASMA_TOKEN=xxxx-xxx-xxx-xxxxx
+# ALL_TOKEN=xxxx-xxx-xxx-xxxxx
+# Token comes from https://cloud.docker.com/u/kdeneon/repository/docker/kdeneon/all/hubbuilds
+# and https://cloud.docker.com/u/kdeneon/repository/docker/kdeneon/plasma/hubbuilds
+
 . ~/docker-token
 
 curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/kdeneon/plasma/trigger/${PLASMA_TOKEN}/
