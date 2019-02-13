1a86fd7d78c6365de55d2cb3c8e253cf9e862a9d
Harald Sitter <sitter@kde.org>
2019-02-13 13:40:24 +0100

    bring in supremely awful by-hash tech for asgen
    this should allow more reliable caching I hope. now also has tests!
    
    this is super awkward on so many levels. but should reliably manage by-hash
    data for dep11

--- nci/asgen_push.rb
+++ nci/asgen_push.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 #
-# Copyright (C) 2016-2018 Harald Sitter <sitter@kde.org>
+# Copyright (C) 2016-2019 Harald Sitter <sitter@kde.org>
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -28,127 +28,189 @@ require 'tty-command'
 require_relative '../ci-tooling/lib/debian/release'
 require_relative '../ci-tooling/lib/nci'
 
-DIST = ENV.fetch('DIST')
-TYPE = ENV.fetch('TYPE')
-APTLY_REPOSITORY = ENV.fetch('APTLY_REPOSITORY')
+class NCI::AppstreamGeneratorPush
+  APTLY_HOME = "/home/neonarchives"
 
-run_dir = File.absolute_path('run')
+  Sum = Struct.new(:file, :value)
 
-# Move data into basic dir structure of repo skel.
-export_dir = "#{run_dir}/export"
-export_data_dir = "#{export_dir}/data"
-repo_dir = "#{export_dir}/repo"
-dep11_dir = "#{repo_dir}/main/dep11"
+  class RSync
+    def sync(from, to)
+      ssh_command = "ssh -o StrictHostKeyChecking=no -i #{ENV.fetch('SSH_KEY_FILE')}"
+      rsync_opts = "-av -e '#{ssh_command}'"
+      system("rsync #{rsync_opts} #{from} #{to}") || raise
+    end
+  end
 
-unless File.exist?(export_data_dir)
-  warn "The data dir #{export_data_dir} does not exist." \
-       ' It seems asgen found no new data. Skipping publish!'
-  exit 0
-end
+  def dist
+    ENV.fetch('DIST')
+  end
 
-FileUtils.rm_r(repo_dir) if Dir.exist?(repo_dir)
-FileUtils.mkpath(dep11_dir)
-FileUtils.cp_r("#{export_data_dir}/#{DIST}/main/.", dep11_dir, verbose: true)
+  def type
+    ENV.fetch('TYPE')
+  end
 
-tmpdir = "/home/neonarchives/asgen_push.#{APTLY_REPOSITORY.tr('/', '-')}"
-targetdir = "/home/neonarchives/aptly/skel/#{APTLY_REPOSITORY}/dists/#{DIST}"
+  def aptly_repository
+    ENV.fetch('APTLY_REPOSITORY')
+  end
 
-# This depends on https://github.com/aptly-dev/aptly/pull/473
-# Aptly versions must take care to actually have the PR applied to them until
-# landed upstream!
-# NB: this is updating off-by-one. i.e. when we run the old data is published,
-#   we update the data but it will only be updated the next time the publish
-#   is updated (we may do this in the future as acquire-by-hash is desired for
-#   such quick update runs).
+  def exist?(sftp, path)
+    sftp.stat!(path)
+    true
+  rescue Net::SFTP::StatusException
+    false
+  end
 
-# We need the checksum of the uncompressed file in the Release file of the repo,
-# this is currently not correctly handled in the aptly skel system. As a quick
-# stop-gap we'll simply make sure an uncompressed file is around.
-# https://github.com/aptly-dev/aptly/pull/473#issuecomment-391281324
-Dir.glob("#{dep11_dir}/**/*.gz") do |compressed|
-  next if compressed.include?('by-hash') # do not follow by-hash
-  system('gunzip', '-k', compressed) || raise
-end
+  def symlink?(sftp, path)
+    sftp.readlink!(path)
+    true
+  rescue Net::SFTP::StatusException
+    false
+  end
 
-Sum = Struct.new(:file, :value)
+  def run
+    run_dir = File.absolute_path('run')
 
-keys_and_tools = {
-  'MD5Sum' => 'md5sum',
-  'SHA1' => 'sha1sum',
-  'SHA256' => 'sha256sum',
-  'SHA512' => 'sha512sum'
-}
+    # Move data into basic dir structure of repo skel.
+    export_dir = "#{run_dir}/export"
+    export_data_dir = "#{export_dir}/data"
+    repo_dir = "#{export_dir}/repo"
+    dep11_dir = "#{repo_dir}/main/dep11"
 
-keys_and_sums = {}
+    unless File.exist?(export_data_dir)
+      warn "The data dir #{export_data_dir} does not exist." \
+          ' It seems asgen found no new data. Skipping publish!'
+      return
+    end
 
-cmd = TTY::Command.new
+    FileUtils.rm_r(repo_dir) if Dir.exist?(repo_dir)
+    FileUtils.mkpath(dep11_dir)
+    FileUtils.cp_r("#{export_data_dir}/#{dist}/main/.", dep11_dir, verbose: true)
 
-# Create a sum map for all files, we'll then by-hash each of them.
-Dir.glob("#{dep11_dir}/*") do |file|
-  next if File.basename(file) == 'by-hash'
-  raise "Did not expect !file: #{file}" unless File.file?(file)
-  keys_and_tools.each do |key, tool|
-    keys_and_sums[key] ||= []
-    sum = cmd.run(tool, file).out.split(' ')[0]
-    keys_and_sums[key] << Sum.new(File.absolute_path(file), sum)
+    tmpdir = "#{APTLY_HOME}/asgen_push.#{aptly_repository.tr('/', '-')}"
+    targetdir = "#{APTLY_HOME}/aptly/skel/#{aptly_repository}/dists/#{dist}"
+
+    # This depends on https://github.com/aptly-dev/aptly/pull/473
+    # Aptly versions must take care to actually have the PR applied to them until
+    # landed upstream!
+    # NB: this is updating off-by-one. i.e. when we run the old data is published,
+    #   we update the data but it will only be updated the next time the publish
+    #   is updated (we may do this in the future as acquire-by-hash is desired for
+    #   such quick update runs).
+
+    # We need the checksum of the uncompressed file in the Release file of the repo,
+    # this is currently not correctly handled in the aptly skel system. As a quick
+    # stop-gap we'll simply make sure an uncompressed file is around.
+    # https://github.com/aptly-dev/aptly/pull/473#issuecomment-391281324
+    Dir.glob("#{dep11_dir}/**/*.gz") do |compressed|
+      next if compressed.include?('by-hash') # do not follow by-hash
+      system('gunzip', '-k', compressed) || raise
+    end
+
+
+    keys_and_tools = {
+      'MD5Sum' => 'md5sum',
+      'SHA1' => 'sha1sum',
+      'SHA256' => 'sha256sum',
+      'SHA512' => 'sha512sum'
+    }
+
+    keys_and_sums = {}
+
+    cmd = TTY::Command.new
+
+    # Create a sum map for all files, we'll then by-hash each of them.
+    Dir.glob("#{dep11_dir}/*") do |file|
+      next if File.basename(file) == 'by-hash'
+      raise "Did not expect !file: #{file}" unless File.file?(file)
+      keys_and_tools.each do |key, tool|
+        keys_and_sums[key] ||= []
+        sum = cmd.run(tool, file).out.split(' ')[0]
+        keys_and_sums[key] << Sum.new(File.absolute_path(file), sum)
+      end
+    end
+
+    Net::SFTP.start('archive-api.neon.kde.org', 'neonarchives',
+                    keys: ENV.fetch('SSH_KEY_FILE'), keys_only: true) do |sftp|
+      dep11_targetdir = "#{targetdir}/main/dep11/"
+      dep11_tmpdir = "#{tmpdir}/main/dep11/"
+
+      puts sftp.session.exec!("rm -rf #{tmpdir}")
+      puts sftp.session.exec!("mkdir -p #{tmpdir}")
+
+      sftp.upload!("#{repo_dir}/.", tmpdir)
+
+      # merge in original by-hash data, so we can update it
+      puts sftp.session.exec!("cp -rv #{dep11_targetdir}/by-hash/. #{dep11_tmpdir}/by-hash/")
+
+      by_hash = "#{dep11_tmpdir}/by-hash/"
+      sftp.mkdir!(by_hash) unless exist?(sftp, by_hash)
+
+      keys_and_sums.each do |key, sums|
+        dir = "#{by_hash}/#{key}/"
+        sftp.mkdir!(dir) unless exist?(sftp, dir)
+
+        sums.each do |sum|
+          basename = File.basename(sum.file)
+          base_path = File.join(dir, basename)
+
+          old = "#{basename}.old"
+          old_path = File.join(dir, old)
+          if symlink?(sftp, old_path)
+            # If we had an old variant, drop it.
+            sftp.remove!(old_path)
+          end
+          if symlink?(sftp, base_path)
+            # If we have a current variant, make it the old variant.
+            sftp.rename!(base_path, old_path,
+              Net::SFTP::Constants::RenameFlags::OVERWRITE | Net::SFTP::Constants::RenameFlags::ATOMIC)
+          end
+
+          # Use our current data as the new current variant.
+          sftp.upload!(sum.file, File.join(dir, sum.value))
+          sftp.symlink!(sum.value, base_path)
+        end
+
+        # Get a list of all blobs and drop all which aren't referenced by any of
+        # the marker symlinks. This should give super reliable cleanup.
+        used_blobs = []
+        blobs = []
+
+        sftp.dir.glob(dir, '*') do |entry|
+          path = File.join(dir, entry.name)
+          puts path
+          if entry.symlink?
+            used_blobs << File.absolute_path(sftp.readlink!(path), dir)
+          else
+            blobs << File.absolute_path(path)
+          end
+        end
+
+        warn "All blobs in #{key}: #{used_blobs}"
+        warn "Used blobs in #{key}: #{blobs}"
+        warn "Blobs to delete in #{key}: #{(blobs - used_blobs)}"
+
+        (blobs - used_blobs).each do |blob|
+          sftp.remove!(blob)
+        end
+      end
+
+      puts sftp.session.exec!("rm -r #{targetdir}/")
+      puts sftp.session.exec!("mkdir -p #{targetdir}/")
+      puts sftp.session.exec!("cp -rv #{tmpdir}/. #{targetdir}/")
+      puts sftp.session.exec!("rm -rv #{tmpdir}")
+    end
+    FileUtils.rm_rf(repo_dir)
+
+    pubdir = "/srv/www/metadata.neon.kde.org/appstream/#{type}_#{dist}"
+
+    # This is the export dep11 data, we don't need it, so throw it away
+    system("rm -rf #{export_data_dir}")
+    # NB: We use rsync here because a) SFTP is dumb and may require copying things
+    #   to tmp path, removing pubdir and moving tmpdir to pubdir, while rsync will
+    #   be faster.
+    remote_dir = "metadataneon@charlotte.kde.org:#{pubdir}"
+    RSync.sync("#{export_dir}/*", "#{remote_dir}/")
   end
 end
-require 'pp'
-pp keys_and_sums
 
-# Net::SFTP.start('archive-api.neon.kde.org', 'neonarchives',
-#                 keys: ENV.fetch('SSH_KEY_FILE'), keys_only: true) do |sftp|
-#   begin
-#     sftp.lstat!("#{targetdir}/by-hash")
-#     sftp.download!("#{targetdir}/by-hash", dep11_dir)
-#   rescue Net::SFTP::StatusException
-#     warn 'by-hash does not exist yet! generating from scratch'
-#   end
-# end
-
-# keys_and_sums.each do |key, sums|
-#   dir = "#{dep11_dir}/by-hash/#{key}/"
-#   FileUtils.mkpath(dir) unless File.exist?(dir)
-#   Dir.chdir(dir) do
-#     sums.each do |sum|
-#       basename = File.basename(sum.file)
-
-#       rotate = proc do
-#         old = "#{basename}.old"
-#         if File.exist?(old)
-#           link_target = File.readlink(old)
-#           FileUtils.rm_f([link_target, old], verbose: true)
-#         end
-#         FileUtils.mv(basename, old, verbose: true) if File.symlink?(basename)
-#       end
-#       rotate.call
-
-#       FileUtils.cp(sum.file, sum.value, verbose: true)
-#       FileUtils.ln_s(sum.value, basename)
-#     end
-#   end
-# end
-
-Net::SFTP.start('archive-api.neon.kde.org', 'neonarchives',
-                keys: ENV.fetch('SSH_KEY_FILE'), keys_only: true) do |sftp|
-  puts sftp.session.exec!("rm -rf #{tmpdir}")
-  puts sftp.session.exec!("mkdir -p #{tmpdir}")
-
-  sftp.upload!("#{repo_dir}/", tmpdir)
-
-  puts sftp.session.exec!("mkdir -p #{targetdir}/")
-  puts sftp.session.exec!("cp -rv #{tmpdir}/. #{targetdir}/")
-end
-FileUtils.rm_rf(repo_dir)
-
-pubdir = "/srv/www/metadata.neon.kde.org/appstream/#{TYPE}_#{DIST}"
-
-# This is the export dep11 data, we don't need it, so throw it away
-system("rm -rf #{export_data_dir}")
-# NB: We use rsync here because a) SFTP is dumb and may require copying things
-#   to tmp path, removing pubdir and moving tmpdir to pubdir, while rsync will
-#   be faster.
-remote_dir = "metadataneon@charlotte.kde.org:#{pubdir}"
-ssh_command = "ssh -o StrictHostKeyChecking=no -i #{ENV.fetch('SSH_KEY_FILE')}"
-rsync_opts = "-av -e '#{ssh_command}'"
-system("rsync #{rsync_opts} #{export_dir}/* #{remote_dir}/") || raise
+NCI::AppstreamGeneratorPush.run if $PROGRAM_NAME == __FILE__
---
+++ test/test_nci_asgen_push.rb
@@ -0,0 +1,199 @@
+# frozen_string_literal: true
+#
+# Copyright (C) 2019 Harald Sitter <sitter@kde.org>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) version 3, or any
+# later version accepted by the membership of KDE e.V. (or its
+# successor approved by the membership of KDE e.V.), which shall
+# act as a proxy defined in Section 6 of version 3 of the license.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+require_relative '../ci-tooling/test/lib/testcase'
+require_relative '../nci/asgen_push'
+
+require 'mocha/test_unit'
+require 'shellwords'
+
+module NCI
+  class AppstreamGeneratorPushTest < TestCase
+    def setup
+      Net::SFTP.expects(:start).never
+      Net::SSH.expects(:start).never
+      NCI::AppstreamGeneratorPush::RSync.expects(:sync).never
+      ENV['DIST'] = 'xenial'
+      ENV['TYPE'] = 'release'
+      ENV['APTLY_REPOSITORY'] = 'release'
+      ENV['SSH_KEY_FILE'] = 'ssh.keyfile'
+    end
+
+    def test_run_no_export
+      # Nothing generated => no pushing
+      AppstreamGeneratorPush.new.run
+    end
+    
+    class SFTPStub
+      # Note that exceptions are only allocated. They won't be functional!
+      # We get away with this because we only check if an exception was
+      # raised. Moving forward we should avoid calling methods on sftp exceptions
+      # OR revisit the cheap allocate trick.
+      # Allocated objects exist, but they have not had their initialize called.
+
+      def initialize(session:)
+        @session = session
+      end
+
+      def session
+        return @session
+      end
+      
+      def chroot(path)
+        "#{remote_dir}/#{path}"
+      end
+
+      def dir # also act as Dir, technically a different object in net::sftp though
+        self
+      end
+
+      class NameStub
+        def initialize(path)
+          @path = path
+        end
+
+        def name
+          File.basename(@path)
+        end
+
+        def symlink?
+          File.symlink?(@path)
+        end
+      end
+
+      def glob(path, pattern)
+        warn 'glob'
+        Dir.glob("#{chroot(path)}/#{pattern}") do |entry|
+          yield NameStub.new(entry)
+        end
+      end
+
+      def upload!(from, to)
+        FileUtils.cp_r(from, chroot(to), verbose: true)
+      end
+
+      def stat!(path)
+        File.stat(chroot(path))
+      rescue Errno::ENOENT => e
+        raise Net::SFTP::StatusException.allocate.exception(e.message)
+      end
+
+      def mkdir!(path)
+        Dir.mkdir(chroot(path))
+      rescue Errno::ENOENT => e
+        raise Net::SFTP::StatusException.allocate.exception(e.message)
+      end
+
+      def readlink!(path)
+        File.readlink(chroot(path))
+      rescue Errno::ENOENT => e
+        raise Net::SFTP::StatusException.allocate.exception(e.message)
+      end
+
+      def symlink!(old, new)
+        File.symlink(old, chroot(new))
+      rescue Errno::ENOENT => e
+        raise Net::SFTP::StatusException.allocate.exception(e.message)
+      end
+
+      def rename!(old, new, _flags)
+        File.rename(chroot(old), chroot(new))
+      rescue Errno::ENOENT => e
+        raise Net::SFTP::StatusException.allocate.exception(e.message)
+      end
+
+      def remove!(path)
+        system "ls -lah #{chroot(path)}"
+        FileUtils.rm(chroot(path))
+      rescue Errno::ENOENT => e
+        raise Net::SFTP::StatusException.allocate.exception(e.message)
+      end
+
+      private
+
+      def remote_dir
+        @session.remote_dir
+      end
+    end
+
+    class SSHStub
+      attr_reader :remote_dir
+
+      def initialize(remote_dir:)
+        @remote_dir = remote_dir
+        @cmd = TTY::Command.new
+      end
+
+      def exec!(cmd)
+        argv = Shellwords.split(cmd)
+        raise if argv.any? { |x| x.include?('..') }
+        argv = argv.collect { |x| x.start_with?('/') ? "#{remote_dir}/#{x}" : x }
+        @cmd.run!(*argv)
+      end
+    end
+
+    def test_run
+      remote_dir = "#{Dir.pwd}/remote"
+      
+      ssh = SSHStub.new(remote_dir: remote_dir)
+      sftp = SFTPStub.new(session: ssh)
+
+      Net::SFTP.expects(:start).at_least_once.yields(sftp)
+      # ignore this for now. hard to test and not very useful to test either
+      NCI::AppstreamGeneratorPush::RSync.expects(:sync) 
+
+      FileUtils.mkpath(remote_dir)
+      FileUtils.cp_r("#{data}/.", '.')
+      AppstreamGeneratorPush.new.run
+
+      assert_path_exist("#{remote_dir}/home/neonarchives/aptly/skel/release/dists/xenial/main/dep11/Components-amd64.yml")
+      assert_path_exist("#{remote_dir}/home/neonarchives/aptly/skel/release/dists/xenial/main/dep11/by-hash/MD5Sum/2a42a2c7a5dbd3fdb2e832aed8b2cbd5")
+      assert_path_exist("#{remote_dir}/home/neonarchives/aptly/skel/release/dists/xenial/main/dep11/by-hash/MD5Sum/Components-amd64.yml.xz")
+       # tempdir during upload
+      assert_path_not_exist("#{remote_dir}/home/neonarchives/asgen_push.release")
+    end
+
+    def test_run_old_old
+      # Has a current and an old variant already.
+      remote_dir = "#{Dir.pwd}/remote"
+      
+      ssh = SSHStub.new(remote_dir: remote_dir)
+      sftp = SFTPStub.new(session: ssh)
+
+      Net::SFTP.expects(:start).at_least_once.yields(sftp)
+      # ignore this for now. hard to test and not very useful to test either
+      NCI::AppstreamGeneratorPush::RSync.expects(:sync) 
+
+      FileUtils.cp_r("#{data}/.", '.')
+      AppstreamGeneratorPush.new.run
+
+      assert_path_exist("#{remote_dir}/home/neonarchives/aptly/skel/release/dists/xenial/main/dep11/Components-amd64.yml")
+      assert_path_exist("#{remote_dir}/home/neonarchives/aptly/skel/release/dists/xenial/main/dep11/by-hash/MD5Sum/2a42a2c7a5dbd3fdb2e832aed8b2cbd5")
+      assert_path_exist("#{remote_dir}/home/neonarchives/aptly/skel/release/dists/xenial/main/dep11/by-hash/MD5Sum/Components-amd64.yml.xz")
+      assert_path_exist("#{remote_dir}/home/neonarchives/aptly/skel/release/dists/xenial/main/dep11/by-hash/MD5Sum/Components-amd64.yml.xz.old")
+       # tempdir during upload
+      assert_path_not_exist("#{remote_dir}/home/neonarchives/asgen_push.release")
+
+      # This is a special blob which is specifically made different so
+      # it gets dropped by the blobs cleanup.
+      assert_path_not_exist("#{remote_dir}/home/neonarchives/aptly/skel/release/dists/xenial/main/dep11/by-hash/MD5Sum/e3f347cf9d52eeb49cace577d3cb1239")
+    end
+  end
+end
