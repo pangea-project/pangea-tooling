b8525c203ee79a7186a4bae3ed3ca55e0bd9cf94
Jonathan Riddell <jr@jriddell.org>
2019-03-19 20:18:24 +0000

    move stable to testing

--- ci-tooling/lib/qml_dependency_verifier.rb
+++ ci-tooling/lib/qml_dependency_verifier.rb
@@ -34,8 +34,8 @@ class QMLDependencyVerifier
     @log = Logger.new(STDOUT)
     @log.level = Logger::INFO
     @log.progname = self.class.to_s
+    repo = 'testing' if repo == 'stable'
     @repo = repo
-    @repo = 'testing' if @repo == 'stable'
   end
 
   def missing_modules
