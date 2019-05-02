75215c5f50708d89af35b1da8c1be1169d677eee
Jonathan Riddell <jr@jriddell.org>
2019-05-02 13:49:58 +0100

    update repo adding

--- nci/imager/config-hooks-neon-ko/repo.sh
+++ nci/imager/config-hooks-neon-ko/repo.sh
@@ -1,6 +1,22 @@
-gpg --no-default-keyring \
+. /etc/os-release # to get access to version_codename; NB: of host container!
+
+GPG="gpg"
+ARGS=""
+if [ "$VERSION_CODENAME" = "bionic" ]; then
+  apt install -y dirmngr gnupg1
+  ARGS="--batch --verbose"
+  GPG="gpg1"
+fi
+
+$GPG --list-keys
+$GPG \
+  $ARGS \
+  --no-default-keyring \
   --primary-keyring config/archives/ubuntu-defaults.key \
   --keyserver pool.sks-keyservers.net \
   --recv-keys '444D ABCF 3667 D028 3F89  4EDD E6D4 7362 5575 1E5D'
 echo "deb http://archive.neon.kde.org/${NEONARCHIVE} $SUITE main" >> config/archives/neon.list
 echo "deb-src http://archive.neon.kde.org/${NEONARCHIVE} $SUITE main" >> config/archives/neon.list
+
+# make sure _apt can read this file. it may get copied into the chroot
+chmod 644 config/archives/ubuntu-defaults.key || true
