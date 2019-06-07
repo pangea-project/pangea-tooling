8004ad03f0d063f58d707d1f00a442687af778d7
Scarlett Moore <sgmoore@kde.org>
2019-06-07 11:40:47 -0700

    Remove stretch for now as it is not needed in desktop release.

--- ci-tooling/test/test_dci.rb
+++ ci-tooling/test/test_dci.rb
@@ -24,14 +24,13 @@ class DCITest < TestCase
   end
 
   def test_series
-    assert_equal_collection(%w(1901 stretch next), DCI.series.keys)
-    assert_equal_collection(%w(20181001 20170617 20190606), DCI.series.values)
+    assert_equal_collection(%w(1901 next), DCI.series.keys)
+    assert_equal_collection(%w(20181001 20190606), DCI.series.values)
     assert_equal('20181001', DCI.series['1901'])
-    assert_equal('20170617', DCI.series['stretch'])
     assert_equal('20190606', DCI.series['next'])
 
     # With sorting
-    assert_equal('stretch', DCI.series(sort: :ascending).keys.first)
+    assert_equal('next', DCI.series(sort: :ascending).keys.first)
   end
 
   def test_latest_series
