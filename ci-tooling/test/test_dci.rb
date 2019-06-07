234248d087cff85eb72b12463948161238aeb465
Scarlett Moore <sgmoore@kde.org>
2019-06-07 05:56:27 -0700

    Remove backports from dci as it is unused.

--- ci-tooling/test/test_dci.rb
+++ ci-tooling/test/test_dci.rb
@@ -24,18 +24,17 @@ class DCITest < TestCase
   end
 
   def test_series
-    assert_equal_collection(%w(1901 stretch backports next), DCI.series.keys)
-    assert_equal_collection(%w(20181001 20170617 20190606 20190607), DCI.series.values)
+    assert_equal_collection(%w(1901 stretch next), DCI.series.keys)
+    assert_equal_collection(%w(20181001 20170617 20190606), DCI.series.values)
     assert_equal('20181001', DCI.series['1901'])
     assert_equal('20170617', DCI.series['stretch'])
     assert_equal('20190606', DCI.series['next'])
-    assert_equal('20190607', DCI.series['backports'])
 
     # With sorting
     assert_equal('stretch', DCI.series(sort: :ascending).keys.first)
   end
 
   def test_latest_series
-    assert_equal('backports', DCI.latest_series)
+    assert_equal('next', DCI.latest_series)
   end
 end
