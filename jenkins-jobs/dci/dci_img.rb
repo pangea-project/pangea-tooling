b6f81a7e220d6293e72ad41410e727db031dbb59
Scarlett Moore <sgmoore@kde.org>
2019-06-16 12:25:38 -0700

    Update dci image jobs to add type, update template.

---
+++ jenkins-jobs/dci/dci_img.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+require_relative 'job'
+
+class ImageJob < JenkinsJob
+  attr_reader :type
+  attr_reader :repo
+  attr_reader :release
+  attr_reader :architecture
+  attr_reader :flavor
+
+  def initialize(type:, flavor:, release:, architecture:, repo:, branch:)
+    @type = type
+    @flavor = flavor
+    @release = release
+    @architecture = architecture
+    @repo = repo
+    @branch = branch
+    super("img_#{type}_#{flavor}_#{release}_#{architecture}", 'img.xml.erb')
+  end
+end
