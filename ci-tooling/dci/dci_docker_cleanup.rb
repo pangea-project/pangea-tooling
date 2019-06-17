84b9abc0bca40eb875410b0213c534f5554bd327
Scarlett Moore <sgmoore@kde.org>
2019-06-17 15:18:25 -0700

    Add custom docker cleanup for dci as we are ending up with stale outdated images.

---
+++ ci-tooling/dci/dci_docker_cleanup.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require_relative '../../lib/docker/cleanup'
+
+Docker.options[:read_timeout] = 3 * 60 * 60 # 3 hours.
+
+Docker::Cleanup.containers
+Docker::Cleanup.images
+Docker::Cleanup.remove_image('debianci/amd64:next')
+Docker::Cleanup.remove_image('pangea/debian:next')
