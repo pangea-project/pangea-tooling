8bfb98146ae94c507645795bbfd6614e701ec6c9
Scarlett Moore <sgmoore@kde.org>
2019-06-17 09:37:01 -0700

    Add initial snapshot job and tooling for dci.

---
+++ jenkins-jobs/dci/dci_snapshot.rb
@@ -0,0 +1,19 @@
+require_relative '../job'
+
+class SnapShotJob < JenkinsJob
+  attr_reader :type
+  attr_reader :repo
+  attr_reader :release
+  attr_reader :architecture
+  attr_reader :flavor
+
+  def initialize(type:, flavor:, release:, architecture:, repo:, branch:)
+    @type = type
+    @flavor = flavor
+    @release = release
+    @architecture = architecture
+    @repo = repo
+    @branch = branch
+    super("snapshot_#{type}_#{flavor}_#{release}_#{architecture}", 'dci_snapshot.xml.erb')
+  end
+end
