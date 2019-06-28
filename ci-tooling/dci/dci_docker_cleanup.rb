c1bb8a51f9c650b5130732c6185a6b9c6ff963ea
Scarlett Moore <sgmoore@kde.org>
2019-06-28 06:55:43 -0700

    Error to info if docker image doesn't exist in dci docker cleanup.

--- ci-tooling/dci/dci_docker_cleanup.rb
+++ ci-tooling/dci/dci_docker_cleanup.rb
@@ -7,5 +7,11 @@ Docker.options[:read_timeout] = 3 * 60 * 60 # 3 hours.
 
 Docker::Cleanup.containers
 Docker::Cleanup.images
-Docker::Cleanup.remove_image(Docker::Image.get('debianci/amd64:next'))
-Docker::Cleanup.remove_image(Docker::Image.get('pangea/debian:next'))
+%w[debianci/amd64:next pangea/debian:next].each do |name|
+  begin
+    Docker::Cleanup.remove_image(Docker::Image.get(name))
+  rescue => e
+    log.info "Failed to get #{name} :: #{e}"
+    next
+  end
+end
