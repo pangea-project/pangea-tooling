6148fd7acfd6b92fc991e1eaf1ac34ec8ee0c209
Scarlett Moore <sgmoore@kde.org>
2019-07-07 08:17:42 -0700

    Fix logic in dci create repo.

--- dci/dci_create_repo.rb
+++ dci/dci_create_repo.rb
@@ -21,41 +21,42 @@ Aptly::Ext::Remote.dci do
   end
   all_repos.compact!
   @repos = []
-  @pub_repos = []
   DCI.series.each_key do |distribution|
     DCI.types.each do |type|
       file =
         "ci-tooling/data/projects/dci/#{distribution}/#{type}.yaml"
       next unless File.exist?(file)
 
+      puts distribution
+      puts type
       repo_base = YAML.load_stream(File.read(file))
       repo_base.each do |repos|
         repos.each do |_url, allrepos|
           allrepos.each.with_index do |component, _repo|
-            @repos << component.keys
+            puts component
+            component.each_key do |repo|
+              if @all_repos.include?("#{repo}-#{distribution}".gsub(/[\,\"\[\]*]/, ''))
+                puts "#{repo}-#{distribution} exists, moving on.".gsub(/[\,\"\[\]*]/, '')
+                next
+              else
+                puts "Creating #{repo}-#{distribution}".gsub(/[\,\"\[\]*]/, '')
+                # x = Aptly::Repository.create(
+                #   "#{repo}-#{distribution}".gsub(/[\,\"\[\]*]/, ''),
+                #   DefaultDistribution: "netrunner-#{distribution}",
+                #   DefaultComponent: repo.to_s.gsub(/[\,\"\[\]*]/, ''),
+                #   Architectures: %w[all amd64 armhf arm64 i386 source]
+                # )
+                # @repos << { Name: x.Name, Component: x.DefaultComponent }
+              end
+            end
           end
-        end
-      end
-      @repos.each do |repo|
-        if @all_repos.include?("#{repo}-#{distribution}".gsub(/[\,\"\[\]*]/, ''))
-          puts "#{repo}-#{distribution} exists, moving on.".gsub(/[\,\"\[\]*]/, '')
-          next
-        else
-          puts "Creating #{repo}-#{distribution}".gsub(/[\,\"\[\]*]/, '')
-          x = Aptly::Repository.create(
-            "#{repo}-#{distribution}".gsub(/[\,\"\[\]*]/, ''),
-            DefaultDistribution: "netrunner-#{distribution}",
-            DefaultComponent: repo.to_s.gsub(/[\,\"\[\]*]/, ''),
-            Architectures: %w[all amd64 armhf arm64 i386 source]
-          )
-          @pub_repos << { Name: x.Name, Component: x.DefaultComponent }
         end
       end
     end
   end
-  Aptly.publish(
-    @pub_repos,
-    'netrunner',
-    Architectures: %w[all amd64 armhf arm64 i386 source]
-  )
+  # Aptly.publish(
+  #   @repos,
+  #   'netrunner',
+  #   Architectures: %w[all amd64 armhf arm64 i386 source]
+  # )
 end
