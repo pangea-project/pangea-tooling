35c1e1da2d68f4a889e906fb1a547ed57577b6c3
Harald Sitter <sitter@kde.org>
2019-09-12 11:42:11 +0200

    also install neon-settings
    to get access to its apt preference pins

--- nci/imager/build.sh
+++ nci/imager/build.sh
@@ -41,7 +41,8 @@ sudo apt update
 sudo apt dist-upgrade -y
 sudo apt install -y --no-install-recommends \
     git ubuntu-defaults-builder wget ca-certificates zsync distro-info \
-    syslinux-utils livecd-rootfs xorriso pxz base-files lsb-release
+    syslinux-utils livecd-rootfs xorriso pxz base-files lsb-release \
+    neon-settings
 
 cd $WD
 ls -lah
