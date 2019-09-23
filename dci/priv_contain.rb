22770f82bd8ea22e1356ea1df4926be31a334e53
Scarlett Moore <sgmoore@kde.org>
2019-09-23 10:14:17 -0700

    DCI: add /dev to binds for flash loop device in priv_contain.

--- dci/priv_contain.rb
+++ dci/priv_contain.rb
@@ -13,6 +13,6 @@ c = CI::Containment.new(BUILD_TAG,
                         image: CI::PangeaImage.new(:debian, DIST),
                         privileged: true,
                         no_exit_handlers: false,
-                        binds: ["#{dir}:/pwd", "/dev:/dev"])
+                        binds: ["#{Dir.pwd}:/pwd", "/dev:/dev"])
 status_code = c.run(Cmd: ARGV)
 exit status_code
