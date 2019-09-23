20262dae5f166a619bd1ad21fc769904f20afde1
Scarlett Moore <sgmoore@kde.org>
2019-09-23 12:40:25 -0700

    Fix binds dci priv_contain.

--- dci/priv_contain.rb
+++ dci/priv_contain.rb
@@ -3,6 +3,13 @@
 
 require_relative '../lib/ci/containment'
 
+TOOLING_PATH = File.dirname(__dir__)
+binds = [
+  "#{TOOLING_PATH}:#{TOOLING_PATH}",
+  "#{Dir.pwd}:#{Dir.pwd}",
+  "/dev:/dev"
+]
+
 Docker.options[:read_timeout] = 4 * 60 * 60 # 4 hours.
 
 DIST = ENV.fetch('DIST')
@@ -13,6 +20,6 @@ c = CI::Containment.new(BUILD_TAG,
                         image: CI::PangeaImage.new(:debian, DIST),
                         privileged: true,
                         no_exit_handlers: false,
-                        binds: ["#{Dir.pwd}:/pwd", "/dev:/dev"])
+                        binds: binds)
 status_code = c.run(Cmd: ARGV)
 exit status_code
