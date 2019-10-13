df3555658281542c50001adb88b5438557445343
Scarlett Moore <sgmoore@kde.org>
2019-10-13 08:39:54 -0700

    DCI: Use customized publisher job.

---
+++ jenkins-jobs/dci/publisher.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+require_relative '../job'
+
+# publisher
+class DCIPublisherJob < JenkinsJob
+  attr_reader :type
+  attr_reader :distribution
+  attr_reader :artifact_origin
+  attr_reader :dependees
+  attr_reader :downstream_triggers
+  attr_reader :basename
+  attr_reader :repo
+  attr_reader :component
+  attr_reader :architecture
+
+  def initialize(basename, type:, distribution:, dependees:,
+                 component:, upload_map:, architecture:)
+    super("#{basename}_#{architecture}_pub", 'dci_publisher.xml.erb')
+    @type = type
+    @distribution = distribution
+    @artifact_origin = "#{basename}_bin"
+    @dependees = dependees
+    @downstream_triggers = []
+    @basename = basename
+    @component = component
+    @architecture = architecture
+
+    if upload_map
+      @repo = upload_map[component]
+      @repo ||= upload_map['default']
+    end
+  end
+
+  def append(job)
+    @downstream_triggers << job
+  end
+end
