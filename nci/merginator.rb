149267feff5c581571daa476f1f06a04f0714b42
Harald Sitter <sitter@kde.org>
2019-11-13 13:43:12 +0100

    reduce code dupe and improve version handling of merginator
    version handling is now centralized in a derivate of debian version which
    provides additional splitting of the upstream version in its (likely)
    real upstream and the debian-specific suffix (notably +dfsg).
    this allows us to only pass epoch+realupstream into mangling functions
    (where we don't want to mangle suffix or rev, becaues they are different
     across sources, so one is 5.13.2 and the other is 5.13.2+dfsg). it's
    unclear if we actually need the epoch for the mangles, kinda depends on
    whether a potential epoch would be introduced throughout Qt or only
    modules that need them. time will tell
    
    additionally the new version class allows us to only deal with this class
    and mutate as necessary (for e.g. the new_version) without having to worry
    about version building specifics anymore.
    
    all this should yield greater stability when it comes to version handling
    
    this leaves repo-handling with the greatest refactoring opportunity. i.e.
    MerginatorRepo should operate on a single repo so it encapsulates the
    state of that repo instead of having to have a huge ass run method so
    as to have all the state method-local

--- nci/merginator.rb
+++ nci/merginator.rb
@@ -32,7 +32,6 @@ require_relative '../ci-tooling/lib/nci'
 
 TARGET_BRANCH = 'Neon/release'
 
-OLDVERSION = '5.13.1'
 VERSION = '5.13.2'
 
 # not actually qt versioned:
@@ -46,6 +45,7 @@ VERSION = '5.13.2'
 #   temporary the commit says, but it doesn't look all that temporary
 # - qtwebengine way too much delta in there
 MODS = %w[
+  qtvirtualkeyboard
   qtserialport
   qtconnectivity
   qtcharts
@@ -53,7 +53,6 @@ MODS = %w[
   qtsvg
   qtgraphicaleffects
   qtquickcontrols
-  qtvirtualkeyboard
   qtspeech
   qtwebview
   qt3d
@@ -74,8 +73,63 @@ MODS = %w[
   qtbase
 ]
 
+# Version helper able to differentiate upstream from real_upstream (i.e. without
+# +dfsg suffix and the like)
+class Version < Debian::Version
+  attr_accessor :real_upstream
+  attr_accessor :real_upstream_suffix
+
+  def initialize(*)
+    super
+
+    self.upstream = super_upstream # force parsing through = method
+  end
+
+  # Returns only epoch with real upstream (i.e. without +dfsg suffix or revision)
+  def epoch_with_real_upstream
+    comps = []
+    comps << "#{epoch}:" if epoch
+    comps << real_upstream
+    comps.join
+  end
+
+  # Override to glue together real and suffix
+  alias :super_upstream :upstream
+  def upstream
+    "#{real_upstream}#{real_upstream_suffix}"
+  end
+
+  # Split upstream into real and suffix
+  def upstream=(input)
+    regex = /(?<real>[\d\.]+)(?<suffix>.*)/
+    match = input.match(regex)
+    @real_upstream = match[:real]
+    raise if @real_upstream.empty?
+    @real_upstream_suffix = match[:suffix]
+  end
+end
 
 class Merginator
+  attr_reader :logger
+  attr_reader :cmd
+  attr_reader :passed
+  attr_reader :skipped
+  attr_reader :failed
+
+  # Git instance (wrapper around git cli)
+  attr_reader :git
+  # Rugged repo (libgit2)
+  attr_reader :repo
+
+  def initialize
+    @logger = TTY::Logger.new
+    @cmd = TTY::Command.new(uuid: false)
+
+    @passed = []
+    @skipped = MODS.dup
+    @failed = []
+  end
+
   # rerere's existing merges to possibly learn how to solve problems.
   # not sure if this does much for us TBH
   def train_rerere(repo)
@@ -109,44 +163,59 @@ class Merginator
     cmd.run 'git clean -fd'
   end
 
+  def setup_repo
+    @git = Git.open(Dir.pwd, log: logger)
+    git.config('merge.dpkg-mergechangelogs.name',
+               'debian/changelog merge driver')
+    git.config('merge.dpkg-mergechangelogs.driver',
+               'dpkg-mergechangelogs -m %O %A %B %A')
+    repo_path = git.repo.path
+    FileUtils.mkpath("#{repo_path}/info")
+    File.write("#{repo_path}/info/attributes",
+               "debian/changelog merge=dpkg-mergechangelogs\n")
+    git.config('user.name', 'Neon CI')
+    git.config('user.email', 'neon@kde.org')
+    git.config('rerere.enabled', 'true')
+
+    @repo = Rugged::Repository.new(Dir.pwd)
+  end
+
+  def mangle_depends(from:, to:)
+    control = Debian::Control.new(Dir.pwd)
+    control.parse!
+    fields = %w[Build-Depends Build-Depends-Indep]
+    fields.each do |field|
+      control.source[field]&.each do |options|
+        options.each do |relationship|
+          next unless relationship.version&.start_with?(from)
+
+          relationship.version.gsub!(from, to)
+        end
+      end
+    end
+    File.write('debian/control', control.dump)
+  end
+
   def run
     Dir.mkdir('qtsies') unless File.exist?('qtsies')
     Dir.chdir('qtsies')
 
     # TODO: rewrite tagdetective to Rugged and split it to isolate the generic logic
 
-    logger = TTY::Logger.new
-    cmd = TTY::Command.new(uuid: false)
-
-    passed = []
-    skipped = MODS.dup
-    failed = []
-
     MODS.each do |mod|
       cmd.run "git clone neon:qt/#{mod}" unless File.exist?(mod)
       Dir.chdir(mod) do
         begin
-          git = Git.open(Dir.pwd, log: logger)
-          git.config('merge.dpkg-mergechangelogs.name',
-                    'debian/changelog merge driver')
-          git.config('merge.dpkg-mergechangelogs.driver',
-                    'dpkg-mergechangelogs -m %O %A %B %A')
-          repo_path = git.repo.path
-          FileUtils.mkpath("#{repo_path}/info")
-          File.write("#{repo_path}/info/attributes",
-                    "debian/changelog merge=dpkg-mergechangelogs\n")
-          git.config('user.name', 'Neon CI')
-          git.config('user.email', 'neon@kde.org')
-          git.config('rerere.enabled', 'true')
+          setup_repo
 
           cmd.run('git reset --hard')
 
           cmd.run "git checkout #{TARGET_BRANCH}"
-          oldversion, = cmd.run 'dpkg-parsechangelog -SVersion'
-          next if oldversion.start_with?(VERSION) # already bumped
+          old_version, = cmd.run 'dpkg-parsechangelog -SVersion'
+          next if old_version.start_with?(VERSION) # already bumped
+          old_version = Version.new(old_version)
 
           last_merge = nil
-          repo = Rugged::Repository.new(Dir.pwd)
           repo.walk(repo.head.target) do |commit|
             next unless commit.parents.size >= 2 # not a merge
 
@@ -172,10 +241,7 @@ class Merginator
 
           # Convert tag name to version without rev.
           last_merge_tag = last_merge
-          last_merge = Debian::Version.new(last_merge.name.split('/')[-1])
-          last_merge.revision = nil
-          last_merge.upstream.gsub!('+dfsg', '')
-          last_merge = last_merge.full
+          last_merge = Version.new(last_merge.name.split('/')[-1])
           logger.warn("last merge was #{last_merge} #{last_merge_tag.name}")
 
           cmd.run 'git checkout master'
@@ -207,50 +273,29 @@ class Merginator
             #   Much less risk of causing conflict because Control technically doesn't
             #   know how to preserve content line-for-line, it just happens to so long
             #   as the input was wrapped-and-sorted.
-            control = Debian::Control.new(Dir.pwd)
-            control.parse!
-            fields = %w[Build-Depends Build-Depends-Indep]
-            fields.each do |field|
-              control.source[field]&.each do |options|
-                options.each do |relationship|
-                  next unless relationship.version&.start_with?(OLDVERSION)
-
-                  relationship.version.gsub!(OLDVERSION, last_merge)
-                end
-              end
-            end
-            File.write('debian/control', control.dump)
+            mangle_depends(from: old_version.epoch_with_real_upstream,
+                           to: last_merge.epoch_with_real_upstream)
             git.commit('Undo depends version bump', add_all: true)
           end
 
           cmd.run "git merge #{tag}"
-          merge_version = Debian::Version.new(tag.split('/')[-1])
-          merge_version.revision = nil
-          merge_version = merge_version.full
+          merge_version = Version.new(tag.split('/')[-1])
+
+          # Construct new version from pre-existing one. This retains epoch
+          # and possibly upstream suffix
+          new_version = old_version.dup
+          new_version.real_upstream = VERSION
+          new_version.revision = "0neon"
 
           # Reapply version delta with new version.
-          control = Debian::Control.new(Dir.pwd)
-          control.parse!
-          fields = %w[Build-Depends Build-Depends-Indep]
-          fields.each do |field|
-            control.source[field]&.each do |options|
-              options.each do |relationship|
-                next unless relationship.version&.start_with?(merge_version)
+          mangle_depends(from: merge_version.epoch_with_real_upstream,
+                         to: new_version.epoch_with_real_upstream)
 
-                relationship.version.gsub!(merge_version, VERSION)
-              end
-            end
-          end
-          File.write('debian/control', control.dump)
-
-          newversion = VERSION
-          oldversion = Debian::Version.new(oldversion)
-          newversion += '+dfsg' if oldversion.upstream.end_with?('+dfsg')
           cmd.run('dch',
                   '--distribution', NCI.current_series,
-                  '--newversion', "#{newversion}-0neon",
-                  "New release #{VERSION}")
-          git.commit("[TR] New release #{VERSION}", add_all: true)
+                  '--newversion', "#{new_version}",
+                  "New release #{new_version.real_upstream}")
+          git.commit("[TR] New release #{new_version.real_upstream}", add_all: true)
 
           passed << mod
         rescue TTY::Command::ExitError => e
@@ -260,8 +305,9 @@ class Merginator
       end
     end
 
-    skipped -= passed
-    skipped -= failed
+    # skipped is a read only attribute, we need to assign the var directly!
+    @skipped -= passed
+    @skipped -= failed
 
     logger.info "Processed: #{passed.join("\n")}"
     logger.info "Skipped: #{skipped.join("\n")}"
@@ -269,4 +315,6 @@ class Merginator
   end
 end
 
-Merginator.new.run if $PROGRAM_NAME == __FILE__
+if $PROGRAM_NAME == __FILE__
+  Merginator.new.run
+end
