0ba9313807fd771af3a8f9b75ac715df23819cd6
Scarlett Moore <sgmoore@kde.org>
2019-12-14 08:00:43 -0700

    Fix docker cleanup dci.

--- ci-tooling/dci/dci_docker_cleanup.rb
+++ ci-tooling/dci/dci_docker_cleanup.rb
@@ -6,12 +6,12 @@ Docker.options[:read_timeout] = 3 * 60 * 60 # 3 hours.
 
 Docker::Cleanup.containers
 Docker::Cleanup.images
-log = Docker::Cleanup.log
-# %w[debianci/amd64:next pangea/debian:next debianci/amd64:2001 pangea/debian:2001].each do |name|
-#   begin
-#     Docker::Cleanup.remove_image(Docker::Image.get(name))
-#   rescue => e
-#     log.info "Failed to get #{name} :: #{e}"
-#     next
-#   end
-end
+# log = Docker::Cleanup.log
+# # %w[debianci/amd64:next pangea/debian:next debianci/amd64:2001 pangea/debian:2001].each do |name|
+# #   begin
+# #     Docker::Cleanup.remove_image(Docker::Image.get(name))
+# #   rescue => e
+# #     log.info "Failed to get #{name} :: #{e}"
+# #     next
+# #   end
+# end
