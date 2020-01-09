8f488802ceaf9cde3391020bbb8681e6005f46b5
Bhushan Shah <bhush94@gmail.com>
2020-01-09 08:56:03 +0000

    Introduces devices and architectures_for_device array in mci

--- ci-tooling/lib/mobilekci.rb
+++ ci-tooling/lib/mobilekci.rb
@@ -4,4 +4,12 @@ require_relative 'xci'
 # Plasma Mobile Kubuntu CI specific data.
 module MCI
   extend XCI
+  module_function
+  def architectures_for_device
+    data['architectures_for_device']
+  end
+
+  def devices
+    data['devices']
+  end
 end
