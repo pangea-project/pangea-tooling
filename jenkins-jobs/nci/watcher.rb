64c7f784035cbb1aa43ade44aa4977971f957462
Jonathan Riddell <jr@jriddell.org>
2020-02-25 12:17:57 +0000

    plasma is on an lts so merge into lts branch

--- jenkins-jobs/nci/watcher.rb
+++ jenkins-jobs/nci/watcher.rb
@@ -43,7 +43,7 @@ class WatcherJob < JenkinsJob
                             'neon@git.neon.kde.org:')
     # Don't touch release-lts for Plasma jobs
     if KDEProjectsComponent.plasma_jobs.include? project.name
-      @scm_writable.branch.replace('Neon/release')
+      @scm_writable.branch.replace('Neon/release-lts')
     else
       @scm_writable.branch.replace('Neon/release-lts')
     end
