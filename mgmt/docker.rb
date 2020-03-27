fcf0eddfdbe042dcbe8f8f905529903225bded07
Harald Sitter <sitter@kde.org>
2020-03-27 11:56:28 +0100

    try to log each process to a separate file for mgmt docker
    there's not really a good way to test this so I'll just have to try in
    production. not that it makes a diff because dockering fails right now
    for unknown reasons -.-

--- mgmt/docker.rb
+++ mgmt/docker.rb
@@ -22,8 +22,11 @@ ubuntu_series = [] if ENV.include?('PANGEA_DEBIAN_ONLY')
 ubuntu_series.each_index do |index|
   series = ubuntu_series[index]
   origins = ubuntu_series[index + 1..-1]
-  warn "building ubuntu #{series}"
+  log_path = "#{Dir.pwd}/ubuntu-#{series}.log"
+  warn "building ubuntu #{series}; logging to #{log_path}"
   pid = fork do
+    $stdout.reopen(log_path, 'a')
+    $stderr.reopen(log_path, 'a')
     d = MGMT::Deployer.new('ubuntu', series, origins)
     d.run!
   end
@@ -34,8 +37,11 @@ end
 debian_series = DCI.series.keys
 debian_series = [] if ENV.include?('PANGEA_UBUNTU_ONLY')
 debian_series.each do |k|
-  warn "building debian #{k}"
+  log_path = "#{Dir.pwd}/debian-#{series}.log"
+  warn "building debian #{k}; logging to #{log_path}"
   pid = fork do
+    $stdout.reopen(log_path, 'a')
+    $stderr.reopen(log_path, 'a')
     d = MGMT::Deployer.new('debian', k)
     d.run!
   end
