98f94c6b14a753497752ebcbd4aeccc7e56810c2
Harald Sitter <sitter@kde.org>
2020-04-09 16:01:34 +0200

    make keyrings
    the previous export actually didn't work properly. import into proper
    standalone keyrings. this also works for multiple keys now

--- nci/imager/config-hooks-xenon-mycroft/repo.sh
+++ nci/imager/config-hooks-xenon-mycroft/repo.sh
@@ -3,14 +3,12 @@ apt install -y dirmngr gnupg1
 ARGS="--batch --verbose"
 GPG="gpg1"
 
-$GPG --list-keys
-
-$GPG \
+apt-key export '444D ABCF 3667 D028 3F89  4EDD E6D4 7362 5575 1E5D' | $GPG \
   $ARGS \
   --no-default-keyring \
   --primary-keyring config/archives/ubuntu-defaults.key \
-  --keyserver pool.sks-keyservers.net \
-  --recv-keys '444D ABCF 3667 D028 3F89  4EDD E6D4 7362 5575 1E5D'
+  --import
+
 echo "deb http://archive.neon.kde.org/${NEONARCHIVE} $SUITE main" >> config/archives/neon.list
 echo "deb-src http://archive.neon.kde.org/${NEONARCHIVE} $SUITE main" >> config/archives/neon.list
 
