7a5b10e6f2a7f08dadc96213d4eb19c6dee9a465
Harald Sitter <sitter@kde.org>
2020-04-09 15:35:34 +0200

    imager: export neon key where possible instead of download
    we already have the key in the image, just fish it out of there. this
    makes large parts of this independent of server side or network problems
    
    unfortunately mobiley things use the file as an actual keyring, so
    I guess they'll need to export and pipe into gpg --import intead

--- nci/imager/config-hooks-neon/repo.sh
+++ nci/imager/config-hooks-neon/repo.sh
@@ -1,17 +1,6 @@
-# Use gpg1, mostly because we are lazy and don't know how to best port this to v2
-apt install -y dirmngr gnupg1
-ARGS="--batch --verbose"
-GPG="gpg1"
-
-$GPG --list-keys
-$GPG \
-  $ARGS \
-  --no-default-keyring \
-  --primary-keyring config/archives/ubuntu-defaults.key \
-  --keyserver pool.sks-keyservers.net \
-  --recv-keys '444D ABCF 3667 D028 3F89  4EDD E6D4 7362 5575 1E5D'
-echo "deb http://archive.neon.kde.org/${NEONARCHIVE} $SUITE main" >> config/archives/neon.list
-echo "deb-src http://archive.neon.kde.org/${NEONARCHIVE} $SUITE main" >> config/archives/neon.list
-
+apt-key export '444D ABCF 3667 D028 3F89  4EDD E6D4 7362 5575 1E5D' > config/archives/ubuntu-defaults.key
 # make sure _apt can read this file. it may get copied into the chroot
 chmod 644 config/archives/ubuntu-defaults.key || true
+
+echo "deb http://archive.neon.kde.org/${NEONARCHIVE} $SUITE main" >> config/archives/neon.list
+echo "deb-src http://archive.neon.kde.org/${NEONARCHIVE} $SUITE main" >> config/archives/neon.list
--- nci/imager/config-hooks-plasma-wayland/repo.sh
+++ nci/imager/config-hooks-plasma-wayland/repo.sh
@@ -1,6 +1,6 @@
-gpg --no-default-keyring \
-  --primary-keyring config/archives/ubuntu-defaults.key \
-  --keyserver pool.sks-keyservers.net \
-  --recv-keys '444D ABCF 3667 D028 3F89  4EDD E6D4 7362 5575 1E5D'
+apt-key export '444D ABCF 3667 D028 3F89  4EDD E6D4 7362 5575 1E5D' > config/archives/ubuntu-defaults.key
+# make sure _apt can read this file. it may get copied into the chroot
+chmod 644 config/archives/ubuntu-defaults.key || true
+
 echo "deb http://archive.neon.kde.org/${NEONARCHIVE} $SUITE main" >> config/archives/neon.list
 echo "deb-src http://archive.neon.kde.org/${NEONARCHIVE} $SUITE main" >> config/archives/neon.list
