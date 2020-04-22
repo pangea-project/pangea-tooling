cddaa8b612ec4131aaf853aa48f9d549ac111924
Jonathan Riddell <jr@jriddell.org>
2020-04-22 11:19:46 +0100

    kcalcore is still not yet called kcalendarcore

--- lib/kdeproject_component.rb
+++ lib/kdeproject_component.rb
@@ -25,7 +25,7 @@ require 'httparty'
 
 class KDEProjectsComponent
   class << self
-    @@projects_to_jobs = {'discover'=>'plasma-discover'}
+    @@projects_to_jobs = {'discover'=>'plasma-discover', 'kcalendarcore'=>'kcalcore'}
     @@projects_without_jobs = ['plasma-tests']
 
     def frameworks
