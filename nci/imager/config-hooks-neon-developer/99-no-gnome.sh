7783ffbf72fb26961c4d896f1f37a2ac49c78c78
Jonathan Riddell <jr@jriddell.org>
2020-08-10 11:34:42 +0100

    remove gnome

---
+++ nci/imager/config-hooks-neon-developer/99-no-gnome.sh
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2020 Jonathan Riddell <jr@jriddell.org>
+# SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
+
+apt remove -y gnome-shell ubuntu-session
