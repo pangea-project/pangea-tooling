648cbc5f26b218dd34547a3245720526c81f7cb8
Harald Sitter <sitter@kde.org>
2020-09-28 07:15:14 +0200

    move away from legacy job_name file
    this was a hack originally meant to give easy access to
    out-of-containment info. this has long since been replaced with
    automatic env forwarding in contain.rb so we can now rely on JOB_NAME
    env properly

--- ci-tooling/nci/lib/settings.rb
+++ ci-tooling/nci/lib/settings.rb
@@ -1,22 +1,7 @@
 # frozen_string_literal: true
-#
-# Copyright (C) 2016 Harald Sitter <sitter@kde.org>
-#
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public
-# License as published by the Free Software Foundation; either
-# version 2.1 of the License, or (at your option) version 3, or any
-# later version accepted by the membership of KDE e.V. (or its
-# successor approved by the membership of KDE e.V.), which shall
-# act as a proxy defined in Section 6 of version 3 of the license.
-#
-# This library is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public
-# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+# SPDX-FileCopyrightText: 2016-2020 Harald Sitter <sitter@kde.org>
+# SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
 
 require 'deep_merge'
 require 'yaml'
@@ -48,12 +33,13 @@ module NCI
 
     def for_job
       unless job?
-        puts 'Could not determine job_name. Stamp file missing'
+        puts 'Could not determine job_name. ENV is missing JOB_NAME'
         return {}
       end
       job_patterns = CI::FNMatchPattern.filter(job, settings)
       job_patterns = CI::FNMatchPattern.sort_hash(job_patterns)
       return {} if job_patterns.empty?
+
       merge(job_patterns)
     end
 
@@ -70,12 +56,11 @@ module NCI
     end
 
     def job?
-      @job_exist ||= File.exist?('job_name')
+      @job_exist ||= ENV.key?('JOB_NAME')
     end
 
     def job
-      job? # Make sure exist has been chached.
-      @job ||= File.read('job_name').strip
+      @job ||= ENV.fetch('JOB_NAME').strip
     end
 
     def settings
--- ci-tooling/test/test_nci_settings.rb
+++ ci-tooling/test/test_nci_settings.rb
@@ -1,22 +1,7 @@
 # frozen_string_literal: true
-#
-# Copyright (C) 2016 Harald Sitter <sitter@kde.org>
-#
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public
-# License as published by the Free Software Foundation; either
-# version 2.1 of the License, or (at your option) version 3, or any
-# later version accepted by the membership of KDE e.V. (or its
-# successor approved by the membership of KDE e.V.), which shall
-# act as a proxy defined in Section 6 of version 3 of the license.
-#
-# This library is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public
-# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+# SPDX-FileCopyrightText: 2016-2020 Harald Sitter <sitter@kde.org>
+# SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
 
 require_relative 'lib/testcase'
 require_relative '../nci/lib/settings'
@@ -38,7 +23,7 @@ class NCISettingsTest < TestCase
 
   def test_settings
     NCI::Settings.default_files << fixture_file('.yml')
-    File.write('job_name', 'xenial_unstable_libkolabxml_src')
+    ENV['JOB_NAME'] = 'xenial_unstable_libkolabxml_src'
     settings = NCI::Settings.new
     settings = settings.for_job
     assert_equal({"sourcer"=>{"restricted_packaging_copy"=>true}}, settings)
@@ -46,7 +31,7 @@ class NCISettingsTest < TestCase
 
   def test_settings_singleton
     NCI::Settings.default_files << fixture_file('.yml')
-    File.write('job_name', 'xenial_unstable_libkolabxml_src')
+    ENV['JOB_NAME'] = 'xenial_unstable_libkolabxml_src'
     assert_equal({"sourcer"=>{"restricted_packaging_copy"=>true}}, NCI::Settings.for_job)
   end
 
