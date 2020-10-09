12eaf0024bc6755b2a0cacc02d5fe4b5b5c5a0b5
Harald Sitter <sitter@kde.org>
2020-10-09 10:49:17 +0200

    make sure to require gitlab
    not sure why but it was failing for Bhushan

--- ci-tooling/lib/projects/factory/neon.rb
+++ ci-tooling/lib/projects/factory/neon.rb
@@ -3,6 +3,7 @@
 # SPDX-FileCopyrightText: 2016-2020 Harald Sitter <sitter@kde.org>
 # SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
 
+require 'gitlab'
 require 'tty/command'
 
 require_relative 'base'
