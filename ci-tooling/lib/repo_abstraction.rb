54c1770ba0b5467c9ff56112675877ff00907a5e
Harald Sitter <sitter@kde.org>
2020-10-20 15:49:27 +0200

    style++

--- ci-tooling/lib/repo_abstraction.rb
+++ ci-tooling/lib/repo_abstraction.rb
@@ -1,22 +1,7 @@
 # frozen_string_literal: true
-#
-# Copyright (C) 2014-2016 Harald Sitter <sitter@kde.org>
-#
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public
-# License as published by the Free Software Foundation; either
-# version 2.1 of the License, or (at your option) version 3, or any
-# later version accepted by the membership of KDE e.V. (or its
-# successor approved by the membership of KDE e.V.), which shall
-# act as a proxy defined in Section 6 of version 3 of the license.
-#
-# This library is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public
-# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+# SPDX-FileCopyrightText: 2014-2016 Harald Sitter <sitter@kde.org>
+# SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
 
 require_relative 'apt'
 require_relative 'aptly-ext/filter'
@@ -70,6 +55,7 @@ class Repository
   def add
     repo = Apt::Repository.new(@_name)
     return true if repo.add && update_with_retry
+
     remove
     false
   end
@@ -77,12 +63,14 @@ class Repository
   def remove
     repo = Apt::Repository.new(@_name)
     return true if repo.remove && update_with_retry
+
     false
   end
 
   def install
     @log.info "Installing PPA #{@_name}."
     return false if packages.empty?
+
     pin!
     args = %w[ubuntu-minimal]
     # Map into install expressions, value can be nil so compact and join
@@ -90,6 +78,7 @@ class Repository
     # was nil.
     args += packages.map do |k, v|
       next '' if install_excluded?(k)
+
       [k, v].compact.join('=')
     end
     Apt.install(args)
@@ -98,6 +87,7 @@ class Repository
   def purge
     @log.info "Purging PPA #{@_name}."
     return false if packages.empty?
+
     Apt.purge(packages.keys.delete_if { |x| purge_excluded?(x) })
   end
 
@@ -249,9 +239,11 @@ class RootOnAptlyRepository < Repository
   def dbus_session
     lines = `dbus-launch --sh-syntax`
     raise unless $?.success?
+
     lines = lines.strip.split($/)
     env = lines.collect do |line|
       next unless line.include?('=')
+
       data = line.split('=', 2)
       data[1] = Shellwords.split(data[1]).join.chomp(';')
       data
