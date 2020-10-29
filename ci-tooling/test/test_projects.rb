710a0275fe66ad34b1745402552325e76063f8d0
Harald Sitter <sitter@kde.org>
2020-10-29 13:08:47 +0100

    style++

--- ci-tooling/test/test_projects.rb
+++ ci-tooling/test/test_projects.rb
@@ -1,22 +1,7 @@
 # frozen_string_literal: true
-#
-# Copyright (C) 2015-2016 Harald Sitter <sitter@kde.org>
-#
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public
-# License as published by the Free Software Foundation; either
-# version 2.1 of the License, or (at your option) version 3, or any
-# later version accepted by the membership of KDE e.V. (or its
-# successor approved by the membership of KDE e.V.), which shall
-# act as a proxy defined in Section 6 of version 3 of the license.
-#
-# This library is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public
-# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+# SPDX-FileCopyrightText: 2015-2020 Harald Sitter <sitter@kde.org>
+# SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
 
 require 'fileutils'
 require 'tmpdir'
@@ -465,9 +450,9 @@ class ProjectTest < TestCase
     component = 'componento'
 
     require_relative '../lib/nci'
-    NCI.stubs(:current_series).returns("bionic")
-    NCI.stubs(:future_series).returns("focal")
-    gitrepo = create_fake_git(name: name, component: component, branches: %w(Neon/unstable_focal))
+    NCI.stubs(:current_series).returns('bionic')
+    NCI.stubs(:future_series).returns('focal')
+    gitrepo = create_fake_git(name: name, component: component, branches: %w[Neon/unstable_focal])
     assert_not_nil(gitrepo)
     assert_not_equal(gitrepo, '')
 
