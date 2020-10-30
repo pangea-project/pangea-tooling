c0f10f3a4e1d47e489207798b074bb6632a2f44f
Harald Sitter <sitter@kde.org>
2020-10-30 14:19:12 +0100

    try to remove colord from iso
    I'm actually not a 100% confident this is going to work but from looking
    at the logs the package list is installed in a single apt-get call.
    
    as such we should be able to mark a package from the set for removal
    (i.e. not getting it dragged in at all)

--- nci/imager/config-hooks-neon/20_package_list.sh
+++ nci/imager/config-hooks-neon/20_package_list.sh
@@ -2,10 +2,17 @@
 # terminal | xterm and doesn't know terminal is installed later in the tree.
 # Also explicitly install the efi image packages explicitly so live-build
 # can find them for extraction into the ISO.
+# colord gets removed because it would get dragged in by cups but after
+# discussion with Rohan Garg I've come to the conclusion that colord makes
+# no sense by default. If the user wants to do color profile management, sure,
+# but this is a very specific desire usually requiring very specific hardware
+# to perform the calibration. Without a profile colord adds no value so
+# we may as well not ship it by default.
 cat << EOF > config/package-lists/ubuntu-defaults.list.chroot_install
 shim-signed
 grub-efi-amd64-signed
 grub-efi-ia32-bin
 konsole
 neon-desktop
+colord-
 EOF
