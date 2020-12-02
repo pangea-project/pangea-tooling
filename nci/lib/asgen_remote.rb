433f2a5b5116e3b5d49f3fcff79c6ff1050e3069
Harald Sitter <sitter@kde.org>
2020-12-02 14:28:26 +0100

    make sure the module is defined
    lest ruby falls over because it doesn't know what NCI is, which can
    entirely happen considering the file has no other requires that would
    define the module

--- nci/lib/asgen_remote.rb
+++ nci/lib/asgen_remote.rb
@@ -4,29 +4,31 @@
 # SPDX-FileCopyrightText: 2016-2020 Harald Sitter <sitter@kde.org>
 # SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
 
-class NCI::AppstreamGeneratorRemote
-  def dist
-    ENV.fetch('DIST')
-  end
+module NCI
+  class AppstreamGeneratorRemote
+    def dist
+      ENV.fetch('DIST')
+    end
 
-  def type
-    ENV.fetch('TYPE')
-  end
+    def type
+      ENV.fetch('TYPE')
+    end
 
-  def run_dir
-    @run_dir ||= File.absolute_path('run')
-  end
+    def run_dir
+      @run_dir ||= File.absolute_path('run')
+    end
 
-  def export_dir
-    "#{run_dir}/export"
-  end
+    def export_dir
+      "#{run_dir}/export"
+    end
 
-  def export_dir_data
-    "#{run_dir}/export/data"
-  end
+    def export_dir_data
+      "#{run_dir}/export/data"
+    end
 
-  def rsync_pubdir_expression
-    pubdir = "/srv/www/metadata.neon.kde.org/appstream/#{type}_#{dist}"
-    "metadataneon@charlotte.kde.org:#{pubdir}"
+    def rsync_pubdir_expression
+      pubdir = "/srv/www/metadata.neon.kde.org/appstream/#{type}_#{dist}"
+      "metadataneon@charlotte.kde.org:#{pubdir}"
+    end
   end
 end
