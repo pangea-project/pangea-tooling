d96f12e49d24c2c5aaa1e3898f3b6f8fa06640d4
Harald Sitter <sitter@kde.org>
2021-01-12 13:09:10 +0100

    exclude debug-installer from watching
    it's built from source directly so as to get quick turnaround on
    translations

--- ci-tooling/nci/watcher.rb
+++ ci-tooling/nci/watcher.rb
@@ -8,6 +8,7 @@
 
 require_relative 'lib/watcher'
 require_relative 'lib/setup_env'
+require_relative '../lib/debian/changelog'
 require_relative '../lib/debian/source'
 
 # TODO: we should still detect when ubuntu versions diverge I guess?
@@ -15,6 +16,20 @@ require_relative '../lib/debian/source'
 #   a whole
 if Debian::Source.new(Dir.pwd).format.type == :native
   puts 'This is a native source. Nothing to do!'
+  exit 0
+end
+
+# Special exclusion list. For practical reasons we have kind of neon-specific
+# sources that aren't built from tarballs but rather from git directly.
+# Sources in this list MUST be using KDE_L10N_SYNC_TRANSLATIONS AND have a
+# gitish watch file or none at all.
+# Changes to these requiremenst MUST be discussed with the team!
+source_name = Debian::Changelog.new(Dir.pwd).name
+if %w[drkonqi-pk-debug-installer].include?(source_name) &&
+   File.read('debian/rules').include?('KDE_L10N_SYNC_TRANSLATIONS') &&
+   (!File.exist?('debian/watch') ||
+    File.read('debian/watch').include?('mode=git'))
+  puts 'This is a neon-ish source built from git despite being in release/.'
   exit 0
 end
 
