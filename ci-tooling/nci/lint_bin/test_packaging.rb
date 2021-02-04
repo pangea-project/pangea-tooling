7d763a7f08e0559ad4643844a31aecf4fbc0e273
Harald Sitter <sitter@kde.org>
2021-02-04 11:14:37 +0100

    try to add source linting to packaging test
    as it turns out the main directory would contain the source changes file
    (and shouldn't contain any other changes files) so we should be able to
    lintian in the _bin job still, but at least try to restrict it to amd64
    
    otherwise we'd have to do an entire dance since _src lacks all the
    infrastructure to run lints :(

--- ci-tooling/nci/lint_bin/test_packaging.rb
+++ ci-tooling/nci/lint_bin/test_packaging.rb
@@ -2,6 +2,7 @@
 # SPDX-FileCopyrightText: 2016-2021 Harald Sitter <sitter@kde.org>
 # SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
 
+require_relative '../../lib/dpkg'
 require_relative '../../lib/lint/control'
 require_relative '../../lib/lint/lintian'
 require_relative '../../lib/lint/merge_marker'
@@ -12,28 +13,39 @@ require_relative '../lib/lint/result_test'
 module Lint
   # Test static files.
   class TestPackaging < ResultTest
-    def setup
-      @dir = 'build'
+    def self.arch_all?
+      DPKG::HOST_ARCH == 'amd64'
     end
 
-    %i[Control Series Symbols].each do |klass_name|
+    def setup
+      @dir = 'build' # default dir
+      # dir override per class
+      @klass_to_dir = {
+        Lintian: '.' # lint on the source's changes
+      }
+    end
+
+    %i[Control Series Symbols Lintian].each do |klass_name|
       # Because this is invoked as a kind of blackbox test we'd have a really
       # hard time of testing lintian without either tangling the test up
       # with the build test or adding binary artifacts to the repo. I dislike
       # both so lets assume Lintian doesn't mess up its two function
       # API.
       next if ENV['PANGEA_TEST_NO_LINTIAN'] && klass_name == :Lintian
+      # only run source lintian on amd64, the source is the same across arches.
+      next if klass_name == :Lintian && !arch_all?
 
       %w[warnings informations errors].each do |meth_type|
         class_eval <<-RUBY, __FILE__, __LINE__ + 1
           def test_#{klass_name.downcase}_#{meth_type}
             log_klass = #{klass_name}
             assert_meth = "assert_#{meth_type}".to_sym
+            dir = @klass_to_dir.fetch(klass_name, @dir)
 
             # NB: test-unit runs each test in its own instance, this means we
             # need to use a class variable as otherwise the cache wouldn't
             # persiste across test_ invocations :S
-            result = @@result_#{klass_name} ||= log_klass.new(@dir).lint
+            result = @@result_#{klass_name} ||= log_klass.new(dir).lint
             send(assert_meth, result)
           end
         RUBY
