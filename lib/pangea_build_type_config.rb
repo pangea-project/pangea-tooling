3832a80eca0a57e8c0e665a1dc6dcfd4f7ea7b0e
Harald Sitter <sitter@kde.org>
2021-02-12 13:09:56 +0100

    change build type api to abstract type decision
    this allows us to more generally do a check on whether release build has
    been switched on or not. specifically since we strip flags from dpkg
    when doing release builds the ddebs would be empty and cause a mountain
    of lintian warnings. to avoid that we can simply skip ddeb generation as
    a whole

--- lib/pangea_build_type_config.rb
+++ lib/pangea_build_type_config.rb
@@ -1,20 +1,26 @@
 # frozen_string_literal: true
 
-# SPDX-FileCopyrightText: 2020 Harald Sitter <sitter@kde.org>
+# SPDX-FileCopyrightText: 2020-2021 Harald Sitter <sitter@kde.org>
 # SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
 
 # Whether to mutate build type away from debian native.
 module PangeaBuildTypeConfig
   class << self
-    def release_lts?
-      ENV.fetch('TYPE', '') == 'release-lts'
-    end
-
+    # Whether to override the build type at all (i.e. strip dpkg-buildflags)
     def override?
       enabled? && ubuntu? && (release_lts? || arm?)
     end
 
+    # Whether this build should be run as release build (i.e. no ddebs or symbols)
+    def release_build?
+      override? && release_lts?
+    end
+
     private
+
+    def release_lts?
+      ENV.fetch('TYPE', '') == 'release-lts'
+    end
 
     def enabled?
       !ENV.key?('PANGEA_NO_BUILD_TYPE')
