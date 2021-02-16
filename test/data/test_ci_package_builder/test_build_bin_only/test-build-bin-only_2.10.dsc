dd7b67f72e60e7c21dc13d33b0209b6eedfc1e31
Harald Sitter <sitter@kde.org>
2021-02-16 13:55:36 +0100

    don't build the test source on demand
    we can deal with this binary blob. makes the tests fasters!

---
+++ test/data/test_ci_package_builder/test_build_bin_only/test-build-bin-only_2.10.dsc
@@ -0,0 +1,18 @@
+Format: 3.0 (native)
+Source: test-build-bin-only
+Binary: test-build-bin-only
+Architecture: any
+Version: 2.10
+Maintainer: Santiago Vila <sanvila@debian.org>
+Homepage: http://www.gnu.org/software/hello/
+Standards-Version: 3.9.6
+Build-Depends: debhelper (>= 9.20120311)
+Build-Depends-Indep: kitten-dep-we-can-skip
+Package-List:
+ test-build-bin-only deb devel optional arch=any
+Checksums-Sha1:
+ b9c39b34a7653a7dd2c30b6074ce0fa2b3dd2b11 1038 test-build-bin-only_2.10.tar.gz
+Checksums-Sha256:
+ 3772f3d753bea06d0280bb31bc4d58253c87a28e3eef242290e98d85de6289e3 1038 test-build-bin-only_2.10.tar.gz
+Files:
+ 5779c9d6ac84319e25c5f54a4687efc8 1038 test-build-bin-only_2.10.tar.gz
--- test/test_ci_package_builder.rb
+++ test/test_ci_package_builder.rb
@@ -1,23 +1,7 @@
 # frozen_string_literal: true
-#
-# Copyright (C) 2016 Harald Sitter <sitter@kde.org>
-# Copyright (C) 2015 Rohan Garg <rohan@kde.org>
-#
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public
-# License as published by the Free Software Foundation; either
-# version 2.1 of the License, or (at your option) version 3, or any
-# later version accepted by the membership of KDE e.V. (or its
-# successor approved by the membership of KDE e.V.), which shall
-# act as a proxy defined in Section 6 of version 3 of the license.
-#
-# This library is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public
-# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+# SPDX-FileCopyrightText: 2016-2021 Harald Sitter <sitter@kde.org>
+# SPDX-FileCopyrightText: 2015 Rohan Garg <rohan@kde.org>
+# SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
 
 require_relative '../lib/ci/package_builder'
 require_relative '../lib/debian/changes'
@@ -121,8 +105,6 @@ module CI
     def test_build_bin_only
       FileUtils.cp_r("#{data}/.", Dir.pwd)
 
-      Dir.chdir('build') { system('dpkg-buildpackage -S -us -uc') }
-
       # Disable automatic bin only based on architecture. (i.e. amd64 is arch
       # all so it could be bin only by default, but for this test we want
       # to test bin_only detection, so the arch based bin only is getting in
@@ -188,8 +170,6 @@ module CI
       # bin-only gets auto enabled for a !arch_all architecture (arm64)
 
       FileUtils.cp_r("#{data}/.", Dir.pwd)
-
-      Dir.chdir('build') { system('dpkg-buildpackage -S -us -uc') }
 
       DPKG.stubs(:architecture).returns('arm64')
 
