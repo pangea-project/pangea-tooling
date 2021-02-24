16ab4d796f51010d432bdb3cfe447f87401c4ca6
Scarlett Moore <sgmoore@kde.org>
2021-02-24 14:42:48 -0700

    Fix docker cleanup path in dci.

--- dci/dci_docker_cleanup.rb
+++ dci/dci_docker_cleanup.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
-require_relative '../../lib/docker/cleanup'
+require_relative '../lib/docker/cleanup'
 
 Docker.options[:read_timeout] = 3 * 60 * 60 # 3 hours.
 
