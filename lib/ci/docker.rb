8f0e4ccc4be082cdd83a7bfc3f1cf34680883a8c
Harald Sitter <sitter@kde.org>
2021-03-10 11:09:45 +0100

    add a new compat override
    docker-api 2.1 adds a version check that we absolutely don't need in our
    lives as it breaks VCR recordings -.-

--- lib/ci/docker.rb
+++ lib/ci/docker.rb
@@ -5,6 +5,14 @@
 
 require 'docker'
 
+class Docker::Connection
+  def podman?
+    # We don't use podman. Meanwhile docker-api 2.1 would make a /version request in this method that'd
+    # require re-recording of our VCR cassettes. Instead always return false.
+    false
+  end
+end
+
 # Reset connection in order to pick up any connection options one might set
 # after requiring this file
 Docker.reset_connection!
