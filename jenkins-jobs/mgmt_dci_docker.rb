34571e33819cb993bf8b1cf4bad582002ec70c62
Scarlett Moore <sgmoore@kde.org>
2021-03-16 10:46:32 -0700

    Dump dci docker cleanup overrides, old cruft no longer necessary. Update templates while in there.

---
+++ jenkins-jobs/mgmt_dci_docker.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+require_relative 'job'
+
+# Deploy job
+class MGMTDockerJob < JenkinsJob
+  attr_reader :dependees
+
+  def initialize(dependees:)
+    name = 'mgmt_dci_docker'
+    super(name, "#{name}.xml.erb")
+    @dependees = dependees.collect(&:job_name)
+  end
+end
