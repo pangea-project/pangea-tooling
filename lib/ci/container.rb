d39e34e4be19b0d4c599fabbc567a5a45e0dafd4
Harald Sitter <sitter@kde.org>
2021-05-19 17:10:15 +0200

    bump ulimit and hijack the binary
    a) qtwebengine calls ulimit unconditionally
    b) qtwebengine fails to build cause ulimit may not be permitted in all
    our dockers
    ...
    c) overlay ulimit with /bin/true
    d) bump our overall limit to 4096 (what QWE has)
    
    should this not be enough in the future we can always revisit this
    approach. it is a bit hacky, but then I am fairly sick of QWE weirdness
    and conceptually speaking we any package calling ulimit can sod right
    off, so /bin/true it is.

--- lib/ci/container.rb
+++ lib/ci/container.rb
@@ -75,7 +75,7 @@ module CI
             # https://bugs.launchpad.net/ubuntu/+source/apt/+bug/1332440
             # This in particular affects apt-extracttemplates which will take up
             # to 20 minutes where it should take maybe 1/10 of that.
-            Ulimits: [{ Name: 'nofile', Soft: 1024, Hard: 1024 }],
+            Ulimits: [{ Name: 'nofile', Soft: 4096, Hard: 4096 }],
             # Disable seccomp. Qt 5.10 onwards prefers to use the statx syscall
             # of Linux (4.11+). This syscall is however not whitelisted in the
             # standard seccomp profile of docker. Furthermore, we cannot set
