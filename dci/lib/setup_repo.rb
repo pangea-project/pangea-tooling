e9d7f15742af9e4436e3d8b60220e7d5d00730a2
Scarlett Moore <sgmoore@kde.org>
2021-06-02 16:21:46 -0700

    Update ENV vars in setup_repo, also fix components and use correct prefix. Use debian_release to get the codename properly into backports ( release is already used.

--- dci/lib/setup_repo.rb
+++ dci/lib/setup_repo.rb
@@ -24,26 +24,27 @@ require_relative '../../lib/apt'
 require_relative '../../lib/dpkg'
 require_relative '../../lib/os'
 require_relative '../../lib/retry'
+require_relative '../../lib/dci'
 
-# Mobile CI specific helpers.
+# DCI specific helpers.
 module DCI
   module_function
 
   def setup_repo!
-    @dist = ENV.fetch('DIST_RELEASE')
-    @series = ENV.fetch('DIST_RELEASE')
-    repos = []
+    @series = ENV.fetch('SERIES')
+    @release = ENV.fetch('RELEASE')
+    prefix = 'Netrunner'
     components = []
     setup_i386
 
 #to-do: split components:repo to yaml
-    repos += %w[netrunner]
-    components += %w[extras netrunner netrunner-desktop netrunner-core]
-    components += %w[odroid pine64 zeronet rock64] unless DPKG::BUILD_ARCH == 'amd64'
+    components += %w[extras netrunner artwork common netrunner-desktop netrunner-core]
+    components += %w[c1 netrunner-zeronet rock64] unless DPKG::BUILD_ARCH == 'amd64'
+    dist = "#{@release}-#{@series}"
+    data = DCI.releases["#{@release}"]
+    components = data['components']
 
-    @dist = "netrunner-#{@dist}" unless @dist.start_with?('netrunner')
-
-    add_repos(repos, components)
+    add_repos(prefix, dist, components)
 
     key = "#{__dir__}/../dci_apt.key"
     raise 'Failed to import key' unless Apt::Key.add(key)
@@ -55,27 +56,25 @@ module DCI
   def setup_backports!
     # Because we have no /etc/lsb_release
     Apt.install('lsb-release')
-    release = `lsb_release -sc`.strip
+    debian_release = `lsb_release -sc`.strip
 
-    debline = "deb http://deb.debian.org/debian #{release}-backports main"
+    debline = "deb http://deb.debian.org/debian #{debian_release}-backports main"
     raise 'adding backports failed' unless Apt::Repository.add(debline)
 
     Retry.retry_it(times: 5, sleep: 2) { raise unless Apt.update }
 
-    Apt.dist_upgrade("-t=#{release}-backports")
+    Apt.dist_upgrade("-t=#{debian_release}-backports")
   end
 
   def setup_i386
     system('dpkg --add-architecture i386')
   end
 
-  def add_repos(repos, components)
+  def add_repos(prefix, dist, components)
     components = components.join(' ')
-    repos.each do |repo|
-      debline = "deb http://dci.ds9.pub/#{repo} #{@dist} #{components}"
-      Retry.retry_it(times: 5, sleep: 2) do
-        raise 'adding repo failed' unless Apt::Repository.add(debline)
-      end
+    debline = "deb http://dci.ds9.pub/#{prefix} #{dist} #{components}"
+    Retry.retry_it(times: 5, sleep: 2) do
+      raise 'adding repo failed' unless Apt::Repository.add(debline)
     end
   end
 end
