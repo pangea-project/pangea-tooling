e52d4194020621dc63544430100601c8812cb37a
Scarlett Moore <sgmoore@kde.org>
2021-06-02 16:31:03 -0700

    Use the multijob configuration and remove from less extenadable builder.

--- jenkins-jobs/dci/dci_builder.rb
+++ jenkins-jobs/dci/dci_builder.rb
@@ -1,49 +1,50 @@
 # frozen_string_literal: true
-require_relative 'sourcer'
-require_relative 'binarier'
-require_relative 'publisher'
-
-# Magic builder to create an array of build steps
-# Fun story: ci_reporter uses builder, builder is Builder, can't have a class
-# called Builder or tests will fail. I do rather love my live. Also generic
-# names are really cool for shared artifacts such as gems. I always try to be
-# as generic as possible with shared names.
+# require_relative 'sourcer'
+# require_relative 'binarier'
+# require_relative 'publisher'
+# require_relative '../job'
+# 
+# # Magic builder to create an array of build steps
+# # Fun story: ci_reporter uses builder, builder is Builder, can't have a class
+# # called Builder or tests will fail. I do rather love my live. Also generic
+# # names are really cool for shared artifacts such as gems. I always try to be
+# # as generic as possible with shared names.
 class DCIBuilderJobBuilder
-  def self.job(project, release_type:, series:, architecture:, upload_map: nil)
-    basename = basename(series, release_type, project.component, project.name)
-
-    dependees = project.dependees.collect do |d|
-      "#{basename(series, release_type, d.component, d.name)}_src"
-    end.compact
-    sourcer = DCISourcerJob.new(
-      basename,
-      release_type: release_type,
-      series: series,
-      project: project)
-    publisher = DCIPublisherJob.new(
-      basename,
-      release_type: release_type,
-      series: series,
-      dependees: dependees,
-      component: project.component,
-      upload_map: upload_map,
-      architecture: architecture)
-    binarier = DCIBinarierJob.new(
-      basename,
-      release_type: release_type,
-      series: series,
-      architecture: architecture)
-    sourcer.trigger(binarier)
-    binarier.trigger(publisher)
-    
-    jobs =  [sourcer] + [binarier] + [publisher]
-    basename1 = jobs[0].job_name.rpartition('_')[0]
-    unless basename == basename1
-      raise "unexpected basename diff #{basename} v #{basename1}"
-    end
-  end
-
-  def self.basename(dist, type, component, name)
-    "#{dist}_#{type}_#{component}_#{name}"
+#   def self.job(project, release_type:, series:, architecture:, upload_map: nil)
+#     basename = basename(series, release_type, project.component, project.name)
+# 
+#     dependees = project.dependees.collect do |d|
+#       "#{basename(series, release_type, d.component, d.name)}_src"
+#     end.compact
+#     sourcer = DCISourcerJob.new(
+#       basename,
+#       release_type: release_type,
+#       series: series,
+#       project: project)
+#     publisher = DCIPublisherJob.new(
+#       basename,
+#       release_type: release_type,
+#       series: series,
+#       dependees: publisher_dependees,
+#       component: project.component,
+#       upload_map: upload_map,
+#       architecture: architecture)
+#     binarier = DCIBinarierJob.new(
+#       basename,
+#       release_type: release_type,
+#       series: series,
+#       architecture: architecture)
+#     sourcer.trigger(binarier)
+#     binarier.trigger(publisher)
+# 
+#     jobs = [sourcer, binarier, publisher ]
+#     basename1 = jobs[0].job_name.rpartition('_')[0]
+#     unless basename == basename1
+#       raise "unexpected basename diff #{basename} v #{basename1}"
+#     end
+#   end
+# 
+  def self.basename(series, release_type, component, name)
+    "#{series}_#{release_type}_#{component}_#{name}"
   end
 end
---
+++ jenkins-jobs/dci/dci_project_multi_job.rb
@@ -0,0 +1,147 @@
+# frozen_string_literal: true
+#
+# Copyright (C) 2017 Bhushan Shah <bshah@kde.org>
+# Copyright (C) 2015-2017 Harald Sitter <sitter@kde.org>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) version 3, or any
+# later version accepted by the membership of KDE e.V. (or its
+# successor approved by the membership of KDE e.V.), which shall
+# act as a proxy defined in Section 6 of version 3 of the license.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+require_relative 'sourcer'
+require_relative 'binarier'
+require_relative 'publisher'
+require_relative 'dci_builder'
+require_relative '../job'
+require_relative '../multijob_phase'
+
+# Magic builder to create an array of build steps
+class DCIProjectMultiJob < JenkinsJob
+  def self.job(project, series:, release_type:, release:, components:, upload_map:, architecture:, arm_board: nil)
+    return [] unless project.debian?
+
+    architecture = architecture.dup
+    release_type += "-#{arm_board}" if arm_board
+    series = series.dup
+    basename = DCIBuilderJobBuilder.basename(series, release_type, project.component, project.name)
+
+    dependees = project.dependees.collect do |d|
+      basename(series, release_type, d.component, d.name)
+    end
+    dependees = dependees.compact.uniq.sort
+
+    publisher_dependees = project.dependees.collect do |d|
+      "#{basename(series, release_type, d.component, d.name)}_src"
+    end.compact
+
+    sourcer = DCISourcerJob.new(
+      basename,
+      release_type: release_type,
+      series: series,
+      project: project)
+    publisher = DCIPublisherJob.new(
+      basename,
+      release_type: release_type,
+      series: series,
+      dependees: publisher_dependees,
+      component: project.component,
+      upload_map: upload_map,
+      architecture: architecture)
+    binarier = DCIBinarierJob.new(
+      basename,
+      release_type: release_type,
+      series: series,
+      architecture: architecture)
+    jobs = [sourcer, binarier, publisher]
+    basename1 = jobs[0].job_name.rpartition('_')[0]
+    raise "unexpected basename diff #{basename} v #{basename1}" unless basename == basename1
+
+    jobs << new(basename, project: project, release_type: release_type, series: series, jobs: jobs, dependees: dependees)
+    # The actual jobs array cannot be nested, so flatten it out.
+    jobs.flatten
+  end
+
+  # @! attribute [r] jobs
+  #   @return [Array<String>] jobs invoked as part of the multi-phases
+  attr_reader :jobs
+
+  # @! attribute [r] dependees
+  #   @return [Array<String>] name of jobs depending on this job
+  attr_reader :dependees
+
+  # @! attribute [r] project
+  #   @return [Project] project instance of this job
+  attr_reader :project
+
+  # @! attribute [r] upstream_scm
+  #   @return [CI::UpstreamSCM] upstream scm instance of this job_name
+  # FIXME: this is a compat thingy for sourcer (see render method)
+  attr_reader :upstream_scm
+
+  attr_reader :release
+  attr_reader :series
+  private
+
+  def initialize(basename, project:, release:, series:, jobs:, dependees: [])
+    super(basename, 'project.xml.erb')
+
+    # We use nested jobs for phases with multiple jobs, we need to aggregate
+    # them appropriately.
+    job_names = jobs.collect do |job|
+      next job.collect(&:job_name) if job.is_a?(Array)
+
+      job.job_name
+    end
+
+    @nested_jobs = job_names.freeze
+    @jobs = job_names.flatten.freeze
+    @dependees = dependees.freeze
+    @project = project.freeze
+    @release = release.freeze
+    @series = series.freeze
+  end
+
+  def render_phases
+    ret = ''
+    @nested_jobs.each_with_index do |job, i|
+      ret += MultiJobPhase.new(phase_name: "Phase#{i}",
+                               phased_jobs: [job].flatten).render_template
+    end
+    ret
+  end
+
+  def render_packaging_scm
+    PackagingSCMTemplate.new(scm: @project.packaging_scm).render_template
+  end
+
+  def render_upstream_scm
+    @upstream_scm = @project.upstream_scm # FIXME: compat assignment
+    return '' unless @upstream_scm
+
+    case @upstream_scm.type
+    when 'git'
+      render('upstream-scms/git.xml.erb')
+    when 'svn'
+      render('upstream-scms/svn.xml.erb')
+    when 'tarball'
+      ''
+    when 'bzr'
+      ''
+    when 'uscan'
+      ''
+    else
+      raise "Unknown upstream_scm type encountered '#{@upstream_scm.type}'"
+    end
+  end
+end
