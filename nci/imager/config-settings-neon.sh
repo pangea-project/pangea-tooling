8e39ed5e22b22b42b3dd482bcc578067c1fca2ef
Jonathan Riddell <jr@jriddell.org>
2021-06-14 14:09:49 +0100

    build with hwe linux

--- nci/imager/config-settings-neon.sh
+++ nci/imager/config-settings-neon.sh
@@ -1,3 +1,4 @@
 EDITION=$(echo $NEONARCHIVE | sed 's,/, ,')
 export LB_ISO_VOLUME="${IMAGENAME} ${EDITION} \$(date +%Y%m%d-%H:%M)"
 export LB_ISO_APPLICATION="KDE neon Live"
+export LB_LINUX_PACKAGES="linux-generic-hwe-20.04"
