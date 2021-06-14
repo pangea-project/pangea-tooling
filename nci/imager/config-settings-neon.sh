d824f2ac1dcfcbc10555b4fad331a31ea14ecccb
Jonathan Riddell <jr@jriddell.org>
2021-06-14 16:28:51 +0100

    lb config not liking to set LB_LINUX_FLAVOURS so force it

--- nci/imager/config-settings-neon.sh
+++ nci/imager/config-settings-neon.sh
@@ -1,4 +1,5 @@
 EDITION=$(echo $NEONARCHIVE | sed 's,/, ,')
 export LB_ISO_VOLUME="${IMAGENAME} ${EDITION} \$(date +%Y%m%d-%H:%M)"
 export LB_ISO_APPLICATION="KDE neon Live"
-export LB_LINUX_PACKAGES="linux-generic-hwe-20.04"
+export LB_LINUX_FLAVOURS="generic-hwe-20.04"
+export LB_LINUX_PACKAGES="linux"
