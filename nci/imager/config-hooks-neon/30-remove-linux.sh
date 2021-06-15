bc1ce80e544f2d9408f51b02f04bec0fc2c98cd2
Jonathan Riddell <jr@jriddell.org>
2021-06-15 12:12:40 +0100

    force removal of old linux which for some reason is still installed

---
+++ nci/imager/config-hooks-neon/30-remove-linux.sh
@@ -0,0 +1,3 @@
+# We now use 20.04 HWE linux but this one still gets isntalled for some reason, jriddell 2021-06-15
+apt remove -y linux-headers-5.4.0-74-generic linux-headers-5.4.0-74 linux-generic linux-headers-generic
+
