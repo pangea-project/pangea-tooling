28d6a3b465c76d6271285e573f7bfeda5d23ee6c
Harald Sitter <sitter@kde.org>
2021-06-28 15:02:14 +0200

    refine the kcrash validator cmake a bit
    - now using the TESTS property to filter tests more reliably (this is
    available since cmake 3.12. since we at least have 3.16 we shouldn't
    have any problems with it - even not present it also doesn't matter, the
    continue condition simply becomes noop)
    
    - kcrash and qtest link libs are now looked for with version variance.
    this allows us to search for all types of Qt::Test Qt5::Test and
    Qt6::Test being perfectly compatible with everything
    
    - some comment refinement

--- lib/ci/kcrash_link_validator.cmake
+++ lib/ci/kcrash_link_validator.cmake
@@ -19,6 +19,10 @@ function(kcrash_validator_check_all_targets)
 
     kcrash_validator_get_subs(subs .)
     foreach(sub ${subs})
+        # List of all tests in this directory. Only available in cmake 3.12 (we always have that since 20.04).
+        # These will generally (maybe even always?) have the same name as the target.
+        get_property(_tests DIRECTORY ${sub} PROPERTY TESTS)
+        # All targets in this directory.
         get_property(targets DIRECTORY ${sub} PROPERTY BUILDSYSTEM_TARGETS)
         foreach(target ${targets})
             # Is a linked type (executable/lib)
@@ -28,31 +32,47 @@ function(kcrash_validator_check_all_targets)
                 continue()
             endif()
 
+            # Filter tests
+            # NB: cannot use IN_LIST condition because it is policy dependant
+            #   and we do not want to change the policy configuration
+            list(FIND _tests ${target} target_testlib_index)
+            if(${target_testlib_index} GREATER -1)
+                continue()
+            endif()
+
             # Is part of all target
             get_target_property(target_exclude_all ${target} EXCLUDE_FROM_ALL)
             if(${target_exclude_all})
                 continue()
             endif()
 
-            # Wants KCrash
-            # NB: cannot use IN_LIST condition because it is policy dependant
-            #   and we do not want to change the policy configuration
-            get_target_property(target_libs ${target} LINK_LIBRARIES)
-            list(FIND target_libs "KF5::Crash" target_lib_index)
-            if(${target_lib_index} LESS 0)
+            set(_is_test OFF)
+            set(_links_kcrash OFF)
+            set(_versions ";5;6") # this must be a var or IN LISTS won't work. Unversioned is a valid option!
+            foreach(_version IN LISTS _versions)
+                # Wants KCrash
+                # NB: cannot use IN_LIST condition because it is policy dependant
+                #   and we do not want to change the policy configuration
+                get_target_property(target_libs ${target} LINK_LIBRARIES)
+                list(FIND target_libs "KF${_version}::Crash" target_lib_index)
+                if(${target_lib_index} GREATER -1)
+                    set(_links_kcrash ON)
+                endif()
+                # Filter tests... again.
+                # This further approximates test detection. Unfortunately tests aren't always add_test() and don't
+                # appear in the TESTS property. So we also check if the target at hand links qtest and if that is the
+                # case skip it. Production targets oughtn't ever use qtest and that assumption is likely true 99% of
+                # the time (and for the case when it is not true I'd consider it a bug that qtest is linked at all).
+                list(FIND target_libs "Qt${_version}::Test" target_testlib_index)
+                if(${target_testlib_index} GREATER -1)
+                    set(_is_test ON)
+                endif()
+            endforeach()
+            if(_is_test OR NOT _links_kcrash)
                 continue()
             endif()
 
-            # Not probably a test
-            # (There is no actual way to tell a test from a regular target in
-            # CMake before 3.10 [introduces TESTS property on DIRECTORY], so
-            # we can only approximate this)
-            list(FIND target_libs "Qt5::Test" target_testlib_index)
-            if(${target_testlib_index} GREATER -1)
-                continue()
-            endif()
-
-            message("target: ${target}")
+            message("KCrash validating: ${target}")
             add_custom_target(objdump-kcrash-${target} ALL
                 COMMAND echo "  $<TARGET_FILE:${target}>"
                 COMMAND objdump -p $<TARGET_FILE:${target}> | grep NEEDED | grep libKF5Crash.so
