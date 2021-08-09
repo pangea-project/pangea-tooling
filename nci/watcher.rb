d605a005ba763af4a89c226936131cfa97616de5
Jonathan Riddell <jr@jriddell.org>
2021-08-09 15:29:44 +0100

    new directory for watcher

--- nci/watcher.rb
+++ nci/watcher.rb
@@ -14,9 +14,11 @@ require_relative '../lib/debian/source'
 # TODO: we should still detect when ubuntu versions diverge I guess?
 #   though that may be more applicable to backports version change detection as
 #   a whole
-if Debian::Source.new(Dir.pwd).format.type == :native
-  puts 'This is a native source. Nothing to do!'
-  exit 0
+Dir.chdir('deb-packaging') do
+  if Debian::Source.new(Dir.pwd).format.type == :native
+    puts 'This is a native source. Nothing to do!'
+    exit 0
+  end
 end
 
 # Special exclusion list. For practical reasons we have kind of neon-specific
