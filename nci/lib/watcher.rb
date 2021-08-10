5f43e38ead2e9283c954ae43f91c65a78e125a6f
Jonathan Riddell <jr@jriddell.org>
2021-08-10 12:58:34 +0100

    Revert "new directory for watcher"
    This reverts commit 17182c941c2eeaf084b57c9655efd2948f4d6b05.

--- nci/lib/watcher.rb
+++ nci/lib/watcher.rb
@@ -264,7 +264,7 @@ module NCI
       if ENV.include?('JOB_NAME')
         snap_directory = "snapcraft-kde-applications/" + ENV['JOB_NAME'][ENV['JOB_NAME'].rindex('_')+1..]
         if File.directory?(snap_directory)
-          Dir.chdir(snap_directory) do
+          Dir.chdir("snapcraft-kde-applications/" + package) do
             SnapcraftUpdater.new(newest_dehs_package).run
           end
         else
