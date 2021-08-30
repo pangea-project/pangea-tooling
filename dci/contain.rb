1d8b209beab1505f71ed27d37f8063509620f761
llelectronics <leszek.lesner@web.de>
2021-08-30 15:55:34 +0200

    Failed to fetch release - not set anymore so don't try fetching it

--- dci/contain.rb
+++ dci/contain.rb
@@ -4,7 +4,7 @@
 require_relative '../lib/ci/containment'
 
 Docker.options[:read_timeout] = 36 * 60 * 60 # 36 hours. Because, QtWebEngine.
-RELEASE = ENV.fetch('RELEASE')
+# RELEASE = ENV.fetch('RELEASE')
 DIST = ENV.fetch('RELEASE_TYPE')
 RELEASE_TYPE = ENV.fetch('RELEASE_TYPE')
 SERIES = ENV.fetch('SERIES')
@@ -12,7 +12,7 @@ BUILD_TAG = ENV.fetch('BUILD_TAG')
 
 # Whitelist a bunch of Jenkins variables for consumption inside the container.
 whitelist = %w[BUILD_CAUSE ROOT_BUILD_CAUSE RUN_DISPLAY_URL JOB_NAME
-               NODE_NAME NODE_LABELS RELEASE BUILD_TAG DIST RELEASE_TYPE
+               NODE_NAME NODE_LABELS BUILD_TAG DIST RELEASE_TYPE
                PANGEA_PROVISION_AUTOINST SERIES DH_VERBOSE WORKSPACE]
 whitelist += (ENV['DOCKER_ENV_WHITELIST'] || '').split(':')
 ENV['DOCKER_ENV_WHITELIST'] = whitelist.join(':')
