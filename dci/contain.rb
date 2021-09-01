7fe3dc791a68090ecdd457e4965c4e5d5303d4e5
llelectronics <leszek.lesner@web.de>
2021-09-01 15:21:32 +0200

    dci: contain.rb partially revert env variable fetching to old variables to be able to build packages

--- dci/contain.rb
+++ dci/contain.rb
@@ -4,19 +4,20 @@
 require_relative '../lib/ci/containment'
 
 Docker.options[:read_timeout] = 36 * 60 * 60 # 36 hours. Because, QtWebEngine.
-RELEASE = ENV.fetch('RELEASE')
-DIST = ENV.fetch('RELEASE_TYPE')
-RELEASE_TYPE = ENV.fetch('RELEASE_TYPE')
-SERIES = ENV.fetch('SERIES')
+DIST_RELEASE = ENV.fetch('DIST_RELEASE')
+# RELEASE = ENV.fetch('RELEASE')
+# DIST = ENV.fetch('RELEASE_TYPE')
+# RELEASE_TYPE = ENV.fetch('RELEASE_TYPE')
+# SERIES = ENV.fetch('SERIES')
 BUILD_TAG = ENV.fetch('BUILD_TAG')
 
 # Whitelist a bunch of Jenkins variables for consumption inside the container.
 whitelist = %w[BUILD_CAUSE ROOT_BUILD_CAUSE RUN_DISPLAY_URL JOB_NAME
-               NODE_NAME NODE_LABELS RELEASE BUILD_TAG DIST RELEASE_TYPE
-               PANGEA_PROVISION_AUTOINST SERIES DH_VERBOSE WORKSPACE]
+               NODE_NAME NODE_LABELS DIST_RELEASE BUILD_TAG 
+               PANGEA_PROVISION_AUTOINST DH_VERBOSE WORKSPACE]
 whitelist += (ENV['DOCKER_ENV_WHITELIST'] || '').split(':')
 ENV['DOCKER_ENV_WHITELIST'] = whitelist.join(':')
 
-c = CI::Containment.new(BUILD_TAG, image: CI::PangeaImage.new(:debian, SERIES))
+c = CI::Containment.new(BUILD_TAG, image: CI::PangeaImage.new(:debian, DIST_RELEASE))
 status_code = c.run(Cmd: ARGV)
 exit status_code
