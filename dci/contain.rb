d46a14be539a30ee15fdba7dfa60db3b8bc1db86
Scarlett Moore <sgmoore@kde.org>
2021-09-29 11:50:47 -0500

    Update DCI contain env variables.

--- dci/contain.rb
+++ dci/contain.rb
@@ -4,20 +4,18 @@
 require_relative '../lib/ci/containment'
 
 Docker.options[:read_timeout] = 36 * 60 * 60 # 36 hours. Because, QtWebEngine.
-DIST_RELEASE = ENV.fetch('DIST_RELEASE')
-# RELEASE = ENV.fetch('RELEASE')
-# DIST = ENV.fetch('RELEASE_TYPE')
-# RELEASE_TYPE = ENV.fetch('RELEASE_TYPE')
-# SERIES = ENV.fetch('SERIES')
+DIST = ENV.fetch('SERIES')
+RELEASE = ENV.fetch('RELEASE')
+SERIES = ENV.fetch('SERIES')
 BUILD_TAG = ENV.fetch('BUILD_TAG')
 
 # Whitelist a bunch of Jenkins variables for consumption inside the container.
 whitelist = %w[BUILD_CAUSE ROOT_BUILD_CAUSE RUN_DISPLAY_URL JOB_NAME
-               NODE_NAME NODE_LABELS DIST_RELEASE BUILD_TAG 
+               NODE_NAME NODE_LABELS DIST BUILD_TAG RELEASE SERIES
                PANGEA_PROVISION_AUTOINST DH_VERBOSE WORKSPACE]
 whitelist += (ENV['DOCKER_ENV_WHITELIST'] || '').split(':')
 ENV['DOCKER_ENV_WHITELIST'] = whitelist.join(':')
 
-c = CI::Containment.new(BUILD_TAG, image: CI::PangeaImage.new(:debian, DIST_RELEASE))
+c = CI::Containment.new(BUILD_TAG, image: CI::PangeaImage.new(:debian, DIST))
 status_code = c.run(Cmd: ARGV)
 exit status_code
