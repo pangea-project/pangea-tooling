90c42ac2ebce6f8bd5076588369f136ec24eb88f
Scarlett Moore <sgmoore@kde.org>
2021-10-05 11:54:24 -0600

    Syntax, style++ dci job_updater.

--- jenkins_jobs_update_dci.rb
+++ jenkins_jobs_update_dci.rb
@@ -46,6 +46,7 @@ class ProjectUpdater < Jenkins::ProjectUpdater
     @projects_dir = File.expand_path('projects/dci', @data_dir)
     upload_map_file = File.expand_path('dci.upload.yaml', @data_dir)
     @flavor_dir = File.expand_path('jenkins-jobs/dci', __dir__)
+    @arm = DCI.arm_board_by_release(@release)
     return unless File.exist?(upload_map_file)
 
     @upload_map = YAML.load_file(upload_map_file)
@@ -61,99 +62,89 @@ class ProjectUpdater < Jenkins::ProjectUpdater
     all_builds = []
     jobs = []
     @series = ''
-    @release_type = ''
     @release = ''
-    @arm = ''
     @data_file_name = ''
-    DCI.series.each_key do |series|
-      @series = series
-      @data_dir = File.expand_path("dci/#{series}", @data_dir)
-      DCI.release_types.each do |release_type|
-        puts "Populating jobs for #{release_type}"
-        @release_type = release_type
-          DCI.releases_for_type(@release_type).each do |release|
-            puts "Processing release: #{release}"
-            @release = release
-            data = DCI.get_release_data(@release_type, @release)
-            if  DCI.arm?(@release)
-              @arm = DCI.arm_board_by_release(@release)
-              @data_file_name = "#{@release_type}-#{@arm}.yaml"
-              puts "Working on #{@release}-#{@arm}-#{series}"
-            else
-              @data_file_name = "#{release_type}.yaml"
-              puts "Working on #{release}-#{series}"
-            end
-            next unless data
-            projects = ProjectsFactory.from_file(data, branch: "Netrunner/#{@series}")
-            raise unless projects
-            projects.each do |project|
-              j = DCIProjectMultiJob.new(
-                project,
-                release_type: @release_type,
-                release: @release,
-                components: DCI.components_by_release(data),
-                series: @series,
-                architecture: DCI.arch_by_release(data),
-                arm_board: @arm,
-                upload_map: @upload_map
-              )
-              jobs << j
-              all_builds += j
-            end
-            jobs.each { |job| enqueue(job) }
-            puts all_builds
-            all_builds.flatten!
-            # Remove everything but source as they are the anchor points for
-            # other jobs that might want to reference them.
-            all_builds.select! { |j| j.job_name.end_with?('_src') }
-            # This could actually returned into a collect if placed below
-            meta_build = MetaBuildJob.new(
-            type: @release_type,
-            distribution: @release,
-            downstream_jobs: all_builds)
-            all_meta_builds << enqueue(meta_build)
+    @series = series
+    @data_dir = File.expand_path("dci/#{series}", @data_dir)
+    DCI.release_types.each do |release_type|
+     DCI.releases_for_type(release_type).each do |release|
+      @release_data = DCI.get_release_data(release_type, release)
+      @release = release
+      @data_file_name = DCI.arm?(@release) ? "#{release_type}-#{@arm}.yaml" : "#{release_type}.yaml"
+      DCI.series.each_key do |series|
+       puts "Working on series: #{series} release: #{@data_file_name}.gsub('.yaml', '')"
+       raise unless @data_file_name
 
-            #image Jobs
-            @data_file_name = 'dci.image.yaml'
-            @data_dir = File.expand_path('dci', @data_dir)
+       projects = ProjectsFactory.from_file(@data_file_name, branch: "Netrunner/#{@series}")
+       raise unless projects
 
-            image_job_config = File.expand_path(@data_file_name, @data_dir)
-            load_config = YAML.load_stream(File.read(image_job_config))
-            next unless image_job_config
+       projects.each do |project|
+        j = DCIProjectMultiJob.new(
+          project,
+          release: @release,
+          series: @series,
+          architecture: DCI.arch_by_release(@release_data),
+          upload_map: @upload_map
+        )
+        jobs << j
+        all_builds += j
+        jobs.each { |job| enqueue(job) }
+        all_builds.flatten!
+      end
+      # Remove everything but source as they are the anchor points for
+      # other jobs that might want to reference them.
+      all_builds.select! { |j| j.job_name.end_with?('_src') }
+      # This could actually returned into a collect if placed below
+      meta_build = MetaBuildJob.new(
+        type: @series,
+        distribution: @release,
+        downstream_jobs: all_builds
+      )
+      all_meta_builds << enqueue(meta_build)
+      #image Jobs
+      @data_file_name = 'dci.image.yaml'
+      @data_dir = File.expand_path('dci', @data_dir)
 
-            image_jobs = load_config
-            image_data = image_jobs[@release]
-            enqueue(
-              DCIImageJob.new(
-                release: @release,
-                architecture: DCI.arch_by_release(data),
-                repo: image_data[:repo],
-                branch: image_data[:releases][@series].values
-              )
+        image_job_config = File.expand_path(@data_file_name, @data_dir)
+        load_config = YAML.load_stream(File.read(image_job_config))
+        next unless image_job_config
+
+        all_image_jobs = load_config
+        all_image_jobs.each do |image_job|
+        image_data = image_job[@release]
+        enqueue(
+            DCIImageJob.new(
+              release: @release,
+              architecture: DCI.arch_by_release(@release),
+              repo: image_data[:repo],
+              branch: image_data[:releases][@series].values
             )
-            image_data['snapshots'].each do |snapshot|
-              enqueue(
-                DCISnapShotJob.new(
-                  snapshot: snapshot,
-                  series: @series,
-                  release_type: @release_type,
-                  arm_board: @arm,
-                  architecture: DCI.arch_by_release(data)
-                )
-              )
-            # MGMT Jobs follow
-            docker = enqueue(MGMTDockerJob.new(dependees: all_meta_builds))
-            docker_clean = enqueue(MGMTDockerCleanupJob.new(downstreams: [docker]))
-            tooling_deploy = enqueue(MGMTToolingDeployJob.new(downstreams: [docker_clean]))
-            tooling_progenitor = enqueue(MGMTToolingProgenitorJob.new(downstreams: [tooling_deploy]))
-            enqueue(MGMTDCIToolingJob.new(downstreams: [tooling_progenitor], dependees: [], type: release))
-            enqueue(MGMTPauseIntegrationJob.new(downstreams: all_meta_builds))
-            enqueue(MGMTRepoCleanupJob.new)
+          )
+          image_data['snapshots'].each do |snapshot|
+          enqueue(
+            DCISnapShotJob.new(
+              snapshot: snapshot,
+              series: @series,
+              release: @release,
+              architecture: DCI.arch_by_release(@release)
+            )
+          )
+          docker = enqueue(MGMTDockerJob.new(dependees: all_meta_builds))
+          docker_clean = enqueue(MGMTDockerCleanupJob.new(downstreams: [docker]))
+          tooling_deploy = enqueue(MGMTToolingDeployJob.new(downstreams: [docker_clean]))
+          tooling_progenitor = enqueue(MGMTToolingProgenitorJob.new(downstreams: [tooling_deploy]))
+          enqueue(MGMTDCIToolingJob.new(downstreams: [tooling_progenitor], dependees: [], type: release))
+          enqueue(MGMTPauseIntegrationJob.new(downstreams: all_meta_builds))
+          enqueue(MGMTRepoCleanupJob.new)
           end
         end
       end
+     end
+    end
   end
 end
+
+
 
 
 
