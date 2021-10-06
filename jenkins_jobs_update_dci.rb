ec2981fa9539596ee6ef0e216e8a16f903203dbe
Scarlett Moore <sgmoore@kde.org>
2021-10-06 17:25:50 -0600

    Move data_dir to after we have series value.

--- jenkins_jobs_update_dci.rb
+++ jenkins_jobs_update_dci.rb
@@ -64,7 +64,7 @@ class ProjectUpdater < Jenkins::ProjectUpdater
     @release_type = ''
     @data_file_name = ''
     @series = ''
-    @data_dir = File.expand_path("dci/#{series}", @data_dir)
+
     DCI.release_types.each do |release_type|
       @release_type = release_type
      DCI.releases_for_type(@release_type).each do |release|
@@ -73,6 +73,7 @@ class ProjectUpdater < Jenkins::ProjectUpdater
       @arm = DCI.arm_board_by_release(@release)
       @data_file_name = DCI.arm?(@release) ? "#{@release_type}-#{@arm}.yaml" : "#{@release_type}.yaml"
       DCI.series.each_key do |series|
+      @data_dir = File.expand_path("dci/#{series}", @data_dir)
        puts "Working on series: #{series} release: #{@data_file_name}.gsub('.yaml', '')"
        @series = series
        raise unless @data_file_name
