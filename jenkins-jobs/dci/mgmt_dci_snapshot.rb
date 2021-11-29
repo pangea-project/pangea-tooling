3f194d566a35194f79a186bd719787b27bc1dea1
Scarlett Moore <sgmoore@kde.org>
2021-11-29 12:28:04 -0700

    Fix snapshot job and template to use release: rather than generic release_type.

--- jenkins-jobs/dci/mgmt_dci_snapshot.rb
+++ jenkins-jobs/dci/mgmt_dci_snapshot.rb
@@ -4,21 +4,21 @@ require_relative '../job'
 # Generate a job to run snapshots of aptly repos.
 class DCISnapShotJob < JenkinsJob
   attr_reader :architecture
-  attr_reader :release_type
+  attr_reader :release
   attr_reader :snapshot
   attr_reader :series
   attr_reader :arm_board
 
-  def initialize(snapshot:, series:, release_type:, architecture:, arm_board: nil)
+  def initialize(snapshot:, series:, release:, architecture:, arm_board: nil)
     @release_type = release_type
     @architecture = architecture
     @snapshot = snapshot
     @series = series
     @arm_board = arm_board
     if @arm_board
-      super("snapshot_#{series}_#{release_type}_#{snapshot}_#{architecture}_#{arm_board}", 'mgmt_dci_snapshot.xml.erb')
+      super("snapshot_#{series}_#{release}_#{snapshot}_#{architecture}_#{arm_board}", 'mgmt_dci_snapshot.xml.erb')
     else
-      super("snapshot_#{series}_#{release_type}_#{snapshot}_#{architecture}", 'mgmt_dci_snapshot.xml.erb')
+      super("snapshot_#{series}_#{release}_#{snapshot}_#{architecture}", 'mgmt_dci_snapshot.xml.erb')
     end
   end
 end
