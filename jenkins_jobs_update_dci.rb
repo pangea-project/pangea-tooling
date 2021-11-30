1389833d1914d08c2683b5dc6b94daf8b164776a
Scarlett Moore <sgmoore@kde.org>
2021-11-30 06:37:01 -0700

    Change release var to dci_release to 1) allow global usage without confusion with the pangea global var release usage. 2) clarity
    Style++

--- jenkins_jobs_update_dci.rb
+++ jenkins_jobs_update_dci.rb
@@ -68,35 +68,35 @@ class ProjectUpdater < Jenkins::ProjectUpdater
     DCI.release_types.each do |release_type|
       @release_type = release_type
       DCI.releases_for_type(@release_type).each do |dci_release|
+        @dci_release = dci_release
         @release_data = DCI.get_release_data(@release_type, @dci_release)
-        @dci_release = release
         @arm = DCI.arm_board_by_release(@dci_release)
         @data_file_name = DCI.arm?(@dci_release) ? "#{@release_type}-#{@arm}.yaml" : "#{@release_type}.yaml"
         DCI.series.each_key do |series|
-         @series = series
-         DCI.all_architectures.each do |arch|
-          release_arch = DCI.arch_by_release(@dci_release)
-          data_dir = File.expand_path(@series, @projects_dir)
-          puts "Working on series: #{series} @dci_release: #{@dci_release}"
-          raise unless @data_file_name
+          @series = series
+          DCI.all_architectures.each do |arch|
+            release_arch = DCI.arch_by_release(@dci_release)
+            data_dir = File.expand_path(@series, @projects_dir)
+            puts "Working on series: #{series} @dci_release: #{@dci_release}"
+            raise unless @data_file_name
 
-          file = File.expand_path(@data_file_name,  data_dir)
-          projects = ProjectsFactory.from_file(file, branch: "Netrunner/#{@series}")
-          raise unless projects
+            file = File.expand_path(@data_file_name,  data_dir)
+            projects = ProjectsFactory.from_file(file, branch: "Netrunner/#{@series}")
+            raise unless projects
 
-          projects.each do |project|
-            next unless release_arch == arch
+            projects.each do |project|
+              next unless release_arch == arch
 
-            jobs = DCIProjectMultiJob.job(
-              project,
-              release: @dci_release,
-              series: @series,
-              architecture: arch,
-              upload_map: @upload_map
-            )
-            jobs.each { |j| enqueue(j) }
-            all_builds += jobs
-          end
+              jobs = DCIProjectMultiJob.job(
+                project,
+                release: @dci_release,
+                series: @series,
+                architecture: arch,
+                upload_map: @upload_map
+              )
+              jobs.each { |j| enqueue(j) }
+              all_builds += jobs
+            end
            #  # Remove everything but source as they are the anchor points for
            #  # other jobs that might want to reference them.
            #  all_builds.select! { |j| j.job_name.end_with?('_src') }
@@ -108,26 +108,26 @@ class ProjectUpdater < Jenkins::ProjectUpdater
            # )
            # all_meta_builds << enqueue(meta_build)
            # image Jobs
-          image_data = DCI.image_data_by_release_type(@release_type)
-          @stamp = DateTime.now.strftime("%Y%m%d.%H%M")
-          enqueue(
-           DCIImageJob.new(
-             release: @dci_release,
-             series: @series,
-             architecture: DCI.arch_by_release(image_data),
-             repo: image_data[:repo],
-             branch: image_data.fetch(@dci_release)[:releases].fetch(@series)
+            image_data = DCI.image_data_by_release_type(@release_type)
+            @stamp = DateTime.now.strftime("%Y%m%d.%H%M")
+            enqueue(
+             DCIImageJob.new(
+               release: @dci_release,
+               series: @series,
+               architecture: DCI.arch_by_release(image_data),
+               repo: image_data[:repo],
+               branch: image_data.fetch(@dci_release)[:releases].fetch(@series)
+              )
             )
-          )
-          enqueue(
-            DCISnapShotJob.new(
-              snapshot: "#{@series}-#{@stamp}",
-              series: @series,
-              release: @dci_release,
-              architecture: DCI.arch_by_release(@dci_release)
+            enqueue(
+              DCISnapShotJob.new(
+                snapshot: "#{@series}-#{@stamp}",
+                series: @series,
+                release: @dci_release,
+                architecture: DCI.arch_by_release(@dci_release)
+              )
             )
-          )
-         end
+          end
         end
       end
     end
