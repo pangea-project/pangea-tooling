962b18def7ba575cb853c33ae7171c915b0d0714
Jonathan Riddell <jr@jriddell.org>
2021-12-03 10:25:57 +0000

    update qt_sixty to edit the .install files too

--- nci/qt_sixy.rb
+++ nci/qt_sixy.rb
@@ -3,6 +3,10 @@
 # SPDX-FileCopyrightText: 2021 Harald Sitter <sitter@kde.org>
 # SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
 
+# A quick script to go over qt 6 repos from Debian and simplify the .debs produced to make them only runtime and dev packages
+# The intention is to simplify maintinance so when new Qts come out we don't have to worry about where to put the files
+# This needs manual going over the output for sanity
+
 require_relative '../lib/debian/control'
 require_relative '../lib/projects/factory/neon'
 
@@ -28,27 +32,20 @@ def fold_pkg(pkg, into:)
 end
 
 cmd = TTY::Command.new
-%w[qt6/qt6-base].each do |repo|
+#Dir.glob('*').each do |repo|
+['qt6-base'].each do |repo|
 # ProjectsFactory::Neon.ls.each do |repo| TODO
   next unless repo.include?('qt6')
 
-  # TODO remove
-  next unless repo.include?('base')
-
   p repo
-  # FileUtils.rm_rf('qt_sixy')
-  # Dir.mkdir('qt_sixy')
-  Dir.chdir('qt_sixy') do
+  Dir.chdir(repo + '/neongit/') do
     # cmd.run("git clone --depth 1 --branch master https://invent.kde.org/neon/#{repo}")
     Dir.chdir(File.basename(repo)) do
-      FileUtils.cp('debian/control.bak', 'debian/control') if File.exist?('debian/control.bak')
+      #FileUtils.cp('debian/control.bak', 'debian/control') if File.exist?('debian/control.bak')
       FileUtils.cp('debian/control', 'debian/control.bak')
       control = Debian::Control.new
       control.parse!
       p control.binaries.collect { |x| x['Package'] } # pkgs
-      # TODO deep clone instead of parsing twice
-      old_control = Debian::Control.new
-      old_control.parse!
 
       dev_binaries = control.binaries.select { |x| x['Package'].include?('-dev') }
       bin_binaries = control.binaries.select { |x| !dev_binaries.include?(x) }
@@ -66,6 +63,16 @@ cmd = TTY::Command.new
       dev.replace({'Package' => File.basename(repo) + '-dev', 'Architecture' => 'any', 'Section' => 'kde', 'Description' => '((TBD))'})
       dev['Provides'] = Debian::Deb822.parse_relationships(dev_binaries.collect { |x| x['Package'] }.join(', '))
 
+      bin_binaries_names.each do |package_name|
+        next if bin['Package'] == package_name
+        old_install_file_data = File.read("debian/" + package_name + ".install")
+        new_install_filename = "debian/" + bin['Package'] + ".install"
+        File.write(new_install_filename, old_install_file_data, mode: "a")
+        FileUtils.rm_f("debian/" + package_name + ".install")
+        FileUtils.rm_f("debian/" + package_name + ".symbols")
+        FileUtils.rm_f("debian/" + package_name + ".lintian-overrides")
+      end
+
       bin_binaries.each do |bin_bin|
         p bin_bin
         fold_pkg(bin_bin, into: bin)
@@ -73,6 +80,16 @@ cmd = TTY::Command.new
 
       # bin['Provides'] ||= []
       # bin['Provides'] += bin_binaries.collect { |x| x['Package'] }.join(', ')
+
+      dev_binaries_names.each do |package_name|
+        next if dev['Package'] == package_name
+        old_install_file_data = File.read("debian/" + package_name + ".install")
+        new_install_filename = "debian/" + bin['Package'] + ".install"
+        File.write(new_install_filename, old_install_file_data, mode: "a")
+        FileUtils.rm_f("debian/" + package_name + ".install")
+        FileUtils.rm_f("debian/" + package_name + ".symbols")
+        FileUtils.rm_f("debian/" + package_name + ".lintian-overrides")
+      end
 
       dev_binaries.each do |dev_bin|
         fold_pkg(dev_bin, into: dev)
