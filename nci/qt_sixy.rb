61b2f579941a0e205687878257e85e48cc40a464
Jonathan Riddell <jr@jriddell.org>
2021-12-06 15:26:27 +0000

    refactor qt_sixy into an object and add start of a test for it

--- nci/qt_sixy.rb
+++ nci/qt_sixy.rb
@@ -3,7 +3,7 @@
 # SPDX-FileCopyrightText: 2021 Harald Sitter <sitter@kde.org>
 # SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
 
-# A quick script to go over qt 6 repos from Debian and simplify the .debs produced to make them only runtime and dev packages
+# A quick script to go over a qt 6 repo from Debian and simplify the .debs produced to make them only runtime and dev packages
 # The intention is to simplify maintinance so when new Qts come out we don't have to worry about where to put the files
 # This needs manual going over the output for sanity
 
@@ -13,108 +13,118 @@ require_relative '../lib/projects/factory/neon'
 require 'deep_merge'
 require 'tty/command'
 
-def fold_pkg(pkg, into:)
-  pkg.each do |k,v|
-    next if k == 'Package'
-    next if k == 'Architecture'
-    next if k == 'Multi-Arch'
-    next if k == 'Section'
-    next if k == 'Description'
+class QtSixy
 
-    into[k] = v unless into.include?(k)
-    case into[k].class
-    when Hash, Array
-      into[k].deep_merge!(v)
-    else
-      into[k] += v
+  def initialize(path)
+    Dir.chdir(path)
+    puts "Running Sixy in #{Dir.pwd}"
+    unless File.basename(Dir.pwd).include?("qt6-")
+      puts "Must be run in a 'qt6-foo' repo"
+      exit
     end
+    unless File.exists?("debian")
+      puts "Must be run in a 'qt6-foo' repo with 'debian/' dir"
+      exit
+    end
+  end
+
+  def fold_pkg(pkg, into:)
+    pkg.each do |k,v|
+      next if k == 'Package'
+      next if k == 'Architecture'
+      next if k == 'Multi-Arch'
+      next if k == 'Section'
+      next if k == 'Description'
+
+      into[k] = v unless into.include?(k)
+      case into[k].class
+      when Hash, Array
+        into[k].deep_merge!(v)
+      else
+        into[k] += v
+      end
+    end
+  end
+
+  def run
+    cmd = TTY::Command.new
+    #FileUtils.cp('debian/control.bak', 'debian/control') if File.exist?('debian/control.bak')
+    FileUtils.cp('debian/control', 'debian/control.bak')
+    control = Debian::Control.new
+    control.parse!
+    p control.binaries.collect { |x| x['Package'] } # pkgs
+
+    dev_binaries = control.binaries.select { |x| x['Package'].include?('-dev') }
+    bin_binaries = control.binaries.select { |x| !dev_binaries.include?(x) }
+    control.binaries.replace(control.binaries[0..1])
+    dev_binaries_names = dev_binaries.collect { |x| x['Package'] }
+    bin_binaries_names = bin_binaries.collect { |x| x['Package'] }
+
+    control.binaries.replace( [{}, {}] )
+
+    bin = control.binaries[0]
+    bin.replace({'Package' => File.basename(Dir.pwd), 'Architecture' => 'any', 'Section' => 'kde', 'Description' => '((TBD))'})
+
+    bin['Provides'] = Debian::Deb822.parse_relationships(bin_binaries.collect { |x| x['Package'] }.join(', '))
+    dev = control.binaries[1]
+    dev.replace({'Package' => File.basename(Dir.pwd) + '-dev', 'Architecture' => 'any', 'Section' => 'kde', 'Description' => '((TBD))'})
+    dev['Provides'] = Debian::Deb822.parse_relationships(dev_binaries.collect { |x| x['Package'] }.join(', '))
+
+    bin_binaries_names.each do |package_name|
+      next if bin['Package'] == package_name
+      old_install_file_data = File.read("debian/" + package_name + ".install")
+      new_install_filename = "debian/" + bin['Package'] + ".install"
+      File.write(new_install_filename, old_install_file_data, mode: "a")
+      FileUtils.rm_f("debian/" + package_name + ".install")
+      FileUtils.rm_f("debian/" + package_name + ".symbols")
+      FileUtils.rm_f("debian/" + package_name + ".lintian-overrides")
+    end
+
+    bin_binaries.each do |bin_bin|
+      p bin_bin
+      fold_pkg(bin_bin, into: bin)
+    end
+
+    # bin['Provides'] ||= []
+    # bin['Provides'] += bin_binaries.collect { |x| x['Package'] }.join(', ')
+
+    dev_binaries_names.each do |package_name|
+      next if dev['Package'] == package_name
+      old_install_file_data = File.read("debian/" + package_name + ".install")
+      new_install_filename = "debian/" + bin['Package'] + ".install"
+      File.write(new_install_filename, old_install_file_data, mode: "a")
+      FileUtils.rm_f("debian/" + package_name + ".install")
+      FileUtils.rm_f("debian/" + package_name + ".symbols")
+      FileUtils.rm_f("debian/" + package_name + ".lintian-overrides")
+    end
+
+    dev_binaries.each do |dev_bin|
+      fold_pkg(dev_bin, into: dev)
+    end
+
+
+    dev.each do |k, v|
+      next unless v.is_a?(Array)
+
+      v.each do |relationships|
+        next unless relationships.is_a?(Array)
+        relationships.each do |alternative|
+          next unless alternative.is_a?(Debian::Relationship)
+
+          next unless bin_binaries_names.include?(alternative.name)
+          p alternative
+          alternative.name.replace(bin['Package'])
+        end
+      end
+    end
+
+    File.write('debian/control', control.dump)
+    cmd.run('wrap-and-sort')
+    system('cat debian/control')
   end
 end
 
-cmd = TTY::Command.new
-#Dir.glob('*').each do |repo|
-['qt6-base'].each do |repo|
-# ProjectsFactory::Neon.ls.each do |repo| TODO
-  next unless repo.include?('qt6')
-
-  p repo
-  Dir.chdir(repo + '/neongit/') do
-    # cmd.run("git clone --depth 1 --branch master https://invent.kde.org/neon/#{repo}")
-    Dir.chdir(File.basename(repo)) do
-      #FileUtils.cp('debian/control.bak', 'debian/control') if File.exist?('debian/control.bak')
-      FileUtils.cp('debian/control', 'debian/control.bak')
-      control = Debian::Control.new
-      control.parse!
-      p control.binaries.collect { |x| x['Package'] } # pkgs
-
-      dev_binaries = control.binaries.select { |x| x['Package'].include?('-dev') }
-      bin_binaries = control.binaries.select { |x| !dev_binaries.include?(x) }
-      control.binaries.replace(control.binaries[0..1])
-      dev_binaries_names = dev_binaries.collect { |x| x['Package'] }
-      bin_binaries_names = bin_binaries.collect { |x| x['Package'] }
-
-      control.binaries.replace( [{}, {}] )
-
-      bin = control.binaries[0]
-      bin.replace({'Package' => File.basename(repo), 'Architecture' => 'any', 'Section' => 'kde', 'Description' => '((TBD))'})
-
-      bin['Provides'] = Debian::Deb822.parse_relationships(bin_binaries.collect { |x| x['Package'] }.join(', '))
-      dev = control.binaries[1]
-      dev.replace({'Package' => File.basename(repo) + '-dev', 'Architecture' => 'any', 'Section' => 'kde', 'Description' => '((TBD))'})
-      dev['Provides'] = Debian::Deb822.parse_relationships(dev_binaries.collect { |x| x['Package'] }.join(', '))
-
-      bin_binaries_names.each do |package_name|
-        next if bin['Package'] == package_name
-        old_install_file_data = File.read("debian/" + package_name + ".install")
-        new_install_filename = "debian/" + bin['Package'] + ".install"
-        File.write(new_install_filename, old_install_file_data, mode: "a")
-        FileUtils.rm_f("debian/" + package_name + ".install")
-        FileUtils.rm_f("debian/" + package_name + ".symbols")
-        FileUtils.rm_f("debian/" + package_name + ".lintian-overrides")
-      end
-
-      bin_binaries.each do |bin_bin|
-        p bin_bin
-        fold_pkg(bin_bin, into: bin)
-      end
-
-      # bin['Provides'] ||= []
-      # bin['Provides'] += bin_binaries.collect { |x| x['Package'] }.join(', ')
-
-      dev_binaries_names.each do |package_name|
-        next if dev['Package'] == package_name
-        old_install_file_data = File.read("debian/" + package_name + ".install")
-        new_install_filename = "debian/" + bin['Package'] + ".install"
-        File.write(new_install_filename, old_install_file_data, mode: "a")
-        FileUtils.rm_f("debian/" + package_name + ".install")
-        FileUtils.rm_f("debian/" + package_name + ".symbols")
-        FileUtils.rm_f("debian/" + package_name + ".lintian-overrides")
-      end
-
-      dev_binaries.each do |dev_bin|
-        fold_pkg(dev_bin, into: dev)
-      end
-
-
-      dev.each do |k, v|
-        next unless v.is_a?(Array)
-
-        v.each do |relationships|
-          next unless relationships.is_a?(Array)
-          relationships.each do |alternative|
-            next unless alternative.is_a?(Debian::Relationship)
-
-            next unless bin_binaries_names.include?(alternative.name)
-            p alternative
-            alternative.name.replace(bin['Package'])
-          end
-        end
-      end
-
-      File.write('debian/control', control.dump)
-      cmd.run('wrap-and-sort')
-      system('cat debian/control')
-
-    end
-  end
+if $PROGRAM_NAME == __FILE__
+  sixy = QtSixy.new(ARGV[0])
+  sixy.run
 end
---
+++ test/test_nci_qt_sixy.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+#
+# Copyright (C) 2016 Harald Sitter <sitter@kde.org>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) version 3, or any
+# later version accepted by the membership of KDE e.V. (or its
+# successor approved by the membership of KDE e.V.), which shall
+# act as a proxy defined in Section 6 of version 3 of the license.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+require_relative 'lib/testcase'
+require_relative '../nci/qt_sixy'
+
+require 'mocha/test_unit'
+require 'webmock/test_unit'
+require 'net/ssh/gateway' # so we have access to the const
+
+class NCIRepoCleanupTest < TestCase
+  def setup
+  end
+
+  def teardown
+  end
+
+  def mock_repo
+    repo = mock
+    repo.expects(:Name)
+  end
+
+  def test_sixy_repo
+    puts "XXX data #{data}"
+    sixy = QtSixy.new(data)
+    sixy.run
+
+    #series = NCI.series
+    #names = RepoNames.all('foo')
+    #assert_equal(series.size, names.size)
+  end
+end
