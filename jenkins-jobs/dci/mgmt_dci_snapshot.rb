e33558bed53a6956c16323377112cfaf01260404
Scarlett Moore <sgmoore@kde.org>
2021-12-14 07:18:47 -0700

    Add back release_type ENV var for dci as it is needed in new methods.

--- jenkins-jobs/dci/mgmt_dci_snapshot.rb
+++ jenkins-jobs/dci/mgmt_dci_snapshot.rb
@@ -5,20 +5,22 @@ require_relative '../job'
 class DCISnapShotJob < JenkinsJob
   attr_reader :architecture
   attr_reader :release
+  attr_reader :release_type
   attr_reader :snapshot
   attr_reader :series
   attr_reader :arm_board
 
-  def initialize(snapshot:, series:, release:, architecture:, arm_board: nil)
+  def initialize(snapshot:, series:, release_type:, release:, architecture:, arm_board: nil)
     @release = release
+    @release_type = release_type
     @architecture = architecture
     @snapshot = snapshot
     @series = series
     @arm_board = arm_board
     if @arm_board
-      super("snapshot_#{series}_#{release}_#{snapshot}_#{architecture}_#{arm_board}", 'mgmt_dci_snapshot.xml.erb')
+      super("snapshot_#{series}_#{release_type}_#{release}_#{snapshot}_#{architecture}_#{arm_board}", 'mgmt_dci_snapshot.xml.erb')
     else
-      super("snapshot_#{series}_#{release}_#{snapshot}_#{architecture}", 'mgmt_dci_snapshot.xml.erb')
+      super("snapshot_#{series}_#{release_type}_#{release}_#{snapshot}_#{architecture}", 'mgmt_dci_snapshot.xml.erb')
     end
   end
 end
