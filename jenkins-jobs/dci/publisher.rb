46a1a56a48db62bc96f5aa2e65193f77fdda3139
Scarlett Moore <sgmoore@kde.org>
2022-02-16 19:29:55 -0700

    Update publisher template to current set of plugins and options. Add dci lib method to generate repo_names update publisher method options to what is needed by erb. Update multijob with new publisher variables. Update job updater with distribution needed by publisher.

--- jenkins-jobs/dci/publisher.rb
+++ jenkins-jobs/dci/publisher.rb
@@ -1,19 +1,21 @@
 # frozen_string_literal: true
 require_relative 'dci_project_multi_job'
+require_relative '../job'
 require_relative '../../lib/dci'
 
 # publisher
-class DCIPublisherJob < DCIProjectMultiJob
+class DCIPublisherJob < JenkinsJob
   attr_reader :distribution
   attr_reader :basename
-  attr_reader :repo
+  attr_reader :series_release_repo
   attr_reader :component
   attr_reader :name
   attr_reader :architecture
 
-  def initialize(basename, distribution:, component:, name:, architecture:, upload_map:)
+  def initialize(basename, distribution:, series:, component:, name:, architecture:, upload_map:)
     super("#{basename}_pub", 'dci_publisher.xml.erb')
     @distribution = distribution
+    @series = series
     @component = component
     @name = name
     @architecture = architecture
@@ -21,14 +23,8 @@ class DCIPublisherJob < DCIProjectMultiJob
     raise 'We can do nothing here without an upload_map' unless @upload_map
 
     @repo = DCI.upload_map_repo(@component)
-    @repo_names = []
+    @series_release_repo = DCI.series_release_repo(@series, @repo)
   end
-
-  def repo_names
-    @repo_names << DCI.series_release_repo(@series, @repo)
-    @repo_names
-  end
-
 
   def aptly_resources
     @repo_names.size > 1 ? 0 : 1
