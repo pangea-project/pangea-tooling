727a9ae00ea3d57c3306abcc0aa98cacda9b7f59
Scarlett Moore <sgmoore@kde.org>
2022-03-06 10:53:10 -0700

    Tidy up img attributes and add back series.

--- jenkins-jobs/dci/dci_img.rb
+++ jenkins-jobs/dci/dci_img.rb
@@ -5,20 +5,19 @@ require_relative '../../lib/dci'
 class DCIImageJob < JenkinsJob
   attr_reader :repo
   attr_reader :branch
-  attr_reader :release_distribution
+  attr_reader :distribution
   attr_reader :release_type
   attr_reader :release
   attr_reader :architecture
-  attr_reader :series
 
   def initialize(series:, release_type:, release:, architecture:, repo:, branch:)
-    @series = series
+    super("img_#{release_type}_#{release}-#{series}_#{architecture}", 'dci_img.xml.erb')
     @release_type = release_type
     @release = release
-    @release_distribution = DCI.release_distribution(@release, @series)
+    @series = series
+    @distribution = DCI.release_distribution(@release, series)
     @architecture = architecture
     @repo = repo
     @branch = branch
-    super("img_#{@release_type}_#{@release_distribution}_#{@architecture}", 'dci_img.xml.erb')
   end
 end
