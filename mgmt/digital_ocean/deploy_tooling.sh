e29451c9a3ddfbf9b4de60e12b6655f5e5c7f4f6
Jonathan Esk-Riddell <jr@jriddell.org>
2022-04-27 09:42:36 +0100

    explicitly install old bundler version to work around https://github.com/rubygems/rubygems/issues/5492

--- mgmt/digital_ocean/deploy_tooling.sh
+++ mgmt/digital_ocean/deploy_tooling.sh
@@ -35,7 +35,7 @@ cd /tmp/tooling
 
 ## from  mgmt_tooling_deploy.xml
 rm -rv .bundle || true
-gem install --no-document bundler
+gem install --no-document --version 2.3.11 bundler
 # FIXME: add --without development test back
 # https://github.com/pangea-project/pangea-tooling/issues/17
 bundle install --jobs=`nproc` --system
