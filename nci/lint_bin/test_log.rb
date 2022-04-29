3ac1597c75cfaad87f8bbf214700a10185e1bb0b
Harald Sitter <sitter@kde.org>
2022-04-29 16:37:28 +0200

    fix up bad merge

--- nci/lint_bin/test_log.rb
+++ nci/lint_bin/test_log.rb
@@ -2,7 +2,7 @@
 # SPDX-FileCopyrightText: 2016-2022 Harald Sitter <sitter@kde.org>
 # SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
 
-require 'uri'
+require 'open-uri'
 
 require_relative '../../lib/lint/log'
 require_relative '../../lib/retry'
