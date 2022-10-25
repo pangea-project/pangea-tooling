cfb695cbab1f3d1523db757c7fa09634c87c9e1e
Jonathan Esk-Riddell <jr@jriddell.org>
2022-10-25 17:23:41 +0100

    sync build-hooks-neon-mobile from build-hooks-neon-jammy

--- nci/imager/config-settings-neon-mobile.sh
+++ nci/imager/config-settings-neon-mobile.sh
@@ -1,5 +1,5 @@
 EDITION=$(echo $NEONARCHIVE | sed 's,/, ,')
 export LB_ISO_VOLUME="${IMAGENAME} ${EDITION} Plasma Mobile \$(date +%Y%m%d-%H:%M)"
 export LB_ISO_APPLICATION="KDE neon Plasma Mobile Live"
-export LB_LINUX_FLAVOURS="generic-hwe-20.04"
+export LB_LINUX_FLAVOURS="generic-hwe-22.04"
 export LB_LINUX_PACKAGES="linux"
