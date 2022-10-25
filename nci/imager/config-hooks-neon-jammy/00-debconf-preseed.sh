29956e751b340b774cc35ea8c2114caafbd2c417
Jonathan Esk-Riddell <jr@jriddell.org>
2022-10-25 17:56:16 +0100

    do not special case jammy ISOs

--- nci/imager/config-hooks-neon-jammy/00-debconf-preseed.sh
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: 2020 Harald Sitter <sitter@kde.org>
-# SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
-
-# preseed debconf selections
-
-# - disable man-db updates, they are super slow and who even uses man-db...
-cat << EOF > config/preseed/000-neon.preseed
-man-db man-db/auto-update boolean false
-EOF
