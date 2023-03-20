98060814fb13f64a525155ced95625bc43a08c52
Harald Sitter <sitter@kde.org>
2023-03-20 16:47:16 +0100

    add missing require

--- nci/kf6_deconflictor.rb
+++ nci/kf6_deconflictor.rb
@@ -4,6 +4,7 @@
 # SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
 
 require 'fileutils'
+require 'json'
 
 require_relative '../lib/apt'
 require_relative '../lib/kdeproject_component'
