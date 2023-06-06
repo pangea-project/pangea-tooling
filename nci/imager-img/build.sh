7e54d111d5cf3e20f8bb4f7bf4ad2ee3e3dcd3ec
carlosd <60242467+carlosdem@users.noreply.github.com>
2023-06-06 11:20:03 +1000

    stop complaints for fdisk

--- nci/imager-img/build.sh
+++ nci/imager-img/build.sh
@@ -20,7 +20,7 @@ DATETIME="${_DATE}-${_TIME}"
 REMIX_NAME="pinebook-remix"
 export LIVE_IMAGE_NAME="${IMAGENAME}-${REMIX_NAME}-${TYPE}-${DATETIME}"
 
-apt-get -y install qemu-user-static cpio parted udev zsync pigz live-build
+apt-get -y install qemu-user-static cpio parted udev zsync pigz live-build fdisk
 
 lb clean --all
 rm -rf config
