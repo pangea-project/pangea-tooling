e0571b32da9b628115c7679c48f2759aedf02ce6
Harald Sitter <sitter@kde.org>
2023-09-12 11:36:51 +0200

    docker: try new way of pruning data
    prefer docker prune over our logic. currently we are seeing docker
    leaking overlay2 data. I have a suspicion it is because of our
    aggressive cleanup. so, let's try to let docker system prune do its
    thing first and then we go over everything again with our flamethrower

--- mgmt/docker_cleanup.rb
+++ mgmt/docker_cleanup.rb
@@ -1,9 +1,19 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+# SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
+# SPDX-FileCopyrightText: 2015 Rohan Garg <rohan@garg.io>
+# SPDX-FileCopyrightText: 2015 Harald Sitter <sitter@kde.org>
+
+require 'tty/command'
 require_relative '../lib/docker/cleanup'
 
 Docker.options[:read_timeout] = 3 * 60 * 60 # 3 hours.
 
+# First try to let docker do its thing
+cmd = TTY::Command
+cmd.run('docker', 'system', 'prune', '--all', '--volumes', '--force')
+
+# Then run our aggressive cleanup routines
 Docker::Cleanup.containers
 Docker::Cleanup.images
