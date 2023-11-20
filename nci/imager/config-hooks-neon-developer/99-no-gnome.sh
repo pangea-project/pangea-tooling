15b13f003ecdf054048624573e7a249bbe6818a2
Carlos De Maine <carlosdemaine@gmail.com>
2023-11-20 20:24:04 +1000

    drop all data/conf files which have moved to invent

--- nci/imager/config-hooks-neon-developer/99-no-gnome.sh
+++
@@ -1,4 +0,0 @@
-# SPDX-FileCopyrightText: 2020 Jonathan Riddell <jr@jriddell.org>
-# SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
-
-apt remove -y gnome-shell ubuntu-session
