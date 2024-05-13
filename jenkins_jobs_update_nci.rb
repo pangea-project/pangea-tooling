730aa005ba824e2ec699e0b08b7f4538e1eb3c89
Carlos De Maine <carlosdemaine@gmail.com>
2024-05-13 15:11:56 +1000

    begin to move docker stuffs to neon

--- jenkins_jobs_update_nci.rb
+++ jenkins_jobs_update_nci.rb
@@ -440,6 +440,7 @@ class ProjectUpdater < Jenkins::ProjectUpdater
                                          type: 'unstable',
                                          dist: NCI.future_series))
     end
+    docker = enqueue(MGMTDockerJob.new(dependees: []))
     enqueue(MGMTJenkinsPruneParameterListJob.new)
     enqueue(MGMTJenkinsPruneOld.new)
     enqueue(MGMTJenkinsJobScorer.new)
