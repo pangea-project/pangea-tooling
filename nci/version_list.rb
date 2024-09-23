e3d72b9218d474d6997ab8ba57b332408f9d0c0f
carlosd <60242467+carlosdem@users.noreply.github.com>
2024-09-23 16:59:30 +1000

    fix version_list

--- nci/version_list.rb
+++ nci/version_list.rb
@@ -18,6 +18,17 @@ require_relative 'version_list/violations'
 S_IROTH = 0o4 # posix bitmask for world readable
 
 DEBIAN_TO_KDE_NAMES = {
+  'attica-kf5' => 'attica',
+  'baloo-kf5' => 'baloo',
+  'kactivities-kf5' => 'kactivities',
+  'kcalcore' => 'kcalendarcore', # fixed in kf6 with kf6-kcalendarcore
+  'kdeconnect' => 'kdeconnect-kde',
+  'kdevelop-php' => 'kdev-php',
+  'kdevelop-python' => 'kdev-python',
+  'kdnssd-kf5' => 'kdnssd',
+  'kde-spectacle' => 'spectacle',
+  'kfilemetadata-kf5' => 'kfilemetadata',
+  'kf6-userfeedback'  => 'kf6-kuserfeedback', # why isn't the source name matching the package name??
   'kpim6-incidenceeditor' => 'incidenceeditor',
   'kpim6-pimcommon' => 'pimcommon',
   'kpim6-mailcommon' => 'mailcommon',
@@ -31,78 +42,91 @@ DEBIAN_TO_KDE_NAMES = {
   'kpim6-libgravatar' => 'libgravatar',
   'kpim6-messagelib' => 'messagelib',
   'kpim6-libkgapi' => 'libkgapi',
-  'kde-spectacle' => 'spectacle',
-  'libkf5kipi' => 'libkipi',
-  'kdeconnect' => 'kdeconnect-kde',
   'kio-extras5' => 'kio-extras-kf5',
-
-  # frameworks
-  'kactivities-kf5' => 'kactivities',
-  'kdnssd-kf5' => 'kdnssd',
-  'kwallet-kf5' => 'kwallet',
-  'baloo-kf5' => 'baloo',
   'ksyntax-highlighting' => 'syntax-highlighting',
-  'attica-kf5' => 'attica',
-  'prison-kf5' => 'prison',
-  'kfilemetadata-kf5' => 'kfilemetadata',
-  # fixed in kf6 with kf6-kcalendarcore
-  'kcalcore' => 'kcalendarcore',
-
-  # plasma
+  'ktp-kded-integration-module' => 'ktp-kded-module',
+  'kwallet-kf5' => 'kwallet',
+  'libkf5kipi' => 'libkipi',
   'plasma-discover' => 'discover',
-
-  # KDE Gear
-  'kdevelop-php' => 'kdev-php',
-
-  # the stupidest name of all them stupid divergent names. like what does this
-  # even accomplish...
-  'ktp-kded-integration-module' => 'ktp-kded-module'
+  'prison-kf5' => 'prison'
 }
-
-kf6 = KDEProjectsComponent.kf6
-kf6_debian_to_kde_names = {}
-kf6.each do |framework|
-  kf6_debian_to_kde_names[framework] = framework.gsub('kf6-','')
-end
 
 # Sources that we do not package for some reason. Should be documented why!
 BLACKLIST = [
-  # Not actually useful for anything in production. It's a repo with tests.
-  'plasma-tests',
-  'kfloppy',   # dead project removed next release
-  'kdev-python',
-  'kalendar',   # is now merkuno and reports as conflicting with kcalendarcore
-  'kopete', # removed in 24.02
-  'libkgapi' # we package an old version
+  'extra-cmake-modules',    # superseded by kf6-extra-cmake-modules
+  'breeze-icons',           # superseded by kf6-breeze-icons
+  'kactivities',            # superseded by plasma-activities
+  'kactivities-stats',      # superseded by plasma-activities-stats
+  'kalendar',               # is now merkuno and reports as conflicting with kcalendarcore
+  'kf6-kactivities',        # moved to plasma6 as plasma-activities
+  'kf6-kactivities-stats',  # moved to plasma6 as plasma-activities-stats
+  'kf6-kdelibs4support',    # kde5 framework that was deprecated
+  'kf6-kdesignerplugin',    # kde5 framework that was deprecated
+  'kf6-kdewebkit',          # kde5 framework that was deprecated
+  'kf6-kemoticons',         # kde5 framework that was deprecated
+  'kf6-khtml',              # kde5 framework that was deprecated
+  'kf6-kinit',              # kde5 framework that was deprecated
+  'kf6-kjs',                # kde5 framework that was deprecated
+  'kf6-kjsembed',           # kde5 framework that was deprecated
+  'kf6-kmediaplayer',       # kde5 framework that was deprecated
+  'kf6-plasma-framework',   # moved to plasma6 as libplasma
+  'kf6-kross',              # kde5 framework that was deprecated
+  'kf6-kwayland',           # moved to plasma6 as kwayland
+  'kf6-kxmlrpcclient',      # kde5 framework that was deprecated
+  'kfloppy',                # dead project removed in 23.08
+  'kopete',                 # dead project removed in 24.02
+  'krunner',                # superseded by kf6-krunner
+  'libkgapi',               # we package an old version
+  'oxygen-icons',           # superseded by kf6-oxygen-icons
+  'plasma-framework',       # superseded by libplasma
+  'plasma-tests'            # not actually useful for anything in production. It's a repo with tests.
 ]
 
 # Maps "key" packages to a release scope. This way we can identify what version
 # the given scope has in our repo.
 KEY_MAPS = {
-  'plasma-workspace' => 'Plasma by KDE',
-  'kconfig' => 'KDE Frameworks',
-  'okular' => 'KDE Gear'
+  'kio' => 'KDE Frameworks 5',
+  'kf6-kconfig' => 'KDE Frameworks 6',
+  'okular' => 'KDE Gear',
+  'plasma-workspace' => 'Plasma by KDE'
 }
 
 key_file = ENV.fetch('SSH_KEY_FILE', nil)
 ssh_args = key_file ? [{ keys: [key_file] }] : []
 
 product_and_versions = []
-
 # Grab list of all released tarballs
-%w[release-service frameworks plasma].each do |scope|
+# frameworks needs to before frameworks5, as product_and_versions is a map that we keep adding
+# to, so or our tricky tarball product renaming would rename frameworks5 products as well
+%w[frameworks frameworks5 release-service plasma].each do |scope|
+
+  dir_path = "stable/#{scope}/"
+
+  # allow for farameworks5 scope not matching directory name
+  if scope == 'frameworks5'
+    dir_path.gsub!(/5/, '')
+  end
+
+  # debug our scope
+  puts "processing #{scope} scope"
+
   Net::SFTP.start('rsync.kde.org', 'ftpneon', *ssh_args) do |sftp|
-    # delete old directories
-    dir_path = "stable/#{scope}/"
-    version_dirs = sftp.dir.glob(dir_path, '*')
+    if scope == 'frameworks5'
+    # narrow scope for frameworks5 directory matching only on 5.* releases
+      version_dirs = sftp.dir.glob(dir_path, '5.*')
+    else
+      version_dirs = sftp.dir.glob(dir_path, '*')
+    end
     version_dirs = version_dirs.select(&:directory?)
+    version_dirs.select(&:directory?)
     version_dirs = version_dirs.sort_by { |x| Gem::Version.new(x.name) }
-    # lowest is first, pick the latest two. one of them must be world readable!
+
     latest = version_dirs[-2..-1].reverse.find do |dir|
       world_readable = ((dir.attributes.permissions & S_IROTH) == S_IROTH)
+      warn "Version #{dir.name} of #{scope} has file permissions of #{dir.attributes.permissions}!"
       unless world_readable
         warn "Version #{dir.name} of #{scope} not world readable!" \
-            " This will mean that this scope's version isn't checked!"
+          " This will mean that this scope's version isn't checked!"
         next nil
       end
       dir
@@ -110,22 +134,37 @@ product_and_versions = []
 
     unless latest
       raise 'Neither the latest nor the previous version are world readable!' \
-            ' Something is astray! This means there are two pending releases???'
+          ' Something is astray! This means there are two pending releases???'
     end
 
+    sig_ends = %w[.sig .asc]
     latest_path = "#{dir_path}/#{latest.name}/"
     tars = sftp.dir.glob(latest_path, '**/**')
-
     tars = tars.select { |x| x.name.include?('.tar.') }
-    sig_ends = %w[.sig .asc]
     tars = tars.reject { |x| sig_ends.any? { |s| x.name.end_with?(s) } }
 
+    # collect all our tarballs and map them to product name and version
     product_and_versions += tars.collect do |tar|
       name = File.basename(tar.name) # strip possible subdirs
       match = name.match(/(?<product>[-\w]+)-(?<version>[\d\.]+)\.tar.+/)
       raise "Failed to parse #{name}" unless match
-
       [match[:product], match[:version]]
+    end
+
+    # frameworks need to become kf6-* prefixed to match our package names
+    # since there are already 2 scopes with products of the same name but
+    # different versions we need to change the kf6 scoped product names
+    if scope == 'frameworks'
+      product_and_versions.sort.map do |k, v|
+        k.gsub!(/\A/,"kf6-")
+        # debug our product and versions
+        puts "the current scope= #{scope} && product= #{k} && version= #{v}"
+      end
+    else
+      product_and_versions.sort.map do |k, v|
+        # debug our product and versions
+        puts "the current scope= #{scope} && product= #{k} && version= #{v}"
+      end
     end
   end
 end
@@ -145,7 +184,6 @@ Aptly::Ext::Remote.neon do
 
     # map debian names to kde names so we can easily compare things
     by_name = by_name.collect { |k, v| [DEBIAN_TO_KDE_NAMES.fetch(k, k), v] }.to_h
-    by_name = by_name.collect { |k, v| [kf6_debian_to_kde_names.fetch(k, k), v] }.to_h
 
     # Hash the packages by their versions, take the versions and sort them
     # to get the latest available version of the specific package at hand.
@@ -160,12 +198,12 @@ Aptly::Ext::Remote.neon do
     # This kind of collapses all plasma packages into one Plasma entry for
     # example.
     KEY_MAPS.each do |key_package, pretty_name|
-      version = by_name[key_package]
-      scoped_versions[pretty_name] = version
+      puts version = by_name[key_package]
+      puts scoped_versions[pretty_name] = version
     end
 
     # The same entity can appear with different versions. Notably that happens
-    # when a hotfix is put in the same directory. For exampke kio 5.74.0 had
+    # when a hotfix is put in the same directory. For example kio 5.74.0 had
     # a bug so 5.74.1 is put in the same dir (even though frameworks usually
     # have no .1 releases).
     # More generally put that means if the same product appears more than once
@@ -178,15 +216,14 @@ Aptly::Ext::Remote.neon do
     product_and_versions.map do |k, v|
       product_and_versions_h[k] ||= v
       next if Gem::Version.new(product_and_versions_h[k]) >= Gem::Version.new(v)
-
-      product_and_versions_h[k] = v # we found a greater version
+      puts product_and_versions_h[k] = v # we found a greater version
     end
 
     checker = DidYouMean::SpellChecker.new(dictionary: by_name.keys)
     product_and_versions_h.each do |remote_name, remote_version|
       next if BLACKLIST.include?(remote_name) # we don't package some stuff
 
-      in_repo = by_name.include?(remote_name)
+      puts in_repo = by_name.include?(remote_name)
       unless in_repo
         corrections = checker.correct(remote_name)
         violations << MissingPackageViolation.new(remote_name, corrections)
@@ -197,6 +234,8 @@ Aptly::Ext::Remote.neon do
       # release such as plasma it doesn't get listed separately in our
       # output hash.
       repo_version = by_name.delete(remote_name)
+
+
       if repo_version != remote_version
         violations <<
           WrongVersionViolation.new(remote_name, remote_version, repo_version)
@@ -205,9 +244,9 @@ Aptly::Ext::Remote.neon do
     end
 
     packaged_versions = packaged_versions.merge(by_name)
-  end
-end
 
+end
+end
 template = ERB.new(File.read("#{__dir__}/version_list/version_list.html.erb"))
 html = template.result(OpenStruct.new(
   scoped_versions: scoped_versions,
