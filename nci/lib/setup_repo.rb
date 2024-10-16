8809400f4f4b54cbb6aeec66cb1277d8ea1991c0
carlosd <60242467+carlosdem@users.noreply.github.com>
2024-10-16 18:48:27 +1000

    drop it as it seems to be adding jammy to noble sources.list

--- nci/lib/setup_repo.rb
+++ nci/lib/setup_repo.rb
@@ -178,9 +178,6 @@ APT::Default-Release "#{setup_repo_codename}";
     end
 
     def type_to_repo(type, dist)
-      if dist == "jammy"
-        @type = 'testing' if @type == 'stable'
-      end
       type.tr('-', '/')
     end
 
